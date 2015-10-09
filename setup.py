5b5c8e79200403e042c3c6507bc838b8c293f227
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2015-10-09 15:28:37 +0200

    add a bunch of files for demo

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
+# coding: utf8
+
+__author__ = "Jérôme Kieffer"
+__date__ = "09/10/2015"
+__license__ = "MIT"
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
 
 classifiers = ["Development Status :: 1 - Planning",
                "Environment :: Console",
@@ -17,18 +22,27 @@ classifiers = ["Development Status :: 1 - Planning",
                "Operating System :: Microsoft :: Windows",
                "Operating System :: POSIX",
                "Programming Language :: Cython",
-               "Programming Language :: Python",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 3",
                "Programming Language :: Python :: Implementation :: CPython",
                "Topic :: Documentation :: Sphinx",
                "Topic :: Scientific/Engineering :: Physics",
                "Topic :: Software Development :: Libraries :: Python Modules",
                ]
+
+with open("README.md", "r")as fp:
+    long_description = fp.read()
+
 setup(name='silx',
       version='0.0.1',
       url="https://github.com/silex-kit/silx",
       author="data analysis unit",
       author_email="silx@esrf.fr",
       classifiers = classifiers,
+      description="Software library for X-Ray data analysis",
+      long_description=long_description,
+      packages=["silx", "silx.io", "silx.third_party", "silx.visu"],
+
       )
 
 
