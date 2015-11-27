a5d4dc5e661c2a9e87dff24a217353eb3289d621
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-11-27 14:12:40 +0100

    Prevent the sources from being imported

--- setup.py
+++ setup.py
@@ -2,22 +2,26 @@
 # coding: utf8
 
 __author__ = "Jérôme Kieffer"
-__date__ = "09/10/2015"
+__date__ = "27/11/2015"
 __license__ = "MIT"
 
 import sys
 import os
 try:
     from setuptools import setup
+    from setuptools.command.build_py import build_py as _build_py
 except ImportError:
     from distutils.core import setup
+    from distutils.command.build_py import build_py as _build_py
+
+
+PROJECT = "silx"
+cmdclass = {}
 
 
 def get_version():
-    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "silx"))
-    import _version
-    sys.path.pop(0)
-    return _version.strictversion
+    import version
+    return version.strictversion
 
 
 def get_readme():
@@ -48,21 +52,40 @@ classifiers = ["Development Status :: 1 - Planning",
                ]
 
 
+class build_py(_build_py):
+    """
+    Enhanced build_py which copies version to the built
+    """
+    def build_package_data(self):
+        """Copy data files into build directory
+        Patched in such a way version.py -> silx/_version.py"""
+        print(self.data_files)
+        _build_py.build_package_data(self)
+        for package, src_dir, build_dir, filenames in self.data_files:
+            if package == PROJECT:
+                filename = "version.py"
+                target = os.path.join(build_dir, "_" + filename)
+                self.mkpath(os.path.dirname(target))
+                self.copy_file(os.path.join(filename), target,
+                               preserve_mode=False)
+                break
+
+cmdclass['build_py'] = build_py
+
 install_requires = ["numpy", "h5py"]
 setup_requires = ["numpy", "cython"]
 
 
-setup(name='silx',
+setup(name=PROJECT,
       version=get_version(),
       url="https://github.com/silex-kit/silx",
       author="data analysis unit",
       author_email="silx@esrf.fr",
-      classifiers = classifiers,
+      classifiers=classifiers,
       description="Software library for X-Ray data analysis",
       long_description=get_readme(),
       packages=["silx", "silx.io", "silx.third_party", "silx.visu"],
       install_requires=install_requires,
       setup_requires=setup_requires,
+      cmdclass=cmdclass,
       )
-
-
--- silx/__init__.py
+++ silx/__init__.py
@@ -1,5 +1,12 @@
-# Placeholder
+# coding: utf-8
+from __future__ import absolute_import, print_function, division
+
 __author__ = "Jérôme Kieffer"
 __license__ = "MIT"
-__date__ = "11/10/2015"
-from _version import version_info, strictversion, hexversion
+__date__ = "27/11/2015"
+
+try:
+    from ._version import __date__ as date
+    from ._version import version, version_info, hexversion, strictversion
+except ImportError:
+    raise RuntimeError("Do NOT use silx from its sources: build it and use the built version")
