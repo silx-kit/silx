a78bc344f9ee6d03a6acca0e6a127fc5462f53d6
Payno <henri.payno@gmail.com>
2015-12-08 16:57:16 +0100

    Fix resetting the module loading

--- run_tests.py
+++ run_tests.py
@@ -237,9 +237,7 @@ if options.insource:
 
     sys.path.insert(0, build_dir)
     logger.warning("Patched sys.path, added: '%s'" % build_dir)
-    print("try to import " + PROJECT_NAME)
-    # TODO : next line is an issue for CircleCI. If removed. Pass the test ok. Otherwise fail.
-    # module = importlib.import_module(PROJECT_NAME)
+    module = importlib.import_module(PROJECT_NAME)
 
 
 PROJECT_VERSION = getattr(module, 'version', '')
