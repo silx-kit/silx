bd6d57a841fd52a3b71041d88ca82bc801a5e0a8
Thomas Vincent <thomas.vincent@esrf.fr>
2015-12-08 11:10:33 +0100

    Fix CONFIG_ARGS None on appveyor

--- ci/info_platform.py
+++ ci/info_platform.py
@@ -7,7 +7,7 @@ __date__ = "08/12/2015"
 __license__ = "MIT"
 
 
-import sys, numpy
+import sys
 
 print("Python %s bits" % (tuple.__itemsize__ * 8))
 print("       maxsize: %s\t maxunicode: %s" % (sys.maxsize, sys.maxunicode))
@@ -16,12 +16,22 @@ print(" ")
 
 try:
     from distutils.sysconfig import get_config_vars
-except:
+except ImportError:
     from sysconfig import get_config_vars
-print("Config " + " ".join(get_config_vars("CONFIG_ARGS")))
+
+config = get_config_vars("CONFIG_ARGS")
+if config is not None:
+    print("Config: " + " ".join(config))
+else:
+    print("Config: None")
 print("")
 
-print("Numpy %s" % numpy.version.version)
-print("      include %s" % numpy.get_include())
-print("      options %s" % numpy.get_printoptions())
+try:
+    import numpy
+except ImportError:
+    print("Numpy not installed")
+else:
+    print("Numpy %s" % numpy.version.version)
+    print("      include %s" % numpy.get_include())
+    print("      options %s" % numpy.get_printoptions())
 print("")
