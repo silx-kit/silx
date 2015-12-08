e31ce0b19bb60749d15c6522ac102177dc5a610f
Payno <henri.payno@gmail.com>
2015-12-08 09:48:17 +0100

    Test modification of circleCrFile

--- testCircleCI.py
+++ testCircleCI.py
@@ -1,3 +1,7 @@
-class Class01(object):
-	"""docstring for ClassName"""
-		
+class MyFirstClass(object):
+	"""this is the constructor of my first class in Python"""
+
+
+newClass = MyFirstClass();
+
+print(newClass.__doc__)	
