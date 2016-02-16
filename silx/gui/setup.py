b8caa9500c4d7ad01847df2e4c2c9b99d37537fa
Thomas Vincent <thomas.vincent@esrf.fr>
2016-02-16 10:50:16 +0100

    Add place-holder packages for widgets and plot

---
+++ silx/gui/setup.py
@@ -0,0 +1,14 @@
+from numpy.distutils.misc_util import Configuration
+
+
+def configuration(parent_package='', top_path=None):
+    config = Configuration('gui', parent_package, top_path)
+    config.add_subpackage('plot')
+
+    return config
+
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+
+    setup(configuration=configuration)
--- silx/setup.py
+++ silx/setup.py
@@ -3,10 +3,10 @@ from numpy.distutils.misc_util import Configuration
 
 def configuration(parent_package='', top_path=None):
     config = Configuration('silx', parent_package, top_path)
+    config.add_subpackage('gui')
     config.add_subpackage('io')
     config.add_subpackage('test')
     config.add_subpackage('third_party')
-    config.add_subpackage('visu')
 
     config.add_extension('dummy',
                          sources=['dummy.pyx'],
