452cd0c6846b6be47b77c4a70df4ce3396a0d489
Pierre Knobel <pierre.knobel@esrf.fr>
2016-02-22 10:07:39 +0100

    Define macros for locale management
    Without this, atof can't decode floats properly for certain locales,
    like de_DE.

--- silx/io/setup.py
+++ silx/io/setup.py
@@ -1,5 +1,36 @@
+import os
+import sys
+
 import numpy
 from numpy.distutils.misc_util import Configuration
+
+# Locale and platform management
+SPECFILE_USE_GNU_SOURCE = os.getenv("SPECFILE_USE_GNU_SOURCE")
+if SPECFILE_USE_GNU_SOURCE is None:
+    SPECFILE_USE_GNU_SOURCE = 0
+    if sys.platform.lower().startswith("linux"):
+        print("WARNING:")
+        print("A cleaner locale independent implementation")
+        print("may be achieved setting SPECFILE_USE_GNU_SOURCE to 1")
+        print("For instance running this script as:")
+        print("SPECFILE_USE_GNU_SOURCE=1 python setup.py build")
+else:
+    SPECFILE_USE_GNU_SOURCE = int(SPECFILE_USE_GNU_SOURCE)
+
+if sys.platform == "win32":
+    define_macros = [('WIN32',None)]
+elif os.name.lower().startswith('posix'):
+    define_macros = [('SPECFILE_POSIX', None)]
+    #this one is more efficient but keeps the locale
+    #changed for longer time
+    #define_macros = [('PYMCA_POSIX', None)]
+    #the best choice is to have _GNU_SOURCE defined
+    #as a compilation flag because that allows the
+    #use of strtod_l
+    if SPECFILE_USE_GNU_SOURCE:
+        define_macros = [('_GNU_SOURCE', 1)]
+else:
+    define_macros = []
 
 
 def configuration(parent_package='', top_path=None):
@@ -14,6 +45,7 @@ def configuration(parent_package='', top_path=None):
 
     config.add_extension('specfile',
                          sources=sources,
+                         define_macros = define_macros,
                          include_dirs = ['specfile/include', numpy.get_include()],
                          language='c')
 
