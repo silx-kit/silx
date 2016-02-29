eaec77cf7ed14204cd4dfed2c360ecb6532dd9c0
Damian Naudet <naudet@esrf.fr>
2016-02-29 13:41:04 +0100

    Added math module folder.

---
+++ silx/math/setup.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+import numpy
+
+from numpy.distutils.misc_util import Configuration
+
+
+def configuration(parent_package='', top_path=None):
+    config = Configuration('math', parent_package, top_path)
+
+    return config
+
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+
+    setup(configuration=configuration)
