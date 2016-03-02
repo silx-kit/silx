8003efd69b552bcd7810e3082f351b2e444486d1
Damien Naudet <naudet@linnaudet.esrf.fr>
2016-03-02 15:08:54 +0100

    Fixed : math module not declared in the silx setup script.

--- silx/setup.py
+++ silx/setup.py
@@ -33,6 +33,7 @@ from numpy.distutils.misc_util import Configuration
 def configuration(parent_package='', top_path=None):
     config = Configuration('silx', parent_package, top_path)
     config.add_subpackage('io')
+    config.add_subpackage('math')
     config.add_subpackage('test')
     config.add_subpackage('third_party')
     config.add_subpackage('visu')
