a9ff85fa90801d1b7a28bad26b7100225e8e1446
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-03-03 10:19:01 +0100

    auto-project name

--- silx/__init__.py
+++ silx/__init__.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import, print_function, division
 
 __author__ = "Jérôme Kieffer"
 __license__ = "MIT"
-__date__ = "27/11/2015"
+__date__ = "28/01/2016"
 
+import os
+project = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 try:
     from ._version import __date__ as date
     from ._version import version, version_info, hexversion, strictversion
 except ImportError:
-    raise RuntimeError("Do NOT use silx from its sources: build it and use the built version")
+    raise RuntimeError("Do NOT use %s from its sources: build it and use the built version" % project)
