bd0a4d4795b66c31ce8c9685b5641ee7a2e443f3
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-03-17 09:42:11 +0100

    move image & resolb conflict in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -121,20 +121,12 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-<<<<<<< HEAD
-# html_logo = None
-=======
-html_logo = "silx.png"
->>>>>>> upstream/master
+html_logo = "img/silx.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-<<<<<<< HEAD
-# html_favicon = None
-=======
-html_favicon = "silx.ico"
->>>>>>> upstream/master
+html_favicon = "_static/silx.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
