35e3f67560406d10e2d4a7a1b18ee5215c9e54ef
Pierre <pierre.knobel@esrf.fr>
2016-03-18 08:21:05 +0100

    Fix test for platform specific failure (int type <i4> on windows)

--- silx/io/test/test_utils.py
+++ silx/io/test/test_utils.py
@@ -23,12 +23,23 @@
 #############################################################################*/
 """Tests for utils module"""
 
+__authors__ = ["P. Knobel"]
+__license__ = "MIT"
+__date__ = "18/03/2016"
+
 import h5py
 import os
+import re
 import tempfile
 import unittest
 
 from silx.io.utils import repr_hdf5_tree
+
+def assert_match_any_string_in_list(test, pattern, list_of_strings):
+    for string_ in list_of_strings:
+        if re.match(pattern, string_):
+            return True
+    return False
 
 
 class TestReprHDF5Tree(unittest.TestCase):
@@ -55,20 +66,29 @@ class TestReprHDF5Tree(unittest.TestCase):
     def tearDown(self):
         os.unlink(self.h5_fname)
 
+    def assertMatchAnyStringInList(self, pattern, list_of_strings):
+        for string_ in list_of_strings:
+            if re.match(pattern, string_):
+                return None
+        raise AssertionError("regex pattern %s does not match any" % pattern +
+                             " string in list " + str(list_of_strings))
+
     def test_repr(self):
-        self.theoretical_rep = "+foo\n"
-        self.theoretical_rep += "\t+bar\n"
-        self.theoretical_rep += "\t\t-tmp=\n"
-        self.theoretical_rep += "\t\t-spam=\n"
-        self.theoretical_rep += "\t-data=\n"
         rep = repr_hdf5_tree(self.h5_fname)
         lines = rep.split("\n")
 
         self.assertIn("+foo", lines)
         self.assertIn("\t+bar", lines)
-        self.assertIn('\t\t-tmp=<HDF5 dataset "tmp": shape (3,), type "<i8">', lines)
-        self.assertIn('\t\t-spam=<HDF5 dataset "spam": shape (2, 2), type "<i8">', lines)
-        self.assertIn('\t-data=<HDF5 dataset "data": shape (1,), type "<f8">', lines)
+
+        self.assertMatchAnyStringInList(
+                r'\t\t-tmp=<HDF5 dataset "tmp": shape \(3,\), type "<i[48]">',
+                lines)
+        self.assertMatchAnyStringInList(
+                r'\t\t-spam=<HDF5 dataset "spam": shape \(2, 2\), type "<i[48]">',
+                lines)
+        self.assertMatchAnyStringInList(
+                r'\t-data=<HDF5 dataset "data": shape \(1,\), type "<f[48]">',
+                lines)
 
 
 def suite():
