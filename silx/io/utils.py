bfe2c05fda57fb784175a9935b6cb44ce0ebff89
Pierre <pierre.knobel@esrf.fr>
2016-03-21 08:20:32 +0100

    Draft save curve as SpecFile

--- silx/io/utils.py
+++ silx/io/utils.py
@@ -25,11 +25,13 @@
 
 from collections import defaultdict
 import h5py
+import numpy
 import sys
+import time
 
 __authors__ = ["P. Knobel"]
 __license__ = "MIT"
-__date__ = "16/03/2016"
+__date__ = "18/03/2016"
 
 string_types = (basestring,) if sys.version_info[0] == 2 else (str,)
 
@@ -96,3 +98,56 @@ def tree():
         }
     """
     return defaultdict(tree)
+
+
+# todo: compare APIs (numpy.savetxt, PyMca5.PyMcaIO.SaveArray,
+# PyMcaGui.plotting.PlotWindow.defaultSaveAction…)
+
+def save_spec(specfile, x, y, xlabel=None, ylabels=None):
+    """Saves any number of curves to SpecFile format
+
+    :param specfile: Output Specfile name, or file handle open in write
+        mode.
+    :param x: 1D-Array (or list) of abscissa values
+    :param y: 2D-array (or list of lists) of ordinates values. First index
+        is the curve index, second index is the sample index. The length
+        of the second dimension (number of samples) must be equal to
+        ``len(x)``
+    :param xlabel: Abscissa label as a string
+    :param ylabels: List of columns labels or string of double space
+        delimited labels
+    """
+    if not hasattr(specfile, write):
+        f = open(specfile, "w")
+    else:
+        f = specfile
+
+    x_array = numpy.asarray(x)
+    y_array = numpy.asarray(y)
+
+    # enforce list type for ylabels
+    if isinstance(ylabels, string_types):
+        ylabels = ylabels.split("  ")
+
+    f.write("#F %s\n" % f.name)
+    now = "#D %s\n"%(time.ctime(time.time()))
+    f.write(now)
+    f.write("#S 1 %s\n" % ylabels[0])
+    f.write(now)
+    f.write("#N 2\n")
+    f.write("#L %s  %s\n" % (xlabel, ylabel[0]))
+
+    # for i in range(len(y)):
+    #     ffile.write("%.7g  %.7g\n" % (x[i], y[i]))
+    # ffile.write("\n")
+    if len(y_array.shape) == 1:
+        # TODO: handle one curve
+        raise NotImplementedError
+    else:
+        # TODO: handle multile curves
+        raise NotImplementedError
+savingDate = "#D %s\n"%(time.ctime(time.time()))
+
+
+    if not hasattr(specfile, write):
+        f.close()
