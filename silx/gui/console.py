4c3750b63a32b351dd7759ebe3de15e84cebb393
Pierre <pierre.knobel@esrf.fr>
2016-05-19 16:05:06 +0200

    Simplifications

---
+++ silx/gui/console.py
@@ -0,0 +1,154 @@
+# coding: utf-8
+# /*##########################################################################
+#
+# Copyright (c) 2004-2016 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+"""Widgets offering an IPython console.
+
+This widget is meant to work with :class:`PlotWindow`. The console keeps a
+reference to the :class:`PlotWindow` to allow interacting with it (adding
+curves, saving data, ...)
+"""
+__authors__ = ["Tim Rae", "V.A. Sole", "P. Knobel"]
+__license__ = "MIT"
+__date__ = "19/05/2016"
+
+import logging
+
+from . import qt
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+
+import IPython
+
+# TODO: assess dependencies (pip install ipython, qtconsole)
+# TODO: helpful error message in case ipython or qtconsole are not available
+
+# qtconsole is a separate module in recent versions of IPython/Jupyter
+# http://blog.jupyter.org/2015/04/15/the-big-split/
+if IPython.__version__.startswith("2"):
+    qtconsole = None
+else:
+    try:
+        import qtconsole
+    except ImportError:
+        qtconsole = None
+
+if qtconsole is not None:
+    try:
+        from qtconsole.rich_ipython_widget import RichJupyterWidget as RichIPythonWidget
+    except:
+        from qtconsole.rich_ipython_widget import RichIPythonWidget
+    from qtconsole.inprocess import QtInProcessKernelManager
+else:
+    # Import the console machinery from ipython
+
+    # # Check if we using a frozen version because
+    # # the test of IPython does not find the Qt bindings
+    # import sys
+    # if getattr(sys, 'frozen', False): # work with cx_Freeze
+    #     import IPython.external.qt_loaders
+    #     def has_binding(*var, **kw):
+    #         return True
+    #     IPython.external.qt_loaders.has_binding = has_binding 
+    import IPython.external.qt_loaders
+    def has_binding(*var, **kw):
+        return True
+    IPython.external.qt_loaders.has_binding = has_binding
+
+    from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+    from IPython.qt.inprocess import QtInProcessKernelManager
+
+from IPython.lib import guisupport
+
+
+class IPythonWidget(RichIPythonWidget):
+    """ Convenience class for a live IPython console widget.
+
+    :param custom_banner: Custom welcome message to be printed at the top of
+       the console.
+    """
+
+    def __init__(self, customBanner=None, *args, **kwargs):
+        super(IPythonWidget, self).__init__(*args, **kwargs)
+        if customBanner != None:
+            self.banner = customBanner
+        self.setWindowTitle(self.banner)
+        self.kernel_manager = kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel()
+        kernel_manager.kernel.gui = 'qt4'  # TODO: should "qt4" be hardcoded?
+        self.kernel_client = kernel_client = self._kernel_manager.client()
+        kernel_client.start_channels()
+
+        def stop():
+            kernel_client.stop_channels()
+            kernel_manager.shutdown_kernel()
+            guisupport.get_app_qt4().exit()
+        self.exit_requested.connect(stop)
+
+    def pushVariables(self, variableDict):
+        """ Given a dictionary containing name / value pairs, push those
+        variables to the IPython console widget """
+        self.kernel_manager.kernel.shell.push(variableDict)
+
+
+class IPythonDockWidget(qt.QDockWidget):
+    """Dock Widget including an IPython Console widget inside
+    a vertical layout.
+
+    :param available_vars: Dictionary of variables
+        (``{variable_name: object, …}`) to push to the console's
+        interactive namespace.
+    :param custom_banner: Custom welcome message to be printed at the top of
+        the console.
+    :param parent: Parent widget containing this :class:`qt.QDockWidget`
+    """
+    def __init__(self, available_vars=None, custom_banner=None, parent=None):
+        super(IPythonDockWidget, self).__init__(parent)
+
+        ipyConsole = IPythonWidget(customBanner=custom_banner)
+
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.setWidget(ipyConsole)
+
+        if available_vars is not None:
+            ipyConsole.pushVariables(available_vars)
+
+
+def main():
+    class MyUselessClass(object):
+        msg = "Dummy plt var to test pushing variables to tho console"
+
+        def print_msg(self):
+            print(self.msg)
+
+    app = qt.QApplication([])
+    var = MyUselessClass()
+    widget = IPythonDockWidget(available_vars={"var": var},
+                               custom_banner="Type `whos` for a list of " +
+                                             "available variables \n\n")
+    widget.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
