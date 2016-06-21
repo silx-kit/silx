38ec8972a04a4d20598957ff68d565578392e771
Thomas Vincent <thomas.vincent@esrf.fr>
2016-06-21 16:53:04 +0200

    Change polygon draw interaction: allow to do it with only left button

--- silx/gui/plot/PlotInteraction.py
+++ silx/gui/plot/PlotInteraction.py
@@ -489,6 +489,9 @@ class Select(StateMachine, _PlotInteraction):
 
 class SelectPolygon(Select):
     """Drawing selection polygon area state machine."""
+
+    DRAG_THRESHOLD_DIST = 4
+
     class Idle(State):
         def onPress(self, x, y, btn):
             if btn == LEFT_BTN:
@@ -499,7 +502,19 @@ class SelectPolygon(Select):
         def enter(self, x, y):
             dataPos = self.machine.plot.pixelToData(x, y)
             assert dataPos is not None
+            self._firstPos = x, y
             self.points = [dataPos, dataPos]
+
+            offset = self.machine.DRAG_THRESHOLD_DIST
+            points = [(x - offset, y - offset),
+                      (x - offset, y + offset),
+                      (x + offset, y + offset),
+                      (x + offset, y - offset)]
+            points = [self.machine.plot.pixelToData(x, y, check=False)
+                      for x, y in points]
+            self.machine.setSelectionArea(points, fill=None,
+                                          color=self.machine.color,
+                                          name='first_point')
 
         def updateSelectionArea(self):
             self.machine.setSelectionArea(self.points,
@@ -519,16 +534,45 @@ class SelectPolygon(Select):
                 self.updateSelectionArea()
                 if self.points[-2] != self.points[-1]:
                     self.points.append(dataPos)
+
                 return True
 
         def onMove(self, x, y):
+            dx = abs(self._firstPos[0] - x)
+            dy = abs(self._firstPos[1] - y)
+            if (dx < self.machine.DRAG_THRESHOLD_DIST and
+                    dy < self.machine.DRAG_THRESHOLD_DIST):
+                x, y = self._firstPos  # Snap to first point
+
             dataPos = self.machine.plot.pixelToData(x, y)
             assert dataPos is not None
             self.points[-1] = dataPos
             self.updateSelectionArea()
 
         def onPress(self, x, y, btn):
-            if btn == RIGHT_BTN:
+            if btn == LEFT_BTN:
+                dx = abs(self._firstPos[0] - x)
+                dy = abs(self._firstPos[1] - y)
+                if (dx < self.machine.DRAG_THRESHOLD_DIST and
+                        dy < self.machine.DRAG_THRESHOLD_DIST):
+                    self.machine.resetSelectionArea()
+
+                    dataPos = self.machine.plot.pixelToData(x, y)
+                    assert dataPos is not None
+                    self.points[-1] = dataPos
+                    if self.points[-2] == self.points[-1]:
+                        self.points.pop()
+
+                    self.points.append(self.points[0])
+
+                    eventDict = prepareDrawingSignal('drawingFinished',
+                                                     'polygon',
+                                                     self.points,
+                                                     self.machine.parameters)
+                    self.machine.plot.notify(**eventDict)
+                    self.goto('idle')
+
+            elif btn == RIGHT_BTN:
                 self.machine.resetSelectionArea()
 
                 dataPos = self.machine.plot.pixelToData(x, y)
