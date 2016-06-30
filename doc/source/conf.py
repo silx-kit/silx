8884570190e5a2481068d3fd2907e1e8c0f52094
Thomas Vincent <thomas.vincent@esrf.fr>
2016-06-30 14:58:07 +0200

    Use source order by default for autodoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,6 +36,8 @@ os.environ["PYTHONPATH"] = source_dir + os.pathsep + os.environ.get("PYTHONPATH"
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
 
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
