d7a9b13f0c3db1fa235d6c7be93daf370cec542f
Pierre <pierre.knobel@esrf.fr>
2016-07-05 16:19:24 +0200

    specfilewrapper (compatibility layer between old wrapper and io.specfile)

--- silx/io/specfilewrapper.py
+++ silx/io/specfilewrapper.py
@@ -22,8 +22,31 @@
 #
 #############################################################################*/
 """This module provides a to backward compatibility layer with previous
-specfile wrapper for :mod:`specfile`"""
+specfile wrapper for :mod:`specfile`.
 
+If you are starting a new project, please consider using :mod:`specfile`
+directly.
+
+If you want to use this module for an existing project that used the old
+wrapper, you can try replacing::
+
+    from PyMca5.PyMcaIO import specfilewrapper
+
+with::
+
+    from silx.io import specfilewrapper
+
+There are however differences between this module and the old
+wrapper, due to differences in the underlying implementation.
+
+Feel free to report any of these differences, if they are a problem for you, on
+https://github.com/silx-kit/silx/issues
+"""
+__authors__ = ["P. Knobel"]
+__license__ = "MIT"
+__date__ = "05/07/2016"
+
+import numpy
 from silx.io.specfile import SpecFile, Scan, MCA
 
 class Specfile(SpecFile):
@@ -40,8 +63,6 @@ class Specfile(SpecFile):
         """
         if not isinstance(key, int):
             raise TypeError("Scan index must be an integer")
-
-        return myscandata(scanobj=SpecFile.__getitem__(self, key))
 
         scan_index = key
         # allow negative index, like lists
@@ -64,7 +85,7 @@ class Specfile(SpecFile):
         msg = "Key must be a string 'N.M' with N being the scan"
         msg += " number and M the order (eg '2.3')."
 
-        if not hasattr(key, "lower") or not "." in key:
+        if not hasattr(key, "lower") or "." not in key:
             raise TypeError(msg)
 
         try:
@@ -103,5 +124,98 @@ class Specfile(SpecFile):
 
 
 class myscandata(Scan):
-    pass
+    def __init__(self, specfile, scan_index):
+        Scan.__init__(self, specfile, scan_index)
 
+        self._data = self._specfile.data(self._index)
+        self._mca = MCA(self)
+
+    def allmotorpos(self):
+        """Return a list of all motor positions (identical to
+        attr:`motor_positions`).
+
+        This method serves to maintain compatibility with the old specfile
+        wrapper API.
+        """
+        return self.motor_positions
+
+    def alllabels(self):
+        """
+        Return a list of all labels (:attr:`labels`).
+
+        This method serves to maintain compatibility with the old specfile
+        wrapper API.
+        """
+        return self.labels
+
+    def cols(self):
+        """Return the number of data columns (number of detectors)"""
+        return self.data.shape[1]
+
+    def command(self):
+        """Return the command called for this scan (``#S`` header line)"""
+        return self._specfile.command(self._index)
+
+    def data(self):
+        """Return a data column"""
+        return numpy.transpose(self._data)
+
+    def datacol(self, col):
+        """Return a data column
+
+        :param col: column number (1-based index)"""
+        return self._data[:, col - 1]
+
+    def dataline(self, line):
+        """Return a data column
+
+        :param line: line number (1-based index)"""
+        return self._data[line - 1, :]
+
+    def date(self):
+        """Return the date from the scan header line ``#D``"""
+        return self._specfile.command(self._index)
+
+    def fileheader(self, key=''):
+        """Return a list of file header lines"""
+        # key is there for compatibility
+        return self.file_header
+
+    def header(self, key):
+        """Return a list of scan header lines"""
+        if self.record_exists_in_hdr(key):
+            prefix = "#" + key + " "
+            if key in self.mca_header_dict:
+                prefix = "#@" + key + " "
+                return prefix + self.mca_header_dict[key]
+            elif key in self.scan_header_dict:
+                return prefix + self.scan_header_dict[key]
+            elif key in self.file_header_dict:
+                return prefix + self.file_header_dict[key]
+        elif self.record_exists_in_hdr("@" + key):
+            if key in self.mca_header_dict:
+                prefix = "#@" + key + " "
+                return prefix + self.mca_header_dict[key]
+        return ""
+
+    def lines(self):
+        """Return the number of data lines (number of data points per
+        detector)"""
+        return self._data.shape[0]
+
+    def mca(self, number):
+        """Return one MCA spectrum
+
+        :param number: MCA number (1-based index)"""
+        return self._mca[number - 1]
+
+    def nbmca(self):
+        """Return number of MCAs in this scan"""
+        return len(self.mca)
+
+
+        # FIXME:
+        # - mca is a method in the old implementation, not an attribute
+        # - the old API uses 1-based numbering rather than 0-based for mca(), datacol() and dataline()
+        # - data transposition?
+        # - header is an attribute containing all headers in the new implementation
