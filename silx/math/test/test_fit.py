913c8d697e301a12ad1bd0be1aa22e6f4945fc27
Pierre <pierre.knobel@esrf.fr>
2016-07-26 12:13:24 +0200

    Non regression test for PR #197

--- silx/math/test/test_fit.py
+++ silx/math/test/test_fit.py
@@ -249,74 +249,43 @@ class Test_leastsq(unittest.TestCase):
                                                           fittedpar[i])
                 self.assertTrue(test_condition, msg)
 
-    def testDataWithNaN(self):
-        parameters_actual = [10.5, 2, 1000.0, 20., 15]
-        x = numpy.arange(10000.).reshape(1000, 10)
+    def testUncertainties(self):
+        """Test for validity of uncertainties in returned full-output
+        dictionary. This is a non-regression test for pull request #197"""
+        parameters_actual = [10.5, 2, 1000.0, 20., 15, 2001.0, 30.1, 16]
+        x = numpy.arange(10000.)
         y = self.gauss(x, *parameters_actual)
-        sigma = numpy.sqrt(y)
-        parameters_estimate = [0.0, 1.0, 900.0, 25., 10]
-        model_function = self.gauss
-        x[500] = numpy.inf
-        # check default behavior
-        try:
-            self.instance(model_function, x, y,
-                          parameters_estimate,
-                          sigma=sigma)
-        except ValueError:
-            info = "%s" % sys.exc_info()[1]
-            self.assertTrue("array must not contain inf" in info)
+        parameters_estimate = [0.0, 1.0, 900.0, 25., 10., 1500., 20., 2.0]
 
-        # check requested behavior
-        try:
-            self.instance(model_function, x, y,
-                          parameters_estimate,
-                          sigma=sigma,
-                          check_finite=True)
-        except ValueError:
-            info = "%s" % sys.exc_info()[1]
-            self.assertTrue("array must not contain inf" in info)
+        # test that uncertainties are not 0.
+        fittedpar, cov, infodict = self.instance(self.gauss, x, y, parameters_estimate,
+                                                 full_output=True)
+        uncertainties = infodict["uncertainties"]
+        self.assertEqual(len(uncertainties), len(parameters_actual))
+        self.assertEqual(len(uncertainties), len(fittedpar))
+        for uncertainty in uncertainties:
+            self.assertNotAlmostEqual(uncertainty, 0.)
 
-        fittedpar, cov = self.instance(model_function, x, y,
-                                       parameters_estimate,
-                                       sigma=sigma,
-                                       check_finite=False)
-        test_condition = numpy.allclose(parameters_actual, fittedpar)
-        if not test_condition:
-            msg = "Unsuccessfull fit\n"
-            for i in range(len(fittedpar)):
-                msg += "Expected %g obtained %g\n" % (parameters_actual[i],
-                                                      fittedpar[i])
-            self.assertTrue(test_condition, msg)
-
-        # testing now with ydata containing NaN
-        x = numpy.arange(10000.).reshape(1000, 10)
-        y[500] = numpy.nan
-        fittedpar, cov = self.instance(model_function, x, y,
-                                       parameters_estimate,
-                                       sigma=sigma,
-                                       check_finite=False)
-
-        test_condition = numpy.allclose(parameters_actual, fittedpar)
-        if not test_condition:
-            msg = "Unsuccessfull fit\n"
-            for i in range(len(fittedpar)):
-                msg += "Expected %g obtained %g\n" % (parameters_actual[i],
-                                                      fittedpar[i])
-            self.assertTrue(test_condition, msg)
-
-        # testing now with sigma containing NaN
-        sigma[300] = numpy.nan
-        fittedpar, cov = self.instance(model_function, x, y,
-                                       parameters_estimate,
-                                       sigma=sigma,
-                                       check_finite=False)
-        test_condition = numpy.allclose(parameters_actual, fittedpar)
-        if not test_condition:
-            msg = "Unsuccessfull fit\n"
-            for i in range(len(fittedpar)):
-                msg += "Expected %g obtained %g\n" % (parameters_actual[i],
-                                                      fittedpar[i])
-            self.assertTrue(test_condition, msg)
+        # set constraint FIXED for half the parameters.
+        # This should cause leastsq to return 100% uncertainty.
+        parameters_estimate = [10.6, 2.1, 1000.1, 20.1, 15.1, 2001.1, 30.2, 16.1]
+        CFIXED = 3
+        CFREE = 0
+        constraints = []
+        for i in range(len(parameters_estimate)):
+            if i % 2:
+                constraints.append([CFIXED, 0, 0])
+            else:
+                constraints.append([CFREE, 0, 0])
+        fittedpar, cov, infodict = self.instance(self.gauss, x, y, parameters_estimate,
+                                                 constraints=constraints,
+                                                 full_output=True)
+        uncertainties = infodict["uncertainties"]
+        for i in range(len(parameters_estimate)):
+            if i % 2:
+                # test that all FIXED parameters have 100% uncertainty
+                self.assertAlmostEqual(uncertainties[i],
+                                       parameters_estimate[i])
 
 
 test_cases = (Test_leastsq,)
