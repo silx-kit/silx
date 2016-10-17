fc81e1b1828df03a9bc7afb7f2332a5c3040ee27
Pierre <pierre.knobel@esrf.fr>
2016-10-17 15:46:43 +0200

    Refactoring:
    - ensure internal _array is at least 2-D
    - remove specific methods for 1-D and 2-D arrays, they can be handled by n-D methods
    - rename attribute _dimension -> _perspective, define it in __init__
    - use only one set of methods for 3D arrays, use attribute _perspective to set the data sample index
    - make _index always be a list, even when it is of length 0 or 1 (2D or 3D array)

--- silx/gui/array/ArrayTableModel.py
+++ silx/gui/array/ArrayTableModel.py
@@ -31,61 +31,72 @@ __license__ = "MIT"
 __date__ = "17/10/2016"
 
 
+# TODO: generalise perspective to n-D arrays
 class NumpyArrayTableModel(qt.QAbstractTableModel):
-    """This data model provides access to a given 2D slice in a N-dimensional
+    """This data model provides access to 2D slices in a N-dimensional
     array.
 
+    A slice for a 3-D array is characterized by a perspective (the number of
+    the axis orthogonal to the slice) and an index at which the slice
+    intersects the orthogonal axis.
+
+    In the n-D case, only slices parallel to the last two axes are handled. A
+    slice is therefore characterized by a list of indices locating the
+    slice on all the :math:`n - 2` orthogonal axes.
+
     :param parent: Parent QObject
-    :param narray: Numpy array
+    :param data: Numpy array
     :param str fmt: Format string for representing numerical values.
         Default is ``"%g"``.
-    :param int perspective:
+    :param int perspective: In the 3-D case, this parameter is the number of
+        the axis orthogonal to the data slice. If the unit vectors describing
+        your axes are :math:`\vec{x}, \vec{y}, \vec{z}`, a perspective of 0
+        means you slices are parallel to :math:`\vec{y}\vec{z}`, 1 means they
+        are parallel to :math:`\vec{x}\vec{z}` and 2 means they
+        are parallel to :math:`\vec{x}\vec{y}`.
+        In all other cases (1-D, 2-D, n-D), this parameter is ignored.
     """
-    def __init__(self, parent=None, narray=None, fmt="%g", perspective=0):
+    def __init__(self, parent=None, data=None, fmt="%g", perspective=0):
         qt.QAbstractTableModel.__init__(self, parent)
-        if narray is None:
-            narray = numpy.array([])
-        self._array = narray
+
+        # make sure data is an array (not a scalar, list, or list of lists)
+        if data is None:
+            data = numpy.array([])
+        elif not isinstance(data, numpy.ndarray):
+            data = numpy.array(data)
+        # ensure data is at least 2-dimensional
+        if len(data.shape) < 1:
+            data.shape = (1, 1)
+        elif len(data.shape) < 2:
+            data.shape = (1, data.shape[0])
+
+        self._array = data
+        """n-dimensional numpy array"""
+
         self._format = fmt
-        self._index = 0
-        self.assignDataFunction(perspective)
+        """Format string (default "%g")"""
 
-    def rowCount(self, parent_idx=None):
-        return self._rowCount(parent_idx)
+        self._index = [0] * (len(data.shape) - 2)
+        """This attribute stores the slice index. In case _array is a 3D
+        array, it is an integer.  If the number of dimensions is greater
+        than 3, it is a list of indices."""
 
-    def columnCount(self, parent_idx=None):
-        return self._columnCount(parent_idx)
+        self._perspective = perspective
+        """Axis number orthogonal to the 2D-slice, for a 3-D array"""
 
-    def data(self, index, role=qt.Qt.DisplayRole):
-        return self._data(index, role)
+        self._assignDataFunction()
 
-    def _rowCount1D(self, parent_idx=None):
-        return 1
-
-    def _columnCount1D(self, parent_idx=None):
-        return self._array.shape[0]
-
-    def _data1D(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                # row = 0
-                col = index.column()
-                return self._format % self._array[col]
-        return None
-
-    def _rowCount2D(self, parent_idx=None):
-        return self._array.shape[0]
-
-    def _columnCount2D(self, parent_idx=None):
-        return self._array.shape[1]
-
-    def _data2D(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                row = index.row()
-                col = index.column()
-                return self._format % self._array[row, col]
-        return None
+    def _assignDataFunction(self):
+        shape = self._array.shape
+        if len(shape) == 3:
+            self._rowCount = self._rowCount3D
+            self._columnCount = self._columnCount3D
+            self._data = self._data3D
+        else:
+            # only C order array of images supported
+            self._rowCount = self._rowCountND
+            self._columnCount = self._columnCountND
+            self._data = self._dataND
 
     def _rowCountND(self, parent_idx=None):
         return self._array.shape[-2]
@@ -94,128 +105,116 @@ class NumpyArrayTableModel(qt.QAbstractTableModel):
         return self._array.shape[-1]
 
     def _dataND(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                row = index.row()
-                col = index.column()
-                actualSelection = tuple(self._index + [row, col])
-                return self._format % self._array[actualSelection]
+        if index.isValid() and role == qt.Qt.DisplayRole:
+            row = index.row()
+            col = index.column()
+            selection = tuple(self._index + [row, col])
+            return self._format % self._array[selection]
         return None
 
-    def _rowCount3DIndex0(self, parent_idx=None):
-        return self._array.shape[1]
-
-    def _columnCount3DIndex0(self, parent_idx=None):
-        return self._array.shape[2]
-
-    def _rowCount3DIndex1(self, parent_idx=None):
+    def _rowCount3D(self, parent_idx=None):
+        """Return number of rows in the slice. This is the length of the
+        first axis parallel to the slice (x or y)."""
+        if self._perspective == 0:
+            return self._array.shape[1]
         return self._array.shape[0]
 
-    def _columnCount3DIndex1(self, parent_idx=None):
+    def _columnCount3D(self, parent_idx=None):
+        """Return number of columns in the slice. This is the length of the
+        last axis parallel to the slice (y or z)."""
+        if self._perspective == 2:
+            return self._array.shape[1]
         return self._array.shape[2]
 
-    def _rowCount3DIndex2(self, parent_idx=None):
-        return self._array.shape[0]
-
-    def _columnCount3DIndex2(self, parent_idx=None):
-        return self._array.shape[1]
-
-    def _data3DIndex0(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                row = index.row()
-                col = index.column()
-                return self._format % self._array[self._index, row, col]
+    def _data3D(self, index, role=qt.Qt.DisplayRole):
+        if index.isValid() and role == qt.Qt.DisplayRole:
+            row = index.row()
+            col = index.column()
+            if self._perspective == 0:
+                selection = tuple(self._index + [row, col])
+            elif self._perspective == 1:
+                selection = (row, self._index[0], col)
+            else:
+                selection = tuple([row, col] + self._index)
+            return self._format % self._array[selection]
         return None
 
-    def _data3DIndex1(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                row = index.row()
-                col = index.column()
-                return self._format % self._array[row, self._index, col]
-        return None
+    # Methods to be implemented to subclass QAbstractTableModel
+    def rowCount(self, parent_idx=None):
+        """Return the number of rows (first index) in the data slice"""
+        return self._rowCount(parent_idx)
 
-    def _data3DIndex2(self, index, role=qt.Qt.DisplayRole):
-        if index.isValid():
-            if role == qt.Qt.DisplayRole:
-                row = index.row()
-                col = index.column()
-                return self._format % self._array[row, col, self._index]
-        return None
+    def columnCount(self, parent_idx=None):
+        """Return the number of columns (second index) in the data slice"""
+        return self._columnCount(parent_idx)
 
-    def setArrayData(self, data, perspective=0):
+    def data(self, index, role=qt.Qt.DisplayRole):
+        """Return the string representation of the value at the specified
+        index in the data slice.
         """
-        setStackData(self, data, perspective=0)
-        data is a 3D array
-        dimension is the array dimension acting as index of images
+        return self._data(index, role)
+
+    # Public methods
+    def setArrayData(self, data, perspective=0):
+        """Set the data array
+
+        :param data: Numpy array
+        :param int perspective: For a 3-D array, the array dimension acting
+            as index of images, orthogonal to the image
         """
         if qt.qVersion() > "4.6":
             self.beginResetModel()
         else:
             self.reset()
+
+        # ensure data is a numpy array
+        if data is None:
+            data = numpy.array([])
+        elif not isinstance(data, numpy.ndarray):
+            data = numpy.array(data)
+        # ensure data is at least 2-dimensional
+        if len(data.shape) < 1:
+            data.shape = (1, 1)
+        elif len(data.shape) < 2:
+            data.shape = (1, data.shape[0])
+
         self._array = data
-        self.assignDataFunction(perspective)
-        if len(data.shape) > 3:
-            self._index = []
-            for i in range(len(data.shape) - 2):
-                self._index.append(0)
+        self._index = [0] * (len(data.shape) - 2)
+        self._perspective = perspective
+        self._assignDataFunction()
+
         if qt.qVersion() > "4.6":
             self.endResetModel()
 
-    def assignDataFunction(self, dimension):
-        shape = self._array.shape
-        if len(shape) == 2:
-            self._rowCount = self._rowCount2D
-            self._columnCount = self._columnCount2D
-            self._data = self._data2D
-        elif len(shape) == 1:
-            self._rowCount = self._rowCount1D
-            self._columnCount = self._columnCount1D
-            self._data = self._data1D
-        elif len(shape) > 3:
-            # only C order array of images supported
-            self._rowCount = self._rowCountND
-            self._columnCount = self._columnCountND
-            self._data = self._dataND
-        else:
-            if dimension == 1:
-                self._rowCount = self._rowCount3DIndex1
-                self._columnCount = self._columnCount3DIndex1
-                self._data = self._data3DIndex1
-            elif dimension == 2:
-                self._rowCount = self._rowCount3DIndex2
-                self._columnCount = self._columnCount3DIndex2
-                self._data = self._data3DIndex1
-            else:
-                self._rowCount = self._rowCount3DIndex0
-                self._columnCount = self._columnCount3DIndex0
-                self._data = self._data3DIndex0
-            self._dimension = dimension
-
     def setCurrentArrayIndex(self, index):
-        """
-        This method is ignored if the current array does not
-        not a 3-dimensional array.
+        """Set the active slice index.
+
+        This method is only relevant to arrays with at least 3 dimensions.
+
+        :param sequence index: Index of the active slice in the array.
+            In the general n-D case, this is a sequence of :math:`n - 2`
+            indices where the slice intersects the orthogonal axes.
+        :raise IndexError: If any index in the index sequence is out of bound
+            on its respective axis.
         """
         shape = self._array.shape
         if len(shape) < 3:
             # index is ignored
             return
         if len(shape) == 3:
-            shape = self._array.shape[self._dimension]
-            if hasattr(index, "__len__"):
-                index = index[0]
-            if (index < 0) or (index >= shape):
-                raise ValueError("Index must be an integer lower than %d" % shape)
-            self._index = index
+            len_ = shape[self._perspective]
+            # accept integers as index in the case of 3-D arrays
+            if not hasattr(index, "__len__"):
+                self._index = [index]
+            if not 0 <= self._index[0] < len_:
+                raise ValueError("Index must be a positive integer " +
+                                 "lower than %d" % len_)
         else:
-            # Only N-dimensional arrays of images supported
-            for i in range(len(index)):
-                idx = index[i]
-                if (idx < 0) or (idx >= shape[i]):
-                    raise ValueError("Index %d must be positive integer lower than %d" % \
-                                     (idx, shape[i]))
+            # general n-D case
+            for i, idx in enumerate(index):
+                if not 0 <= idx < shape[i]:
+                    raise IndexError("Index %d must be a positive " % idx +
+                                     "integer lower than %d" % shape[i])
             self._index = index
 
     def setFormat(self, fmt):
@@ -227,12 +226,12 @@ if __name__ == "__main__":
     d = numpy.random.normal(0,1, (5, 1000,1000))
     for i in range(5):
         d[i, :, :] += i
-    #m = NumpyArrayTableModel(fmt="%.5f")
-    #m = NumpyArrayTableModel(None, numpy.arange(100.), fmt="%.5f")
-    #m = NumpyArrayTableModel(None, numpy.ones((100,20)), fmt="%.5f")
-    m = NumpyArrayTableModel(None, d, fmt = "%.5f")
+    # m = NumpyArrayTableModel(fmt="%.5f")
+    # m = NumpyArrayTableModel(None, numpy.arange(100.), fmt="%.5f")
+    # m = NumpyArrayTableModel(None, numpy.ones((100,20)), fmt="%.5f")
+    m = NumpyArrayTableModel(data=d, fmt="%.5f")
     w.setModel(m)
     m.setCurrentArrayIndex(4)
-    #m.setArrayData(numpy.ones((100,)))
+    # m.setArrayData(numpy.ones((100,)))
     w.show()
     a.exec_()
