2b38fcfec880a0b37246d59997d0e6951e809c1a
Pierre <pierre.knobel@esrf.fr>
2016-10-24 16:12:12 +0200

    More tests, for h5py dataset and numpy array.
    Flag test for read-only dataset is still failing

--- silx/gui/widgets/test/test_arraywidget.py
+++ silx/gui/widgets/test/test_arraywidget.py
@@ -28,35 +28,127 @@ __date__ = "13/10/2016"
 
 import unittest
 import numpy
+import tempfile
+import os
 
 from .. import ArrayTableWidget
 from ...testutils import TestCaseQt
+from silx.gui import qt
+
+try:
+    import h5py
+except ImportError:
+    h5py = None
 
 
-class TestArrayWidget(TestCaseQt):
-    """Basic test for ArrayTableWidget"""
+class TestNumpyArrayWidget(TestCaseQt):
+    """Basic test for ArrayTableWidget with a numpy array"""
     def setUp(self):
-        super(TestArrayWidget, self).setUp()
+        super(TestNumpyArrayWidget, self).setUp()
         self.aw = ArrayTableWidget.ArrayTableWidget()
 
     def tearDown(self):
         del self.aw
-        super(TestArrayWidget, self).tearDown()
+        super(TestNumpyArrayWidget, self).tearDown()
+
+    def testShow(self):
+        """test for errors"""
+        self.aw.show()
+        self.qWaitForWindowExposed(self.aw)
+
+    def testSetData0D(self):
+        """test for errors"""
+        a = 1
+        self.aw.setArrayData(a)
+        b = self.aw.getData(copy=True)
+
+        self.assertTrue(numpy.array_equal(a, b))
+
+    def testSetData1D(self):
+        """test for errors"""
+        a = [1, 2]
+        self.aw.setArrayData(a)
+        b = self.aw.getData(copy=True)
+
+        self.assertTrue(numpy.array_equal(a, b))
+
+    def testSetData4D(self):
+        """test for errors"""
+        a = numpy.reshape(numpy.linspace(0.213, 1.234, 10000),
+                          (10, 10, 10, 10))
+        self.aw.setArrayData(a)
+        self.aw.setPerspective((1, 3))
+        b = self.aw.getData(copy=True)
+
+        self.assertTrue(numpy.array_equal(a, b))
+
+    def testFlagEditable(self):
+        self.aw.setArrayData([[0]])
+        idx = self.aw.model.createIndex(0, 0)
+        # model is editable
+        self.assertTrue(
+                self.aw.model.flags(idx) & qt.Qt.ItemIsEditable)
+
+
+@unittest.skipIf(h5py is None, "Could not import h5py")
+class TestH5pyArrayWidget(TestCaseQt):
+    """Basic test for ArrayTableWidget with a dataset.
+
+    Test flags, for dataset open in read-only or read-write modes"""
+    def setUp(self):
+        super(TestH5pyArrayWidget, self).setUp()
+        self.aw = ArrayTableWidget.ArrayTableWidget()
+        self.data = numpy.reshape(numpy.linspace(0.213, 1.234, 10000),
+                                  (10, 10, 10, 10))
+        # create an h5py file with a dataset
+        self.tempdir = tempfile.mkdtemp()
+        self.h5_fname = os.path.join(self.tempdir, "array.h5")
+        h5f = h5py.File(self.h5_fname)
+        h5f["my_array"] = self.data
+        h5f.close()
+
+    def tearDown(self):
+        del self.aw
+        os.unlink(self.h5_fname)
+        os.rmdir(self.tempdir)
+        super(TestH5pyArrayWidget, self).tearDown()
 
     def testShow(self):
         self.aw.show()
         self.qWaitForWindowExposed(self.aw)
 
-    def testSetData(self):
-        a = numpy.reshape(numpy.linspace(0.213, 1.234, 10000),
-                          (10, 10, 10, 10))
+    def _readAndSetData(self, mode):
+        h5f = h5py.File(self.h5_fname, mode)
+        a = h5f["my_array"]
         self.aw.setArrayData(a)
+
+    def testReadOnly(self):
+        self._readAndSetData(mode="r")
+        b = self.aw.getData(copy=True)
+        self.assertTrue(numpy.array_equal(self.data, b))
+
+        # model must not be editable
+        idx = self.aw.model.createIndex(0, 0)
+        self.assertFalse(
+                self.aw.model.flags(idx) & qt.Qt.ItemIsEditable)
+
+    def testReadWrite(self):
+        self._readAndSetData(mode="r+")
+        b = self.aw.getData(copy=True)
+        self.assertTrue(numpy.array_equal(self.data, b))
+
+        idx = self.aw.model.createIndex(0, 0)
+        # model is editable
+        self.assertTrue(
+                self.aw.model.flags(idx) & qt.Qt.ItemIsEditable)
 
 
 def suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest(
-        unittest.defaultTestLoader.loadTestsFromTestCase(TestArrayWidget))
+        unittest.defaultTestLoader.loadTestsFromTestCase(TestNumpyArrayWidget))
+    test_suite.addTest(
+        unittest.defaultTestLoader.loadTestsFromTestCase(TestH5pyArrayWidget))
     return test_suite
 
 
