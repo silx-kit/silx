06d065193f6845f6711d77a78d8b577569392cb2
Pierre <pierre.knobel@esrf.fr>
2016-12-09 09:52:10 +0100

    Add proxy methods for plot API, change resetzoom bhavior
     - implemented: clear, resetZoom, getGraphTitle, setGraphTitle, getGraphXLabel, setGraphXLabel,  getGraphYLabel, setGraphYLabel, setYAxisInverted, isYAxisInverted, getSupportedColormaps, getColormap, setColormap, isKeepDataAspectRatio, setKeepDataAspectRatio
     - resetZoom is now applied only when perspective is changed, but not when frames are browsed
     - PEP-8
     - made most internal attributes private (__), only _volume and the widgets are protected (_)

--- silx/gui/plot/StackView.py
+++ silx/gui/plot/StackView.py
@@ -47,36 +47,12 @@ __date__ = "07/12/2016"
 
 import numpy
 
-from silx.gui import qt, icons
-from silx.gui.plot import PlotWindow, PlotActions   # ??? or PlotWindow, to redefine ProfileToolBar
+from silx.gui import qt
+from silx.gui.plot import PlotWindow, PlotActions
 from silx.gui.plot.Colors import cursorColorForColormap
 from silx.gui.widgets.FrameBrowser import HorizontalSliderWithBrowser
 from silx.gui.plot.PlotTools import ProfileToolBar
 
-
-
-# TODO: Methods to be referenced from plot
-#  - getSupportedColormaps
-#  - clear
-#  - getGraphTitle() / setGraphTitle(title='')
-#  - getGraphXLabel() / setGraphXLabel(label='X')
-#  - getGraphYLabel(axis='left') / setGraphYLabel(label='Y', axis='left') (remove axis param?)
-#  - setYAxisInverted(flag=True) / isYAxisInverted()
-#  - isXAxisLogarithmic()/setXAxisLogarithmic(flag)    ??????????????????
-#  - isKeepDataAspectRatio() / setKeepDataAspectRatio(flag=True)   ???????????
-#  - resetZoom() ?????
-
-
-# TODO: Methods to be implemented
-#  - setColormap (see ImageView) DONE
-#  - getColormap (see ImageView) DONE
-#  - setVolume                   IN PROGRESS (perspective to be implemented)
-#  - getVolumeShape (or getVolumeLimits?)
-#  - setPerspective (or setSlicePlane? or setXYAxes?)
-
-# TODO:
-#  - reimplement autoscale feature in setColormap to use min and max
-#    in the complete volume, not just vmin/vmax of the 2D slice
 
 class StackView(qt.QWidget):
     """Simple stack view 
@@ -85,12 +61,18 @@ class StackView(qt.QWidget):
     """
     def __init__(self, parent=None):
         qt.QWidget.__init__(self, parent)
-
-        self._imageLegend = '__StackView__image' + str(id(self))
         self._volume = None
-        self._volumeview = None
-        self._autoscaleCmap = True
-        self.__perspective = 0 # dimension browse
+        """Loaded data volume, as a numpy array"""
+        self.__volumeview = None
+        """View on :attr:`_volume` with the axes sorted, to have
+        the orthogonal dimension first"""
+        self.__perspective = 0
+        """Orthogonal dimension (depth) in :attr:`_volume`"""
+
+        self.__imageLegend = '__StackView__image' + str(id(self))
+        self.__autoscaleCmap = True
+        """Flag to disable/enable colormap autoscaling
+        based on the 3D volume"""
 
         self._plot = PlotWindow(parent=self, backend=None,
                                 resetzoom=True, autoScale=False,
@@ -101,13 +83,16 @@ class StackView(qt.QWidget):
                                 control=False, position=None,
                                 roi=False, mask=False)
 
-        self._plot.profile = Profile3DToolBar(parent=self._plot, plot=self._plot, volume=self._volumeview)
+        self._plot.profile = Profile3DToolBar(parent=self._plot,
+                                              plot=self._plot,
+                                              volume=self.__volumeview)
         self._plot.addToolBar(self._plot.profile)
         self._plot.setGraphXLabel('Columns')
         self._plot.setGraphYLabel('Rows')
 
         self._browser = HorizontalSliderWithBrowser(self)
         self._browser.valueChanged[int].connect(self.__updateFrameNumber)
+        self._browser.setEnabled(False)
 
         layout = qt.QVBoxLayout(self)
 
@@ -116,7 +101,6 @@ class StackView(qt.QWidget):
         self._plot._introduceNewDockWidget(planeSelction)
         layout.addWidget(self._plot)
         layout.addWidget(self._browser)
-
 
     def __setPerspective(self, perspective):
         """Function called when the dimension browse changes
@@ -129,43 +113,44 @@ class StackView(qt.QWidget):
             if perspective > 2 or perspective < 0:
                 raise ValueError('Can\'t set persepective')
 
-            self.__perspective=perspective
+            self.__perspective = perspective
             self.__createVolumeView()
-            self.__updateFrameNumber(self._browser.value()) 
-
+            self.__updateFrameNumber(self._browser.value())
+            self._plot.resetZoom()
 
     def __createVolumeView(self):
-        """Create the new view on the volume depending on the perspective (axis browse on the viewer)
+        """Create the new view on the volume depending on the perspective
+        (set orthogonal axis browsed on the viewer as first dimension)
         """
-        assert(not self._volume is None)
-        assert(self.__perspective >=0 and self.__perspective < 3)
+        assert not self._volume is None
+        assert 0 <= self.__perspective < 3
         if self.__perspective == 0:
-            self._volumeview = self._volume.view()
+            self.__volumeview = self._volume.view()
         if self.__perspective == 1:
-            self._volumeview = numpy.rollaxis(self._volume, 1)
+            self.__volumeview = numpy.rollaxis(self._volume, 1)
         if self.__perspective == 2:
-            self._volumeview = numpy.rollaxis(self._volume, 2)
+            self.__volumeview = numpy.rollaxis(self._volume, 2)
 
-        self._browser.setRange(0, self._volumeview.shape[0]-1)            
+        self._browser.setRange(0, self.__volumeview.shape[0] - 1)
 
-        self._plot.profile.updateVolume(self._volumeview)
-
+        self._plot.profile.updateVolume(self.__volumeview)
 
     def __updateFrameNumber(self, index):
         """Update the current image displayed
 
         :param index: index of the image to display
         """
-        assert(not self._volumeview is None)
-        self._plot.addImage(self._volumeview[index, :, :], legend=self._imageLegend)
-
+        assert self.__volumeview is not None
+        self._plot.addImage(self.__volumeview[index, :, :],
+                            legend=self.__imageLegend,
+                            resetzoom=False)
 
     # public API
     def setVolume(self, volume, origin=(0, 0), scale=(1., 1.),
                   copy=True, reset=True):
         """Set the stack of images to display.
 
-        :param volume: A 3D array representing the image or None to empty plot.
+        :param volume: A 3D array representing the image or None to clear plot.
         :type volume: numpy.ndarray-like with 3 dimensions or None.
         :param origin: The (x, y) position of the origin of the image.
                        Default: (0, 0).
@@ -186,9 +171,7 @@ class StackView(qt.QWidget):
         assert scale[1] > 0
 
         if volume is None:
-            self._plot.remove(self._imageLegend, kind='image')
-            self._volume = None
-            self._browser.setEnabled(False)
+            self.clear()
             return
 
         data = numpy.array(volume, order='C', copy=copy)
@@ -200,17 +183,16 @@ class StackView(qt.QWidget):
 
         # This call to setColormap takes redefines the meaning of autoscale
         # for 3D volume: take global min/max rather than frame min/max
-        if self._autoscaleCmap:
+        if self.__autoscaleCmap:
             self.setColormap(autoscale=True)
 
         # init plot
-        self._plot.addImage(self._volumeview[0, :, :], 
-                            legend=self._imageLegend,
+        self._plot.addImage(self.__volumeview[0, :, :],
+                            legend=self.__imageLegend,
                             origin=origin, scale=scale,
-                            colormap=self.getColormap(),
-                            replace=False)
+                            colormap=self.getColormap())
 
-        self._plot.setActiveImage(self._imageLegend)
+        self._plot.setActiveImage(self.__imageLegend)
 
         if reset:
             self._plot.resetZoom()
@@ -218,6 +200,79 @@ class StackView(qt.QWidget):
         # enable and init browser
         self._browser.setEnabled(True)
 
+    def clear(self):
+        """Clear the widget:
+
+         - clear the plot
+         - clear the loaded data volume
+        """
+        self._volume = None
+        self.__volumeview = None
+        self.__perspective = 0
+        self._browser.setEnabled(False)
+        self._plot.clear()
+
+    def resetZoom(self):
+        """Reset the plot limits to the bounds of the data and redraw the plot.
+        """
+        self._plot.resetZoom()
+
+    def getGraphTitle(self):
+        """Return the plot main title as a str."""
+        return self._plot.getGraphTitle()
+
+    def setGraphTitle(self, title=""):
+        """Set the plot main title.
+
+        :param str title: Main title of the plot (default: '')
+        """
+        return self._plot.setGraphTitle(title)
+
+    def getGraphXLabel(self):
+        """Return the current X axis label as a str."""
+        return self._plot.getGraphXLabel()
+
+    def setGraphXLabel(self, label="X"):
+        """Set the plot X axis label.
+
+        :param str label: The X axis label (default: 'X')
+        """
+        self._plot.setGraphXLabel(label)
+
+    def getGraphYLabel(self, axis='left'):
+        """Return the current Y axis label as a str.
+
+        :param str axis: The Y axis for which to get the label (left or right)
+        """
+        return self._plot.getGraphYLabel(axis)
+
+    def setGraphYLabel(self, label="Y", axis='left'):
+        """Set the plot Y axis label.
+
+        :param str label: The Y axis label (default: 'Y')
+        :param str axis: The Y axis for which to set the label (left or right)
+        """
+        self._plot.setGraphYLabel(label, axis)
+
+    def setYAxisInverted(self, flag=True):
+        """Set the Y axis orientation.
+
+        :param bool flag: True for Y axis going from top to bottom,
+                          False for Y axis going from bottom to top
+        """
+        self._plot.setYAxisInverted(flag)
+
+    def isYAxisInverted(self):
+        """Return True if Y axis goes from top to bottom, False otherwise."""
+        return self._backend.isYAxisInverted()
+
+    def getSupportedColormaps(self):
+        """Get the supported colormap names as a tuple of str.
+
+        The list should at least contain and start by:
+        ('gray', 'reversed gray', 'temperature', 'red', 'green', 'blue')
+        """
+        return self._plot.getSupportedColormaps()
 
     def getColormap(self):
         """Get the current colormap description.
@@ -228,7 +283,6 @@ class StackView(qt.QWidget):
         """
         # default colormap used by addImage
         return self._plot.getDefaultColormap()
-
 
     def setColormap(self, colormap=None, normalization=None,
                     autoscale=None, vmin=None, vmax=None, colors=None):
@@ -292,8 +346,8 @@ class StackView(qt.QWidget):
             # each time a new image is added to the plot.
             # Here, we want to use min and max of global volume,
             # and not change them when browsing slides
-            cmapDict['autoscale'] = False         # autoscale in the addImage meaning
-            self._autoscaleCmap = autoscale       # our autoscale attribute
+            cmapDict['autoscale'] = False
+            self.__autoscaleCmap = autoscale
             if autoscale and (self._volume is not None):
                 cmapDict['vmin'] = self._volume.min()
                 cmapDict['vmax'] = self._volume.max()
@@ -312,10 +366,20 @@ class StackView(qt.QWidget):
         activeImage = self._plot.getActiveImage()
         if activeImage is not None:
             data, legend, info, _pixmap = activeImage[0:4]
-
             self._plot.addImage(data, legend=legend, info=info,
                                 colormap=self.getColormap(),
-                                replace=False)
+                                resetzoom=False)
+
+    def isKeepDataAspectRatio(self):
+        """Returns whether the plot is keeping data aspect ratio or not."""
+        return self._plot.isKeepDataAspectRatio()
+
+    def setKeepDataAspectRatio(self, flag=True):
+        """Set whether the plot keeps data aspect ratio or not.
+
+        :param bool flag: True to respect data aspect ratio
+        """
+        self._plot.setKeepDataAspectRatio(flag)
 
 
 class DockPlanes(qt.QDockWidget):
@@ -356,12 +420,10 @@ class DockPlanes(qt.QDockWidget):
 
         self.setFeatures(qt.QDockWidget.DockWidgetMovable)
 
-
     def __planeSelectionChanged(self):
         """Callback function when the radio button change
         """
         self.sigPlaneSelectionChanged.emit(self.getPlaneSelected())
-
 
     def getPlaneSelected(self):
         """Return the plane selected following the convention : 
@@ -390,13 +452,13 @@ class Profile3DToolBar(ProfileToolBar):
                               display the profile curve or None to create one.
         :param str title: See :class:`QToolBar`.
         :param parent: See :class:`QToolBar`.
-        :param volume: the 3D volume. Always compute the profile across the first axis
+        :param volume: the 3D volume. Always compute the profile across
+            the first axis
         """
         super(Profile3DToolBar, self).__init__(parent, plot, profileWindow, title)
         self._volume = volume
         self.profil3DAction = self.__create3DProfilAction(volume)
         self._setComputeIn3D(False)
-
 
     def __create3DProfilAction(self, volume):
         """Initialize the Profile3DAction action 
@@ -404,7 +466,6 @@ class Profile3DToolBar(ProfileToolBar):
         self.profile3d = Profile3DAction(plot=self.plot, parent=self.plot)
         self.profile3d.sigChange3DProfile.connect(self._setComputeIn3D)
         self.addAction(self.profile3d)
-
 
     def updateVolume(self, volume):
         """Update the volume browse
@@ -415,7 +476,6 @@ class Profile3DToolBar(ProfileToolBar):
         """
         self._volume = volume
 
-
     def _setComputeIn3D(self, b):
         """Set if we want to compute the profile in 2D or in 3D
 
@@ -423,7 +483,6 @@ class Profile3DToolBar(ProfileToolBar):
         """
         self._computeIn3D = b
         self.updateProfile()
-
 
     def updateProfile(self):
         """Redefine the one from :class:`ProfileToolBar`
@@ -438,7 +497,7 @@ class Profile3DToolBar(ProfileToolBar):
             self.profileWindow.setGraphYLabel('Y')
 
             # TODO : should we add a different color gradation relative to slices ?
-            data=self.plot.getActiveImage()
+            data = self.plot.getActiveImage()
             super(Profile3DToolBar, self)._createProfile(currentData=self._volume[0, :, :], 
                                                          params=self.plot.getActiveImage()[4],
                                                          volume=self._volume)
@@ -458,14 +517,14 @@ class Profile3DAction(PlotActions.PlotAction):
     """
     sigChange3DProfile = qt.Signal(bool)
     def __init__(self, plot, parent=None):
-        super(Profile3DAction, self).__init__( plot=plot, 
-                                               icon='cube', 
-                                               text='3D profile',
-                                               tooltip='If activated, will compute the profile on the dimension browsed',
-                                               triggered=self.__compute3DProfile,
-                                               checkable=True,
-                                               parent=parent)
-
+        super(Profile3DAction, self).__init__(
+                plot=plot,
+                icon='cube',
+                text='3D profile',
+                tooltip='If activated, will compute the profile on the dimension browsed',
+                triggered=self.__compute3DProfile,
+                checkable=True,
+                parent=parent)
 
     def __compute3DProfile(self):
         """Callback when the QAction is activated
