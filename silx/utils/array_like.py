99caab7819f628685a5cee7ca58fcc23e2342077
Pierre <pierre.knobel@esrf.fr>
2016-12-14 16:12:31 +0100

    new TransposedDatasetView, to transpose data without numpy casting

--- silx/utils/array_like.py
+++ silx/utils/array_like.py
@@ -34,7 +34,6 @@ __license__ = "MIT"
 __date__ = "13/12/2016"
 
 
-
 def is_array(obj):
     """Return True if object implements necessary attributes to be
     considered similar to a numpy array.
@@ -136,57 +135,184 @@ def get_array_type(array_like):
 
     return numpy.dtype(type(subsequence))
 
+#
+# class ArrayLike(object):
+#     """
+#
+#     :param array_like: Array, dataset or nested sequence.
+#          Nested sequences must be rectangular and of homogeneous type.
+#     """
+#     def __init__(self, array_like):
+#         """
+#
+#         """
+#         super(ArrayLike, self).__init__()
+#         self.array_like = array_like
+#         """original object"""
+#         self._cache = None
+#         """data as a numpy array (created when/if needed)"""
+#
+#         if is_array(array_like):
+#             self.shape = array_like.shape
+#             self.dtype = array_like.dtype
+#         elif is_nested_sequence(array_like):
+#             self.shape = get_shape(array_like)
+#             self.dtype = get_dtype(array_like)
+#
+#         self.array_type = get_array_type(array_like)
+#
+#     @property
+#     def cached_array(self):
+#         """data as a numpy array (created when/if needed)"""
+#         if self._cache is None:
+#             self._cache = numpy.asarray(self.array_like)
+#         return self._cache
+#
+#     def __len__(self):
+#         return len(self.array_like)
+#
+#     def __getitem__(self, item):
+#         """Implement slicing and fancy indexing for sequences"""
+#         # arrays and datasets already support slicing and fancy indexing
+#         if self.array_type in ["numpy array", "h5py dataset"]:
+#             return self.array_like[item]
+#
+#         # From now on, we assume array_like is a nested sequence.
+#         # Regular int indexing or simple slice
+#         if isinstance(item, (int, slice)):
+#             return self.array_like[item]
+#
+#         # multidimensional/fancy slicing: numpy array needed
+#         if hasattr(item, "__len__"):
+#             #
+#             return self.cached_array[item]
+#             # TODO: implement nD slicing without array casting
 
-class ArrayLike(object):
-    """
 
-    :param array_like: Array, dataset or nested sequence.
-         Nested sequences must be rectangular and of homogeneous type.
+class TransposedDatasetView(object):
     """
-    def __init__(self, array_like):
+    This class provides a way to transpose a dataset without
+    casting it into a numpy array. This way, the dataset in a file need not
+    necessarily be integrally read into memory to access data slices.
+
+    The read performance depends a lot on the way the dataset was written
+    to file. Depending on the chunking strategy, reading a complete 2D slice
+    in an unfavorable direction may still require the entire dataset to
+    be read from disk.
+
+    :param dataset: h5py dataset
+    :param transposition: List of dimension numbers in the wanted order
+    """
+    def __init__(self, dataset, transposition=None):
         """
 
         """
-        super(ArrayLike, self).__init__()
-        self.array_like = array_like
-        """original object"""
-        self._cache = None
-        """data as a numpy array (created when/if needed)"""
+        super(TransposedDatasetView, self).__init__()
+        self.dataset = dataset
+        """original dataset"""
 
-        if is_array(array_like):
-            self.shape = array_like.shape
-            self.dtype = array_like.dtype
-        elif is_nested_sequence(array_like):
-            self.shape = get_shape(array_like)
-            self.dtype = get_dtype(array_like)
+        self.shape = dataset.shape
+        """Tuple of array dimensions"""
+        self.dtype = dataset.dtype
+        """Data-type of the array’s element"""
+        self.ndim = len(dataset.shape)
+        """Number of array dimensions"""
 
-        self.array_type = get_array_type(array_like)
+        size = 0
+        if self.ndim:
+            size = 1
+            for dimsize in self.shape:
+                size *= dimsize
+        self.size = size
+        """Number of elements in the array."""
 
-    @property
-    def cached_array(self):
-        """data as a numpy array (created when/if needed)"""
-        if self._cache is None:
-            self._cache = numpy.asarray(self.array_like)
-        return self._cache
-
-    def __len__(self):
-        return len(self.array_like)
+        self.transposition = transposition or list(range(self.ndim))
+        """List of dimension numbers. By default this is simply
+        [0, ..., self.ndim], but it can be changed by using
+        :meth:`transpose`, to control the indices order when using
+        nD indexing or nD slicing."""
 
     def __getitem__(self, item):
-        """Implement slicing and fancy indexing for sequences"""
-        # arrays and datasets already support slicing and fancy indexing
-        if self.array_type in ["numpy array", "h5py dataset"]:
-            return self.array_like[item]
+        """Handle fancy indexing with regards to the dimension order as
+        specified in :attr:`transposition`
 
-        # From now on, we assume array_like is a nested sequence.
-        # Regular int indexing or simple slice
-        if isinstance(item, (int, slice)):
-            return self.array_like[item]
+        The supported fancy-indexing syntax is explained at
+        http://docs.h5py.org/en/latest/high/dataset.html#fancy-indexing.
 
-        # multidimensional/fancy slicing: numpy array needed
-        if hasattr(item, "__len__"):
-            return self.cached_array[item]
-            # TODO: implement nD slicing without array casting
+        Additional restrictions exist if the data has been transposed:
 
+            - numpy boolean array indexing is not supported
+            - ellipsis objects are not supported
 
+        :param item: Index, possibly fancy index (must be supported by h5py)
+        :return:
+        """
+        # no transposition, let the original dataset handle indexing
+        if self.transposition == list(range(self.ndim)):
+            return self.dataset[item]
+
+        # 1-D slicing -> n-D slicing (n=1)
+        if not hasattr(item, "__len__"):
+            # first dimension index is given
+            item = [item]
+            # following dimensions are indexed with : (all elements)
+            item += [slice(0, sys.maxint, 1) for _i in range(self.ndim - 1)]
+
+        # n-dimensional slicing
+        if len(item) != self.ndim:
+            raise IndexError(
+                "N-dim slicing requires a tuple of N indices/slices. " +
+                "Needed dimensions: %d" % self.ndim)
+
+        # get list of indices sorted in the original dataset order
+        new_indices = []
+        for dimension in self.transposition:
+            new_indices.append(item[dimension])
+
+        output_data_not_transposed = self.dataset[new_indices]
+
+        # now we must transpose the output data
+        output_dimensions = []
+        fixed_dimensions = []
+        for i, idx in enumerate(item):
+            # slices and sequences
+            if not isinstance(idx, int):
+                output_dimensions.append(self.transposition[i])
+            # regular integer index
+            else:
+                # whenever a dimension is fixed, the rank of following
+                # dimensions is decremented
+                fixed_dimensions.append(self.transposition[i])
+                for i2, od in enumerate(output_dimensions):
+                    if od > self.transposition[i]:
+                        output_dimensions[i2] -= 1
+
+        assert (len(output_dimensions) + len(fixed_dimensions)) == self.ndim
+
+        return numpy.transpose(output_data_not_transposed,
+                               axes=output_dimensions)
+
+    def __array__(self, dtype=None):
+        """Cast the dataset into a numpy array, and return it.
+
+        If a transposition has been done on this dataset, return
+        a transposed view of a numpy array.a"""
+        return numpy.transpose(numpy.array(self.dataset, dtype=dtype),
+                               self.transposition)
+
+    def transpose(self, transposition=None):
+        """Re-order (permute) the order of the array.
+
+        The returned object is a :class:`TransposedDatasetView` pointing to the same dataset
+        but with a different :attr:`transposition`
+
+        :param list[int] transposition: List of dimension numbers in the wanted order
+        :return: Transposed TransposedDatasetView
+        """
+        # by default, reverse the dimensions
+        if transposition is None:
+            transposition = list(reversed(self.transposition))
+
+        return TransposedDatasetView(self.dataset,
+                                     transposition)
 
