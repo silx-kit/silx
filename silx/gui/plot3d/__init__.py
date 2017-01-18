3d1d7326ced4a161a31a882bc6e99e734905b152
Thomas VINCENT <thomas.vincent@esrf.fr>
2017-01-18 18:14:47 +0100

    Remove isAvailable function and raise ImportError

--- silx/gui/plot3d/__init__.py
+++ silx/gui/plot3d/__init__.py
@@ -24,41 +24,22 @@
 # ###########################################################################*/
 """
 This package provides widgets displaying 3D content based on OpenGL.
+
+It depends on PyOpenGL and QtOpenGL.
 """
 from __future__ import absolute_import
 
 __authors__ = ["T. Vincent"]
 __license__ = "MIT"
-__date__ = "05/01/2017"
+__date__ = "18/01/2017"
 
-
-import logging as _logging
 
 from .. import qt as _qt
+
+if not _qt.HAS_OPENGL:
+    raise ImportError('Qt.QtOpenGL is not available')
 
 try:
     import OpenGL as _OpenGL
 except ImportError:
-    _OpenGL = None
-
-
-_logger = _logging.getLogger(__name__)
-
-
-if not _qt.HAS_OPENGL:
-    _logger.warning(
-        'Qt.QtOpenGL is not available: silx.gui.plot3d modules will fail.')
-
-if _OpenGL is None:
-    _logger.warning(
-        'PyOpenGL is not installed: silx.gui.plot3d modules will fail')
-
-
-def isAvailable():
-    """Returns True if plot3d functionality is available, False otherwise.
-
-    This function checks for PyOpenGL and QtOpenGL availability.
-    The availability of OpenGL 2.1 (required by plot3d) is not checked here.
-
-    :rtype: bool"""
-    return _OpenGL is not None and _qt.HAS_OPENGL
+    raise ImportError('PyOpenGL is not installed')
