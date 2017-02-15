9ae2740fb4544ab9788849c250a058e57a150902
payno <payno@users.noreply.github.com>
2017-02-15 11:17:28 +0100

    Update setup.py

--- silx/math/setup.py
+++ silx/math/setup.py
@@ -31,7 +31,6 @@ import os.path
 import numpy
 
 from numpy.distutils.misc_util import Configuration
-
 def configuration(parent_package='', top_path=None):
     config = Configuration('math', parent_package, top_path)
     config.add_subpackage('test')
