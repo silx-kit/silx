a0bf8fa710b96eaeb715de5c3843115099127066
Pierre <pierre.knobel@esrf.fr>
2017-02-24 16:27:02 +0100

    function _recursive_addcurve, implement NXdata1dView.setData and getDataPriority

--- silx/gui/data/NXdataView.py
+++ silx/gui/data/NXdataView.py
@@ -26,11 +26,10 @@
 :class:`silx.gui.data.DataViewer.DataViewer`
 """
 import logging
-import silx.io
+from silx.io import nxdata
 
 from silx.gui import icons
-from silx.gui import qt
-from .DataViewer import DataView, DataViewer, CompositeDataView
+from .DataViewer import DataView, CompositeDataView
 
 __authors__ = ["P. Knobel"]
 __license__ = "MIT"
@@ -40,99 +39,51 @@ __date__ = "22/02/2017"
 _logger = logging.getLogger(__name__)
 
 
-def is_NXdata(group):   # noqa
-    """Check if a h5py group is a **valid** NX_data group.
+def _recursive_addcurve(plot, x, signal, xlabel=None, ylabel=None,
+                        legend_prefix="NXdata spectrum ",
+                        index=tuple()):
+    """Plot all curves in an array with an arbitrary number of dimensions (n).
+    The last dimension of signal represents the sample number and must have
+    the same length as the 1-D array x.
+    All previous dimensions of the signal array represent the (n-1) dimensional
+    index of the curves.
+    The legend is the legend_prefix concatenated with the index (e.g.
+    "NXdata spectrum (511, 1023)")
 
-    Log warnings to troubleshoot malformed NXdata groups.
-    See specification http://download.nexusformat.org/sphinx/classes/base_classes/NXdata.html
+    Example::
 
-    :param group: h5py-like group
-    :raise TypeError: if group is not a h5py group, a spech5 group,
-        or a fabioh5 group
+        import numpy
+        x = numpy.arange(10)
+
+        signal = numpy.zeros((5, 10, 10))
+        for i in range(signal.shape[0]):
+            for j in range(signal.shape[1]):
+                signal[i, j] = i*10 + x*j
+
+        from silx.gui import qt
+        from silx.gui.plot import Plot1D
+        app = qt.QApplication([])
+        plot = Plot1D()
+
+        _recursive_addcurve(plot, x, signal,
+                            legend_prefix="a*10 + b*x with (a, b)=")
+
+        plot.show()
+        app.exec_()
+
     """
-    if not silx.io.is_group(group):
-        raise TypeError("group must be a h5py-like group")
-    if group.attrs.get("NX_class") != "NXdata":
-        return False
-    if "signal" not in group.attrs:
-        _logger.warning("NXdata group does not define a signal attr.")
-        return False
-
-    signal_name = group.attrs["signal"]
-    if signal_name not in group or not silx.io.is_dataset(group[signal_name]):
-        _logger.warning(
-                "Cannot find signal dataset '%s' in NXdata group" % signal_name)
-        return False
-
-    signal_ndim = len(group[signal_name].shape)
-
-    if "axes" in group.attrs:
-        axes = group.attrs.get("axes")
-        if isinstance(axes, str):
-            axes = [axes]
-
-        if signal_ndim != len(axes):
-            # FIXME: an axis dataset can apply to more than 1 dimension (check len(@AXISNAME_indices))
-            _logger.warning("%d axes defined for %dD signal" %
-                            (len(axes), signal_ndim))
-            return False
-
-        for axis in axes:
-            if axis == ".":
-                continue
-            if axis not in group or not silx.io.is_dataset(group[axis]):
-                _logger.warning("Could not find axis dataset '%d'" % axis +
-                                " in NXdata group")
-                return False
-            if len(group[axis].shape) != 1:
-                _logger.warning("Axis %s is not a 1D dataset" % axis)
-                return False
-    return True
+    if len(index) == len(signal.shape) - 1:
+        y = signal[index]
+        plot.addCurve(x=x, y=y, xlabel=xlabel, ylabel=ylabel,
+                      legend=legend_prefix + str(index))
+    else:
+        for i in range(signal.shape[len(index)]):
+            _recursive_addcurve(plot, x, signal, xlabel, ylabel,
+                                legend_prefix,
+                                index + (i, ))
 
 
-def NXdata_signal(group):
-    """Return the signal dataset in a NXdata group.
-
-    :param group: h5py-like group following the NeXus *NXdata* specification.
-    :return: Dataset whose name is specified in the *signal* attribute
-        of *group*.
-    :rtype: Dataset
-    """
-    if not is_NXdata(group):
-        raise TypeError("group is not a valid NXdata class")
-
-    return group[group.attrs["signal"]]
-
-
-def NXdata_axes(group):
-    """Return the axes datasets in a NXdata group.
-
-    :param group: h5py-like Group following the NeXus *NXdata* specification.
-    :return: List of datasets whose names are specified in the *axes*
-        attribute of *group*. The output list has as many elements as there
-        are dimensions in the signal. If a dimension has no dimension scale,
-        `None` is inserted in the list in its position.
-    :rtype: list[Dataset or None]
-    """
-    if not is_NXdata(group):
-        raise TypeError("group is not a valid NXdata class")
-
-    signal_ndims = len(NXdata_signal(group).shape)
-    axes = group.attrs.get("axes")
-
-    if axes is None:
-        return [None for _i in range(signal_ndims)]
-
-    # single string means that there is a single axis
-    if isinstance(axes, str):
-        return [group[axes]]
-
-    # axes is a list of string
-    return [group[axis] for axis in axes]
-    # FIXME: axes not necessarily in the right order, check @AXISNAME_indices
-
-
-class NXdata1dView(DataView):   # noqa
+class NXdata1dView(DataView):
     def __init__(self, parent):
         DataView.__init__(self, parent)
 
@@ -149,15 +100,35 @@ class NXdata1dView(DataView):   # noqa
         self.getWidget().clear()
 
     def setData(self, data):
-        signal = NXdata_signal(data)
-        x_axis = NXdata_axis(data)
-        data = self.normalizeData(data)
-        self.getWidget().addCurve(legend="data",
-                                  x=range(len(data)),
-                                  y=data,
-                                  resetzoom=self.__resetZoomNextTime)
-        self.__resetZoomNextTime = True
+        signal = nxdata.get_signal(data)
+        signal_name = data.attrs["signal"]
+        signal_len = signal.shape[-1]
+        group_name = data.name
+        x_axis = nxdata.get_axes(data)[-1]
+        x_label = nxdata.get_axes_names(data)[-1]
 
+        if x_axis is None:
+            x_axis = range(signal_len)
+
+        if len(signal.shape) == 1:
+            # single curve
+            assert x_axis.shape == signal.shape
+            legend = "NXdata curve " + group_name
+            self.getWidget().addCurve(legend=legend,
+                                      x=x_axis, y=signal,
+                                      xlabel=x_label, ylabel=signal_name)
+        else:
+            # array of curves (spectra)
+            assert x_axis.shape == signal.shape[-1:]
+            _recursive_addcurve(plot=self.getWidget(),
+                                x=x_axis, signal=signal,
+                                xlabel=x_label, ylabel=signal_name,
+                                legend_prefix="NXdata spectrum ")
+
+    def getDataPriority(self, data, info):
+        if info.isNXdata and nxdata.signal_is_1D(data):
+            return 100
+        return DataView.UNSUPPORTED
 
 
 class NXdataView(CompositeDataView):
@@ -192,7 +163,7 @@ class NXdataView(CompositeDataView):
         return []
 
     def getDataPriority(self, data, info):
-        if is_NXdata(data):
+        if nxdata.is_valid(data):
             return 100
         else:
             return DataView.UNSUPPORTED
