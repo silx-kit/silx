d75c2a28d5e6ec504d2b81d3489fb7994f9b2ef0
Pierre <pierre.knobel@esrf.fr>
2017-02-28 11:51:05 +0100

    Create and use ArrayImagePlot widget in NXdataImageView
    NXdata2dView renamed to NXdataImageView

--- silx/gui/data/NXdataView.py
+++ silx/gui/data/NXdataView.py
@@ -31,7 +31,7 @@ import numpy
 # scatter plot handling
 import matplotlib.cm
 import matplotlib.colors
-from silx.gui.plot.MPLColormap import viridis
+import silx.gui.plot.MPLColormap
 
 from silx.io import nxdata
 from silx.gui import qt, icons
@@ -80,11 +80,15 @@ class NXdataScalarView(DataView):
 class ArrayCurvePlot(qt.QWidget):
     """
     Widget for plotting a curve from a multi-dimensional signal array
-    and a 1D axes array.
+    and a 1D axis array.
 
     The signal array can have an arbitrary number of dimensions, the only
     limitation being that the last dimension must have the same length as
     the axis array.
+
+    The widget provides sliders to select indices on the first (n - 1)
+    dimensions of the signal arry, and buttons to add/replace selected
+    curves to the plot.
     """
     def __init__(self, parent=None):
         """
@@ -202,62 +206,194 @@ class NXdataCurveView(DataView):
         return DataView.UNSUPPORTED
 
 
-def image_axes_are_regular(x_axis, y_axis):
-    """Return True if both x_axis and y_axis are regularly spaced arrays
-
-    :param x_axis: 1D numpy array
-    :param y_axis: 1D numpy array
+class ArrayImagePlot(qt.QWidget):
     """
-    delta_x = x_axis[1:] - x_axis[:-1]
-    if not numpy.isclose(delta_x, delta_x[0]).all():
-        return False
-    delta_y = y_axis[1:] - y_axis[:-1]
-    if not numpy.isclose(delta_y, delta_y[0]).all():
-        return False
-    return True
+    Widget for plotting an image from a multi-dimensional signal array
+    and two 1D axes array.
 
+    The signal array can have an arbitrary number of dimensions, the only
+    limitation being that the last two dimensions must have the same length as
+    the axes arrays.
 
-def get_origin_scale(axis):
-    """Assuming axis is a regularly spaced 1D array,
-    return a tuple (origin, scale) where:
-        - origin = axis[0]
-        - scale = axis[1] - axis[0]
-    :param axis: 1D numpy array
-    :return: Tuple (axis[0], axis[1] - axis[0])
+    Sliders are provided to select indices on the first (n - 2) dimensions of
+    the signal array, and the plot is updated to show the image corresponding
+    to the selection.
+
+    If one or both of the axes does not have regularly spaced values, the
+    the image is plotted as a coloured scatter plot.
     """
-    return axis[0], axis[1] - axis[0]
+    def __init__(self, parent=None):
+        """
+
+        :param parent: Parent QWidget
+        """
+        super(ArrayImagePlot, self).__init__(parent)
+
+        self.__signal = None
+        self.__signal_name = None
+        self.__x_axis = None
+        self.__x_axis_name = None
+        self.__y_axis = None
+        self.__y_axis_name = None
+
+        self._plot = Plot2D(self)
+        self._plot.setDefaultColormap(
+                {"name": "viridis",
+                 "normalization": "linear",
+                 "autoscale": True})
+        self._selector = NumpyAxesSelector(self)
+        self.__selector_is_connected = False  # connected after setImageData
+
+        layout = qt.QVBoxLayout()
+        layout.addWidget(self._plot)
+        layout.addWidget(self._selector)
+
+        self.setLayout(layout)
+
+    def setImageData(self, signal,
+                     x_axis=None, y_axis=None,
+                     signal_name=None,
+                     xlabel=None, ylabel=None,
+                     title=None):
+        """
+
+        :param signal: n-D dataset, whose last dimension is used as the
+            curve's y values.
+        :param x_axis: 1-D dataset used as the image's x coordinates. If
+            provided, its lengths must be equal to the length of the last
+            dimension of ``signal``.
+        :param y_axis: 1-D dataset used as the image's y. If provided,
+            its lengths must be equal to the length of the 2nd to last
+            dimension of ``signal``.
+        :param signal_name: Label used in the legend
+        :param xlabel: Label for X axis
+        :param ylabel: Label for Y axis
+        :param title: Graph title
+        """
+        if self.__selector_is_connected:
+            self._selector.selectionChanged.disconnect(self._updateImage)
+            self.__selector_is_connected = False
+
+        self.__signal = signal
+        self.__signal_name = signal_name or ""
+        self.__x_axis = x_axis
+        self.__x_axis_name = xlabel
+        self.__y_axis = y_axis
+        self.__y_axis_name = ylabel
+
+        self._selector.setData(signal)
+        self._selector.setAxisNames([ylabel or "Y", xlabel or "X"])
+
+        self._plot.setGraphTitle(title or "")
+        self._plot.setGraphXLabel(self.__x_axis_name or "X")
+        self._plot.setGraphYLabel(self.__y_axis_name or "Y")
+
+        self._updateImage()
+
+        if not self.__selector_is_connected:
+            self._selector.selectionChanged.connect(self._updateImage)
+            self.__selector_is_connected = True
+
+    @staticmethod
+    def _image_axes_are_regular(x_axis, y_axis):
+        """Return True if both x_axis and y_axis are regularly spaced arrays
+
+        :param x_axis: 1D numpy array
+        :param y_axis: 1D numpy array
+        """
+        delta_x = x_axis[1:] - x_axis[:-1]
+        if not numpy.isclose(delta_x, delta_x[0]).all():
+            return False
+        delta_y = y_axis[1:] - y_axis[:-1]
+        if not numpy.isclose(delta_y, delta_y[0]).all():
+            return False
+        return True
+
+    @staticmethod
+    def _get_origin_scale(axis):
+        """Assuming axis is a regularly spaced 1D array,
+        return a tuple (origin, scale) where:
+            - origin = axis[0]
+            - scale = axis[1] - axis[0]
+        :param axis: 1D numpy array
+        :return: Tuple (axis[0], axis[1] - axis[0])
+        """
+        return axis[0], axis[1] - axis[0]
+
+    @staticmethod
+    def _applyColormap(values,
+                       colormap=silx.gui.plot.MPLColormap.viridis,
+                       minVal=None,
+                       maxVal=None, ):
+        """Compute RGBA array of shape (n, 4) from a 1D array of length n.
+
+        :param values: 1D array of values
+        :param colormap: colormap to be used
+        """
+        if minVal is None:
+            minVal = values.min()
+        if maxVal is None:
+            maxVal = values.max()
+        sm = matplotlib.cm.ScalarMappable(
+                norm=matplotlib.colors.Normalize(vmin=minVal, vmax=maxVal),
+                cmap=colormap)
+        colors = sm.to_rgba(values)
+        return colors
+
+    def _updateImage(self):
+        img = self._selector.selectedData()
+        x_axis = self.__x_axis
+        y_axis = self.__y_axis
+
+        is_regular_image = False
+        if x_axis is None and y_axis is None:
+            is_regular_image = True
+            origin = (0, 0)
+            scale = (1., 1.)
+        else:
+            if x_axis is None:
+                x_axis = numpy.arange(img.shape[-1])
+            if y_axis is None:
+                y_axis = numpy.arange(img.shape[-2])
+            if self._image_axes_are_regular(x_axis, y_axis):
+                is_regular_image = True
+                xorigin, xscale = self._get_origin_scale(x_axis)
+                yorigin, yscale = self._get_origin_scale(y_axis)
+                origin = (xorigin, yorigin)
+                scale = (xscale, yscale)
+
+        legend = self.__signal_name + "["
+        for sl in self._selector.selection():
+            if sl == slice(None):
+                legend += ":, "
+            else:
+                legend += str(sl) + ", "
+        legend = legend[:-2] + "]"
+
+        if is_regular_image:
+            # single regular image
+            self._plot.addImage(img, legend=legend,
+                                xlabel=self.__x_axis_name,
+                                ylabel=self.__y_axis_name,
+                                origin=origin, scale=scale)
+        else:
+            # TODO: use addScatter
+            scatterx, scattery = numpy.meshgrid(x_axis, y_axis)
+            rgbacolor = self._applyColormap(numpy.ravel(img))
+            self._plot.addCurve(
+                    numpy.ravel(scatterx), numpy.ravel(scattery),
+                    color=rgbacolor, symbol="o", linestyle="")
+
+    def clear(self):
+        self._plot.clear()
 
 
-# fixme: use addScatter when available
-def _applyColormap(values,
-                   colormap=viridis,
-                   minVal=None,
-                   maxVal=None,):
-    """Compute RGBA array of shape (n, 4) from a 1D array of length n.
-
-    :param values: 1D array of values
-    :param colormap: colormap to be used
-    """
-    if minVal is None:
-        minVal = values.min()
-    if maxVal is None:
-        maxVal = values.max()
-
-    sm = matplotlib.cm.ScalarMappable(
-            norm=matplotlib.colors.Normalize(vmin=minVal, vmax=maxVal),
-            cmap=colormap)
-    colors = sm.to_rgba(values)
-
-    return colors
-
-
-class NXdata2dView(DataView):
+class NXdataImageView(DataView):
     def __init__(self, parent):
         DataView.__init__(self, parent)
 
     def createWidget(self, parent):
-        from silx.gui.plot import Plot2D
-        widget = Plot2D(parent)
+        widget = ArrayImagePlot(parent)
         return widget
 
     def axesNames(self, data, info):
@@ -274,46 +410,10 @@ class NXdata2dView(DataView):
         y_axis, x_axis = nxdata.get_axes(data)[-2:]
         y_label, x_label = nxdata.get_axes_names(data)[-2:]
 
-        self.getWidget().setGraphTitle("NXdata group " + group_name)
-        self.getWidget().setGraphXLabel(x_label if x_label is not None else "X")
-        self.getWidget().setGraphYLabel(y_label if y_label is not None else "Y")
-
-        if len(signal.shape) == 2:
-            is_regular_image = False
-            if x_axis is None and y_axis is None:
-                is_regular_image = True
-                origin = (0, 0)
-                scale = (1., 1.)
-            elif image_axes_are_regular(x_axis, y_axis):
-                is_regular_image = True
-                xorigin, xscale = get_origin_scale(x_axis)
-                yorigin, yscale = get_origin_scale(y_axis)
-                origin = (xorigin, yorigin)
-                scale = (xscale, yscale)
-            if is_regular_image:
-                # single regular image
-                legend = "NXdata image " + group_name
-                self.getWidget().addImage(signal, legend=legend,
-                                          xlabel=x_label, ylabel=signal_name,
-                                          origin=origin, scale=scale)
-            else:
-                # TODO: use addScatter
-                scatterx, scattery = numpy.meshgrid(x_axis, y_axis)
-                rgbacolor = _applyColormap(numpy.ravel(signal))
-                numpy.ravel(signal)
-                self.getWidget().addCurve(
-                        numpy.ravel(scatterx), numpy.ravel(scattery),
-                        color=rgbacolor, symbol="o", linestyle="")
-
-        # TODO:
-        # else:
-        #     # array of curves (spectra)
-        #     assert x_axis.shape == signal.shape[-1:]
-        #     _recursive_addcurve(plot=self.getWidget(),
-        #                         x=x_axis, signal=signal,
-        #                         xlabel=x_label, ylabel=signal_name,
-        #                         legend_prefix="NXdata spectrum ")
-
+        self.getWidget().setImageData(
+                     signal, x_axis=x_axis, y_axis=y_axis,
+                     signal_name=signal_name, xlabel=x_label, ylabel=y_label,
+                     title="NXdata group %s: %s" % (group_name, signal_name))
 
     def getDataPriority(self, data, info):
         data = self.normalizeData(data)
@@ -334,4 +434,4 @@ class NXdataView(CompositeDataView):
 
         self.addView(NXdataScalarView(parent))
         self.addView(NXdataCurveView(parent))
-        self.addView(NXdata2dView(parent))
+        self.addView(NXdataImageView(parent))
