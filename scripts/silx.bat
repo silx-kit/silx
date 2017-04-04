e2b7d839b24741b54b2013cdb4c23520754af2f2
Valentin Valls <valentin.valls@esrf.fr>
2017-04-04 13:38:34 +0200

    Fix silx.bat to use the module and ignore user env

--- scripts/silx.bat
+++ scripts/silx.bat
@@ -1,4 +1,5 @@
 @echo off
-set CURRENT_PATH=%~dp0
-python "%CURRENT_PATH%silx-launcher.py" %*
+@rem -I: isolate Python from the user's environment (implies -E and -s)
+@rem It avoid to be overided my a path like ./silx
+python -I -m silx %*
 @echo on
