e1cc377bdd4acb60f5c67baa85f6b34f7d9034e0
t20100 <thomas.vincent@esrf.fr>
2017-04-12 09:31:07 +0200

    typo

--- scripts/silx.bat
+++ scripts/silx.bat
@@ -1,5 +1,5 @@
 @echo off
 @rem -I: isolate Python from the user's environment (implies -E and -s)
-@rem It avoid to be overided my a path like ./silx
+@rem It avoids to be overidded by a path like ./silx
 python -I -m silx %*
 @echo on
