da0e400e223a65050df76767a9963c1c02267f31
Payno <henri.payno@esrf.fr>
2017-05-03 14:42:12 +0200

    PEP8

--- silx/math/medianfilter/test/test_medianfilter.py
+++ silx/math/medianfilter/test/test_medianfilter.py
@@ -29,11 +29,9 @@ __date__ = "02/05/2017"
 
 import unittest
 import numpy
-import tempfile
 import os
-from  silx.math.medianfilter import medfilt2d
+from silx.math.medianfilter import medfilt2d
 from silx.test.utils import ParametricTestCase
-
 try:
     import scipy
 except:
@@ -49,17 +47,16 @@ class TestMedianFilterNearest(ParametricTestCase):
     """Unit tests for the median filter in nearest mode"""
 
     random_mat = numpy.array([
-        [ 0.05564293,  0.62717157,  0.75002406,  0.40555336,  0.70278975],
-        [ 0.76532598,  0.02839148,  0.05272484,  0.65166994,  0.42161216],
-        [ 0.23067427,  0.74219128,  0.56049024,  0.4440632 ,  0.28773158],
-        [ 0.81025249,  0.20303021,  0.68382382,  0.46372299,  0.81281709],
-        [ 0.94691602,  0.07813661,  0.81651256,  0.84220106,  0.33623165]])
-
+        [0.05564293,  0.62717157,  0.75002406,  0.40555336,  0.70278975],
+        [0.76532598,  0.02839148,  0.05272484,  0.65166994,  0.42161216],
+        [0.23067427,  0.74219128,  0.56049024,  0.44406320,  0.28773158],
+        [0.81025249,  0.20303021,  0.68382382,  0.46372299,  0.81281709],
+        [0.94691602,  0.07813661,  0.81651256,  0.84220106,  0.33623165]])
 
     def testFilter3_100(self):
         """Test median filter on a 10x10 matrix with a 3x3 kernel."""
         dataIn = numpy.arange(100, dtype=numpy.int32)
-        dataIn = dataIn.reshape((10,10))
+        dataIn = dataIn.reshape((10, 10))
 
         dataOut = medfilt2d(image=dataIn,
                             kernel_size=(3, 3),
@@ -90,9 +87,10 @@ class TestMedianFilterNearest(ParametricTestCase):
         self.assertTrue(dataOut[1, 0] == 6)
 
     def testFilterWidthOne(self):
-        """Make sure a filter of one by one give the same result as the input"""
+        """Make sure a filter of one by one give the same result as the input
+        """
         dataIn = numpy.arange(100, dtype=numpy.int32)
-        dataIn = dataIn.reshape((10,10))
+        dataIn = dataIn.reshape((10, 10))
 
         dataOut = medfilt2d(image=dataIn,
                             kernel_size=(1, 1),
@@ -103,16 +101,17 @@ class TestMedianFilterNearest(ParametricTestCase):
     def testInputDataIsNotModify(self):
         """Make sure input data is not modify by the median filter"""
         dataIn = numpy.arange(100, dtype=numpy.int32)
-        dataIn = dataIn.reshape((10,10))
+        dataIn = dataIn.reshape((10, 10))
         dataInCopy = dataIn.copy()
 
-        dataOut = medfilt2d(image=dataIn,
-                            kernel_size=(3, 3),
-                            conditional=False)
+        medfilt2d(image=dataIn,
+                  kernel_size=(3, 3),
+                  conditional=False)
         self.assertTrue(numpy.array_equal(dataIn, dataInCopy))
 
     def testThreads(self):
-        """Make sure the result doesn't depends on the number of threads used"""
+        """Make sure the result doesn't depends on the number of threads used
+        """
         dataIn = numpy.random.rand(100, 100)
 
         former = os.environ.get("OMP_NUM_THREADS")
@@ -156,16 +155,16 @@ class TestMedianFilterNearest(ParametricTestCase):
         self.assertTrue(dataOut[9, 9] == 98)
 
     def testTypes(self):
-        """Test that all needed types have their implementation of the median filter
+        """Test that all needed types have their implementation of the median
+        filter
         """
-        for testType in [numpy.float32, numpy.float64, numpy.int16, numpy.uint16,
-                         numpy.int32, numpy.int64, numpy.uint64]:
+        for testType in [numpy.float32, numpy.float64, numpy.int16,
+                         numpy.uint16, numpy.int32, numpy.int64, numpy.uint64]:
             data = (numpy.random.rand(10, 10) * 65000).astype(testType)
             out = medfilt2d(image=data,
                             kernel_size=(3, 3),
                             conditional=False)
             self.assertTrue(out.dtype.type is testType)
-
 
     def testFilter3_1D(self):
         """Simple test of a three by three kernel median filter"""
@@ -174,7 +173,7 @@ class TestMedianFilterNearest(ParametricTestCase):
         dataOut = medfilt2d(image=dataIn,
                             kernel_size=(5),
                             conditional=False)
-        
+
         self.assertTrue(dataOut[0] == 0)
         self.assertTrue(dataOut[9] == 9)
         self.assertTrue(dataOut[99] == 99)
@@ -187,14 +186,15 @@ class TestMedianFilterNearest(ParametricTestCase):
         kernels = [(3, 7), (7, 5), (1, 1), (3, 3)]
         for kernel in kernels:
             with self.subTest(kernel=kernel):
-                resScipy = scipy.ndimage.median_filter(data, size=kernel, mode='nearest')
+                resScipy = scipy.ndimage.median_filter(input=data,
+                                                       size=kernel,
+                                                       mode='nearest')
                 resSilx = medfilt2d(image=data,
                                     kernel_size=kernel,
                                     conditional=False)
 
                 self.assertTrue(numpy.array_equal(resScipy, resSilx))
 
-    
     @unittest.skipUnless(scipy, "scipy not available")
     def testRandomMatrice(self):
         kernels = [(3, 7), (7, 5), (1, 1), (3, 3)]
@@ -207,7 +207,7 @@ class TestMedianFilterNearest(ParametricTestCase):
                 resSilx = medfilt2d(image=self.random_mat,
                                     kernel_size=kernel,
                                     conditional=False)
-                
+
                 self.assertTrue(numpy.array_equal(resScipy, resSilx))
 
 
