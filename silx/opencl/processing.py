2585577a50e1d6ba57a20df81372c8008a18776f
Payno <henri.payno@esrf.fr>
2017-05-04 11:31:13 +0200

    Remove call to non existing function. But release is done on the destructoion so not needed

--- silx/opencl/processing.py
+++ silx/opencl/processing.py
@@ -160,7 +160,6 @@ class OpenclProcessing(object):
                     size = numpy.dtype(buf.dtype).itemsize * buf.size
                     mem[buf.name] = pyopencl.Buffer(self.ctx, buf.flags, size)
             except pyopencl.MemoryError as error:
-                release_cl_buffers(mem)
                 raise MemoryError(error)
 
         self.cl_mem.update(mem)
