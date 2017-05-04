b7a3c23046b91df082474c05292cb8a8d5f7d406
Jerome Kieffer <jerome.kieffer@esrf.fr>
2017-05-04 17:15:03 +0200

    typo

--- setup.py
+++ setup.py
@@ -503,8 +503,8 @@ package_data = {
             'gui/icons/*.mng',
             'gui/icons/*.gif',
             'gui/icons/animated/*.png',
-            'opencl/*.cl',
-            'opencl/sift/*.cl']
+            'opencl/sift/*.cl',
+            'opencl/*.cl']
     }
 setup_kwargs.update(name=PROJECT,
                     version=get_version(),
