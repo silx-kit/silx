37ed0836ee69776b3b7f1cb3a408361fbde28a6e
Thomas VINCENT <thomas.vincent@esrf.fr>
2017-07-13 16:23:23 +0200

    Add inherit from object

--- silx/gui/test/utils.py
+++ silx/gui/test/utils.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2016 European Synchrotron Radiation Facility
+# Copyright (c) 2016-2017 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -349,7 +349,7 @@ class TestCaseQt(unittest.TestCase):
         return result
 
 
-class SignalListener():
+class SignalListener(object):
     """Util to listen a Qt event and store parameters
     """
 
