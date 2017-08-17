silx-kit/silx

commit 6afc314789577f888806a7487f609934523593d7

Use the new axis API

silx/gui/plot/ImageView.py
@@ -47,7 +47,7 @@ from __future__ import division
 
 __authors__ = ["T. Vincent"]
 __license__ = "MIT"
-__date__ = "22/06/2017"
+__date__ = "27/06/2017"
 
 
 import logging
@@ -376,8 +376,8 @@ class ImageView(PlotWindow):
             scale = activeImage.getScale()
             height, width = data.shape
 
-            xMin, xMax = self.getGraphXLimits()
-            yMin, yMax = self.getGraphYLimits()
+            xMin, xMax = self.getXAxis().getLimits()
+            yMin, yMax = self.getYAxis().getLimits()
 
             # Convert plot area limits to image coordinates
             # and work in image coordinates (i.e., in pixels)
@@ -440,8 +440,8 @@ class ImageView(PlotWindow):
                     vOffset = 0.1 * (vMax - vMin)
                     if vOffset == 0.:
                         vOffset = 1.
-                    self._histoHPlot.setGraphYLimits(vMin - vOffset,
-                                                     vMax + vOffset)
+                    self._histoHPlot.getYAxis().setLimits(vMin - vOffset,
+                                                          vMax + vOffset)
 
                     coords = numpy.arange(2 * histoVVisibleData.size)
                     yCoords = (coords + 1) // 2 + subsetYMin
@@ -458,8 +458,8 @@ class ImageView(PlotWindow):
                     vOffset = 0.1 * (vMax - vMin)
                     if vOffset == 0.:
                         vOffset = 1.
-                    self._histoVPlot.setGraphXLimits(vMin - vOffset,
-                                                     vMax + vOffset)
+                    self._histoVPlot.getXAxis().setLimits(vMin - vOffset,
+                                                          vMax + vOffset)
             else:
                 self._dirtyCache()
                 self._histoHPlot.remove(kind='curve')
@@ -472,8 +472,8 @@ class ImageView(PlotWindow):
 
         Takes care of y coordinate conversion.
         """
-        xMin, xMax = self.getGraphXLimits()
-        yMin, yMax = self.getGraphYLimits()
+        xMin, xMax = self.getXAxis().getLimits()
+        yMin, yMax = self.getYAxis().getLimits()
         self._radarView.setVisibleRect(xMin, yMin, xMax - xMin, yMax - yMin)
 
     # Plots event listeners
@@ -507,14 +507,14 @@ class ImageView(PlotWindow):
             self._updateHistograms()
 
             # could use eventDict['xdata'], eventDict['ydata'] instead
-            xMin, xMax = self.getGraphXLimits()
-            yMin, yMax = self.getGraphYLimits()
+            xMin, xMax = self.getXAxis().getLimits()
+            yMin, yMax = self.getYAxis().getLimits()
 
             # Set horizontal histo limits
-            self._histoHPlot.setGraphXLimits(xMin, xMax)
+            self._histoHPlot.getXAxis().setLimits(xMin, xMax)
 
             # Set vertical histo limits
-            self._histoVPlot.setGraphYLimits(yMin, yMax)
+            self._histoVPlot.getYAxis().setLimits(yMin, yMax)
 
             self._updateRadarView()
 
@@ -542,9 +542,9 @@ class ImageView(PlotWindow):
 
         elif eventDict['event'] == 'limitsChanged':
             if (not self._updatingLimits and
-                    eventDict['xdata'] != self.getGraphXLimits()):
+                    eventDict['xdata'] != self.getXAxis().getLimits()):
                 xMin, xMax = eventDict['xdata']
-                self.setGraphXLimits(xMin, xMax)
+                self.getXAxis().setLimits(xMin, xMax)
 
     def _histoVPlotCB(self, eventDict):
         """Callback for vertical histogram plot events."""
@@ -568,9 +568,9 @@ class ImageView(PlotWindow):
 
         elif eventDict['event'] == 'limitsChanged':
             if (not self._updatingLimits and
-                    eventDict['ydata'] != self.getGraphYLimits()):
+                    eventDict['ydata'] != self.getYAxis().getLimits()):
                 yMin, yMax = eventDict['ydata']
-                self.setGraphYLimits(yMin, yMax)
+                self.getYAxis().setLimits(yMin, yMax)
 
     def _radarViewCB(self, left, top, width, height):
         """Slot for radar view visible rectangle changes."""
@@ -582,9 +582,9 @@ class ImageView(PlotWindow):
         """Sync image, vertical histogram and radar view axis orientation."""
         if inverted is None:
             # Do not perform this when called from plot signal
-            inverted = self.isYAxisInverted()
+            inverted = self.getYAxis().isInverted()
 
-        self._histoVPlot.setYAxisInverted(inverted)
+        self._histoVPlot.getYAxis().isInverted(inverted)
 
         # Use scale to invert radarView
         # RadarView default Y direction is from top to bottom
@@ -793,8 +793,8 @@ class ImageViewMainWindow(ImageView):
         super(ImageViewMainWindow, self).__init__(parent, backend)
         self.setWindowFlags(qt.Qt.Window)
 
-        self.setGraphXLabel('X')
-        self.setGraphYLabel('Y')
+        self.getXAxis().setLabel('X')
+        self.getYAxis().setLabel('Y')
         self.setGraphTitle('Image')
 
         # Add toolbars and status bar

examples/fftPlotAction.py
@@ -40,7 +40,7 @@ See shiftPlotAction.py for a simpler example with more basic comments.
 """
 __authors__ = ["P. Knobel"]
 __license__ = "MIT"
-__date__ = "12/01/2017"
+__date__ = "27/06/2017"
 
 import numpy
 import os
@@ -77,8 +77,8 @@ class FftAction(PlotAction):
     def _rememberGraphLabels(self):
         """Store labels and title as attributes"""
         self.original_title = self.plot.getGraphTitle()
-        self.original_xlabel = self.plot.getGraphXLabel()
-        self.original_ylabel = self.plot.getGraphYLabel()
+        self.original_xlabel = self.plot.getXAxis().getLabel()
+        self.original_ylabel = self.plot.getYAxis().getLabel()
 
     def fftAllCurves(self, checked=False):
         """Get all curves from our PlotWindow, compute the amplitude spectrum
@@ -97,13 +97,13 @@ class FftAction(PlotAction):
             self._rememberGraphLabels()
             # change them
             self.plot.setGraphTitle("Amplitude spectrum")
-            self.plot.setGraphXLabel("Frequency")
-            self.plot.setGraphYLabel("Amplitude")
+            self.plot.getXAxis().setLabel("Frequency")
+            self.plot.getYAxis().setLabel("Amplitude")
         else:
             # restore original labels
             self.plot.setGraphTitle(self.original_title)
-            self.plot.setGraphXLabel(self.original_xlabel)
-            self.plot.setGraphYLabel(self.original_ylabel)
+            self.plot.getXAxis().setLabel(self.original_xlabel)
+            self.plot.getYAxis().setLabel(self.original_ylabel)
 
         self.plot.clearCurves()
 
@@ -186,8 +186,8 @@ plotwin.addCurve(x, y2, legend="cos")
 plotwin.addCurve(x, y3, legend="square wave")
 
 plotwin.setGraphTitle("Original data")
-plotwin.setGraphYLabel("amplitude")
-plotwin.setGraphXLabel("time")
+plotwin.getYAxis().setLabel("amplitude")
+plotwin.getXAxis().setLabel("time")
 
 plotwin.show()
 app.exec_()

silx/gui/data/DataViews.py
@@ -38,7 +38,7 @@ from silx.io.nxdata import NXdata
 
 __authors__ = ["V. Valls", "P. Knobel"]
 __license__ = "MIT"
-__date__ = "15/06/2017"
+__date__ = "27/06/2017"
 
 _logger = logging.getLogger(__name__)
 
@@ -436,8 +436,8 @@ class _Plot2dView(DataView):
         from silx.gui import plot
         widget = plot.Plot2D(parent=parent)
         widget.setKeepDataAspectRatio(True)
-        widget.setGraphXLabel('X')
-        widget.setGraphYLabel('Y')
+        widget.getXAxis().setLabel('X')
+        widget.getYAxis().setLabel('Y')
         return widget
 
     def clear(self):

silx/gui/data/NXdataWidgets.py
@@ -26,7 +26,7 @@
 """
 __authors__ = ["P. Knobel"]
 __license__ = "MIT"
-__date__ = "20/03/2017"
+__date__ = "27/06/2017"
 
 import numpy
 
@@ -135,8 +135,8 @@ class ArrayCurvePlot(qt.QWidget):
             self.selectorDock.show()
 
         self._plot.setGraphTitle(title or "")
-        self._plot.setGraphXLabel(self.__axis_name or "X")
-        self._plot.setGraphYLabel(self.__signal_name or "Y")
+        self._plot.getXAxis().setLabel(self.__axis_name or "X")
+        self._plot.getYAxis().setLabel(self.__signal_name or "Y")
         self._updateCurve()
 
         if not self.__selector_is_connected:
@@ -188,8 +188,8 @@ class ArrayCurvePlot(qt.QWidget):
                                   xerror=self.__axis_errors,
                                   yerror=y_errors)
         self._plot.resetZoom()
-        self._plot.setGraphXLabel(self.__axis_name)
-        self._plot.setGraphYLabel(self.__signal_name)
+        self._plot.getXAxis().setLabel(self.__axis_name)
+        self._plot.getYAxis().setLabel(self.__signal_name)
 
     def clear(self):
         self._plot.clear()
@@ -289,8 +289,8 @@ class ArrayImagePlot(qt.QWidget):
             self.selectorDock.show()
 
         self._plot.setGraphTitle(title or "")
-        self._plot.setGraphXLabel(self.__x_axis_name or "X")
-        self._plot.setGraphYLabel(self.__y_axis_name or "Y")
+        self._plot.getXAxis().setLabel(self.__x_axis_name or "X")
+        self._plot.getYAxis().setLabel(self.__y_axis_name or "Y")
 
         self._updateImage()
 
@@ -352,8 +352,8 @@ class ArrayImagePlot(qt.QWidget):
                                   numpy.ravel(scattery),
                                   numpy.ravel(img),
                                   legend=legend)
-        self._plot.setGraphXLabel(self.__x_axis_name)
-        self._plot.setGraphYLabel(self.__y_axis_name)
+        self._plot.getXAxis().setLabel(self.__x_axis_name)
+        self._plot.getYAxis().setLabel(self.__y_axis_name)
         self._plot.resetZoom()
 
     def clear(self):
@@ -450,8 +450,8 @@ class ArrayStackPlot(qt.QWidget):
 
         self._stack_view.setGraphTitle(title or "")
         # by default, the z axis is the image position (dimension not plotted)
-        self._stack_view.setGraphXLabel(self.__x_axis_name or "X")
-        self._stack_view.setGraphYLabel(self.__y_axis_name or "Y")
+        self._stack_view.getXAxis().setLabel(self.__x_axis_name or "X")
+        self._stack_view.getYAxis().setLabel(self.__y_axis_name or "Y")
 
         self._updateStack()
 

silx/gui/fit/BackgroundWidget.py
@@ -35,7 +35,7 @@ from silx.math.fit import filters
 
 __authors__ = ["V.A. Sole", "P. Knobel"]
 __license__ = "MIT"
-__date__ = "24/01/2017"
+__date__ = "27/06/2017"
 
 
 class HorizontalSpacer(qt.QWidget):
@@ -400,7 +400,7 @@ class BackgroundWidget(qt.QWidget):
                                   legend='SNIP Background',
                                   resetzoom=False)
         if self._xmin is not None and self._xmax is not None:
-            self.graphWidget.setGraphXLimits(xmin=self._xmin, xmax=self._xmax)
+            self.graphWidget.getXAxis().setLimits(xmin=self._xmin, xmax=self._xmax)
 
 
 class BackgroundDialog(qt.QDialog):

silx/gui/plot/ColormapDialog.py
@@ -61,7 +61,7 @@ from __future__ import division
 
 __authors__ = ["V.A. Sole", "T. Vincent"]
 __license__ = "MIT"
-__date__ = "29/03/2016"
+__date__ = "27/06/2017"
 
 
 import logging
@@ -214,8 +214,8 @@ class ColormapDialog(qt.QDialog):
         """Init the plot to display the range and the values"""
         self._plot = PlotWidget()
         self._plot.setDataMargins(yMinMargin=0.125, yMaxMargin=0.125)
-        self._plot.setGraphXLabel("Data Values")
-        self._plot.setGraphYLabel("")
+        self._plot.getXAxis().setLabel("Data Values")
+        self._plot.getYAxis().setLabel("")
         self._plot.setInteractiveMode('select', zoomOnWheel=False)
         self._plot.setActiveCurveHandling(False)
         self._plot.setMinimumSize(qt.QSize(250, 200))

silx/gui/plot/CurvesROIWidget.py
@@ -46,7 +46,7 @@ ROI are defined by :
 
 __authors__ = ["V.A. Sole", "T. Vincent"]
 __license__ = "MIT"
-__date__ = "26/04/2017"
+__date__ = "27/06/2017"
 
 from collections import OrderedDict
 
@@ -743,7 +743,7 @@ class CurvesROIDockWidget(qt.QDockWidget):
         """Handle ROI widget signal"""
         _logger.debug("PlotWindow._roiSignal %s", str(ddict))
         if ddict['event'] == "AddROI":
-            xmin, xmax = self.plot.getGraphXLimits()
+            xmin, xmax = self.plot.getXAxis().getLimits()
             fromdata = xmin + 0.25 * (xmax - xmin)
             todata = xmin + 0.75 * (xmax - xmin)
             self.plot.remove('ROI min', kind='marker')
@@ -787,7 +787,7 @@ class CurvesROIDockWidget(qt.QDockWidget):
             if newroi == "ICR":
                 roiDict[newroi]['type'] = "Default"
             else:
-                roiDict[newroi]['type'] = self.plot.getGraphXLabel()
+                roiDict[newroi]['type'] = self.plot.getXAxis().getLabel()
             roiDict[newroi]['from'] = fromdata
             roiDict[newroi]['to'] = todata
             self.roiWidget.fillFromROIDict(roilist=roiList,

silx/gui/plot/PlotInteraction.py
@@ -26,7 +26,7 @@
 
 __authors__ = ["T. Vincent"]
 __license__ = "MIT"
-__date__ = "24/01/2017"
+__date__ = "27/06/2017"
 
 
 import math
@@ -153,11 +153,11 @@ class Pan(_ZoomOnWheel):
         xData, yData, y2Data = self._pixelToData(x, y)
         lastX, lastY, lastY2 = self._previousDataPos
 
-        xMin, xMax = self.plot.getGraphXLimits()
-        yMin, yMax = self.plot.getGraphYLimits(axis='left')
-        y2Min, y2Max = self.plot.getGraphYLimits(axis='right')
+        xMin, xMax = self.plot.getXAxis().getLimits()
+        yMin, yMax = self.plot.getYAxis().getLimits()
+        y2Min, y2Max = self.plot.getYAxis(axis='right').getLimits()
 
-        if self.plot.isXAxisLogarithmic():
+        if self.plot.getXAxis()._isLogarithmic():
             try:
                 dx = math.log10(xData) - math.log10(lastX)
                 newXMin = pow(10., (math.log10(xMin) - dx))
@@ -176,7 +176,7 @@ class Pan(_ZoomOnWheel):
             if newXMin < FLOAT32_SAFE_MIN or newXMax > FLOAT32_SAFE_MAX:
                 newXMin, newXMax = xMin, xMax
 
-        if self.plot.isYAxisLogarithmic():
+        if self.plot.getYAxis()._isLogarithmic():
             try:
                 dy = math.log10(yData) - math.log10(lastY)
                 newYMin = pow(10., math.log10(yMin) - dy)
@@ -287,22 +287,22 @@ class Zoom(_ZoomOnWheel):
                 self._lastClick = time.time(), (dataPos[0], dataPos[1], x, y)
 
             # Zoom-in centered on mouse cursor
-            # xMin, xMax = self.plot.getGraphXLimits()
-            # yMin, yMax = self.plot.getGraphYLimits()
-            # y2Min, y2Max = self.plot.getGraphYLimits(axis="right")
+            # xMin, xMax = self.plot.getXAxis().getLimits()
+            # yMin, yMax = self.plot.getYAxis().getLimits()
+            # y2Min, y2Max = self.plot.getYAxis(axis='right').getLimits()
             # self.zoomStack.append((xMin, xMax, yMin, yMax, y2Min, y2Max))
             # self._zoom(x, y, 2)
         elif btn == RIGHT_BTN:
             try:
                 xMin, xMax, yMin, yMax, y2Min, y2Max = self.zoomStack.pop()
             except IndexError:
-                previousLimits = (self.plot.getGraphXLimits(),
-                                  self.plot.getGraphYLimits(),
-                                  self.plot.getGraphYLimits(axis='right'))
+                previousLimits = (self.plot.getXAxis().getLimits(),
+                                  self.plot.getYAxis().getLimits(),
+                                  self.plot.getYAxis(axis='right').getLimits())
                 self.plot.resetZoom()
-                newLimits = (self.plot.getGraphXLimits(),
-                             self.plot.getGraphYLimits(),
-                             self.plot.getGraphYLimits(axis='right'))
+                newLimits = (self.plot.getXAxis().getLimits(),
+                             self.plot.getYAxis().getLimits(),
+                             self.plot.getYAxis(axis='right').getLimits())
                 if previousLimits == newLimits:
                     # Signal mouse clicked event
                     dataPos = self.plot.pixelToData(x, y)
@@ -362,9 +362,9 @@ class Zoom(_ZoomOnWheel):
 
         if x0 != x1 or y0 != y1:  # Avoid empty zoom area
             # Store current zoom state in stack
-            xMin, xMax = self.plot.getGraphXLimits()
-            yMin, yMax = self.plot.getGraphYLimits()
-            y2Min, y2Max = self.plot.getGraphYLimits(axis="right")
+            xMin, xMax = self.plot.getXAxis().getLimits()
+            yMin, yMax = self.plot.getYAxis().getLimits()
+            y2Min, y2Max = self.plot.getYAxis(axis="right").getLimits()
             self.zoomStack.append((xMin, xMax, yMin, yMax, y2Min, y2Max))
 
             if self.plot.isKeepDataAspectRatio():

commit f43c98ec893df0e1f5fc9a99062c71a4d7a5acde

Fix bug introduced in commit 6afc314789577f888806a7487f609934523593d7
axis.isInverted(inverted) --> axis.setInverted(inverted)

silx/gui/plot/ImageView.py
@@ -47,7 +47,7 @@ from __future__ import division
 
 __authors__ = ["T. Vincent"]
 __license__ = "MIT"
-__date__ = "16/08/2017"
+__date__ = "17/08/2017"
 
 
 import logging
@@ -584,7 +584,7 @@ class ImageView(PlotWindow):
             # Do not perform this when called from plot signal
             inverted = self.getYAxis().isInverted()
 
-        self._histoVPlot.getYAxis().isInverted(inverted)
+        self._histoVPlot.getYAxis().setInverted(inverted)
 
         # Use scale to invert radarView
         # RadarView default Y direction is from top to bottom