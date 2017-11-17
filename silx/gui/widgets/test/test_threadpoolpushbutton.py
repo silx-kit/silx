a2792892090ead61c69894a73f86cbb3b77af559
Valentin Valls <valentin.valls@esrf.fr>
2017-11-17 11:33:09 +0100

    Avoid to saturate the threadpool for this test

--- silx/gui/widgets/test/test_threadpoolpushbutton.py
+++ silx/gui/widgets/test/test_threadpoolpushbutton.py
@@ -74,7 +74,7 @@ class TestThreadPoolPushButton(TestCaseQt):
     def testMultiExecution(self):
         button = ThreadPoolPushButton()
         button.setCallable(self._trace, "a", 0)
-        number = qt.QThreadPool.globalInstance().maxThreadCount() * 2
+        number = qt.QThreadPool.globalInstance().maxThreadCount()
         for _ in range(number):
             button.executeCallable()
         self.waitForPendingOperations(button)
