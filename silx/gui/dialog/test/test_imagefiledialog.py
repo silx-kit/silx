silx-kit/pyFAI

Clean up save/restore tests

pyFAI/gui/dialog/test/test_imagefiledialog.py
@@ -26,7 +26,7 @@
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "25/10/2017"
+__date__ = "03/11/2017"
 
 
 import unittest
@@ -511,76 +511,88 @@ class TestImageFileDialogApi(utils.TestCaseQt, _UtilsMixin):
     def testSaveRestoreState(self):
         dialog = ImageFileDialog()
         dialog.setDirectory(_tmpDirectory)
+        colormap = Colormap(normalization=Colormap.LOGARITHM)
+        dialog.setColormap(colormap)
         self.qWaitForPendingActions(dialog)
+
         state = dialog.saveState()
         dialog2 = ImageFileDialog()
         result = dialog2.restoreState(state)
         self.assertTrue(result)
+        self.assertTrue(dialog2.colormap().getNormalization(), "log")
 
     def printState(self):
         dialog = ImageFileDialog()
         colormap = Colormap(normalization=Colormap.LOGARITHM)
+        dialog.setDirectory("")
+        dialog.setHistory([])
         dialog.setColormap(colormap)
         dialog.setSidebarUrls([])
         state = dialog.saveState()
         string = ""
+        strings = []
         for i in range(state.size()):
-            d = ord(state.at(i)[0])
+            d = state.data()[i]
+            if not isinstance(d, int):
+                d = ord(d)
             if d > 0x20 and d < 0x7F:
                 string += chr(d)
             else:
                 string += "\\x%02X" % d
-        print(string)
+            if len(string) > 60:
+                strings.append(string)
+                string = ""
+        strings.append(string)
+        strings = ["b'%s'" % s for s in strings]
+        print()
+        print("\\\n".join(strings))
+
+    STATE_VERSION1_QT5 = b''\
+        b'\x00\x00\x001pyFAI.gui.dialog.ImageFileDialog.ImageFileDialog'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00#\x00\x00'\
+        b'\x00\xFF\x00\x00\x00\x01\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF'\
+        b'\xFF\xFF\xFF\xFF\xFF\xFF\x01\xFF\xFF\xFF\xFF\x01\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'\
+        b'\x00\x0C\x00\x00\x00\x00\xA4\x00\x00\x00\x08Browser\x00\x00\x00'\
+        b'\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00\x87\x00\x00\x00\xFF\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x01\x90\x00\x00\x00\x04\x01\x01\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF\xFF\x00\x00\x00'\
+        b'\x81\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00d\x00\x00\x00'\
+        b'\x01\x00\x00\x00\x00\x00\x00\x00d\x00\x00\x00\x01\x00\x00\x00'\
+        b'\x00\x00\x00\x00d\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'\
+        b'd\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03\xE8\x00\xFF\xFF'\
+        b'\xFF\xFF\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09Color'\
+        b'map\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01\x00'\
+        b'\x00\x00\x04log\x00'
+
+    STATE_VERSION1_QT4 = b''\
+        b'\x00\x00\x001pyFAI.gui.dialog.ImageFileDialog.ImageFileDialog'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00"\x00\x00'\
+        b'\x00\xFF\x00\x00\x00\x00\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF'\
+        b'\xFF\xFF\xFF\xFF\xFF\xFF\x01\x00\x00\x00\x06\x01\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00'\
+        b'\x0C\x00\x00\x00\x00w\x00\x00\x00\x08Browser\x00\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x0C\x00\x00\x00\x00Z\x00\x00\x00\xFF\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x01\x90\x00\x00\x00\x04\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF\xFF\x00\x00\x00\x81\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x01\x90\x00\x00\x00\x04\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09C'\
+        b'olormap\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01'\
+        b'\x00\x00\x00\x04log\x00'
 
     def testAvoidRestoreRegression_Version1(self):
         if qt.qVersion().startswith("5."):
-            self._testAvoidRestoreRegression_Qt5_Version1()
+            state = self.STATE_VERSION1_QT5
         elif qt.qVersion().startswith("4."):
-            self._testAvoidRestoreRegression_Qt4_Version1()
+            state = self.STATE_VERSION1_QT4
+        else:
+            assert(False)
 
-    def _testAvoidRestoreRegression_Qt5_Version1(self):
-        state = b'\x00\x00\x00'\
-            b'1pyFAI.gui.dialog.ImageFileDialog.ImageFileDialog\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0c\x00\x00\x00\x00#\x00\x00\x00\xff\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'\
-            b'\xff\xff\x01\xff\xff\xff\xff\x01\x00\x00\x00\x01\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x13E:/valls/pyfai.git\x00\x00'\
-            b'\x00\x00\x0c\x00\x00\x00\x00t\x00\x00\x00\x08Browser\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0c\x00\x00\x00\x00W\x00\x00\x00\xff\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00d\xff\xff\xff\xff\x00\x00\x00'\
-            b'\x81\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\xe8\x00\xff\xff'\
-            b'\xff\xff\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\tColormap'\
-            b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01\x00\x00'\
-            b'\x00\x04log\x00'
         state = qt.QByteArray(state)
-
-        dialog = self.createDialog()
-        result = dialog.restoreState(state)
-        self.assertTrue(result)
-        colormap = dialog.colormap()
-        self.assertTrue(colormap.getNormalization(), "log")
-
-    def _testAvoidRestoreRegression_Qt4_Version1(self):
-        state = b'\x00\x00\x00'\
-            b'1pyFAI.gui.dialog.ImageFileDialog.ImageFileDialog\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00"\x00\x00\x00\xFF\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'\
-            b'\xFF\xFF\xFF\x01\x00\x00\x00\x05\x01\x00\x00\x00\x01\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x13E:/valls/pyfai.git\x00\x00'\
-            b'\x00\x00\x0C\x00\x00\x00\x00k\x00\x00\x00\x08Browser\x00\x00'\
-            b'\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00N\x00\x00\x00\xFF'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF'\
-            b'\xFF\x00\x00\x00\x81\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x01\x00\x00\x00\x01\x00\x00\x00\x09Colormap\x00\x00\x00\x00\x01'\
-            b'\x00\x00\x00\x00\x05gray\x00\x01\x01\x00\x00\x00\x04log\x00'
-        state = qt.QByteArray(state)
-
         dialog = self.createDialog()
         result = dialog.restoreState(state)
         self.assertTrue(result)

silx-kit/silx

silx/gui/dialog/test/test_imagefiledialog.py
@@ -26,7 +26,7 @@
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "25/10/2017"
+__date__ = "03/11/2017"
 
 
 import unittest
@@ -511,76 +511,88 @@ class TestImageFileDialogApi(utils.TestCaseQt, _UtilsMixin):
     def testSaveRestoreState(self):
         dialog = ImageFileDialog()
         dialog.setDirectory(_tmpDirectory)
+        colormap = Colormap(normalization=Colormap.LOGARITHM)
+        dialog.setColormap(colormap)
         self.qWaitForPendingActions(dialog)
+
         state = dialog.saveState()
         dialog2 = ImageFileDialog()
         result = dialog2.restoreState(state)
         self.assertTrue(result)
+        self.assertTrue(dialog2.colormap().getNormalization(), "log")
 
     def printState(self):
         dialog = ImageFileDialog()
         colormap = Colormap(normalization=Colormap.LOGARITHM)
+        dialog.setDirectory("")
+        dialog.setHistory([])
         dialog.setColormap(colormap)
         dialog.setSidebarUrls([])
         state = dialog.saveState()
         string = ""
+        strings = []
         for i in range(state.size()):
-            d = ord(state.at(i)[0])
+            d = state.data()[i]
+            if not isinstance(d, int):
+                d = ord(d)
             if d > 0x20 and d < 0x7F:
                 string += chr(d)
             else:
                 string += "\\x%02X" % d
-        print(string)
+            if len(string) > 60:
+                strings.append(string)
+                string = ""
+        strings.append(string)
+        strings = ["b'%s'" % s for s in strings]
+        print()
+        print("\\\n".join(strings))
+
+    STATE_VERSION1_QT5 = b''\
+        b'\x00\x00\x001silx.gui.dialog.ImageFileDialog.ImageFileDialog'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00#\x00\x00'\
+        b'\x00\xFF\x00\x00\x00\x01\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF'\
+        b'\xFF\xFF\xFF\xFF\xFF\xFF\x01\xFF\xFF\xFF\xFF\x01\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'\
+        b'\x00\x0C\x00\x00\x00\x00\xA4\x00\x00\x00\x08Browser\x00\x00\x00'\
+        b'\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00\x87\x00\x00\x00\xFF\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x01\x90\x00\x00\x00\x04\x01\x01\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF\xFF\x00\x00\x00'\
+        b'\x81\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00d\x00\x00\x00'\
+        b'\x01\x00\x00\x00\x00\x00\x00\x00d\x00\x00\x00\x01\x00\x00\x00'\
+        b'\x00\x00\x00\x00d\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'\
+        b'd\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03\xE8\x00\xFF\xFF'\
+        b'\xFF\xFF\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09Color'\
+        b'map\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01\x00'\
+        b'\x00\x00\x04log\x00'
+
+    STATE_VERSION1_QT4 = b''\
+        b'\x00\x00\x001silx.gui.dialog.ImageFileDialog.ImageFileDialog'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00"\x00\x00'\
+        b'\x00\xFF\x00\x00\x00\x00\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF'\
+        b'\xFF\xFF\xFF\xFF\xFF\xFF\x01\x00\x00\x00\x06\x01\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00'\
+        b'\x0C\x00\x00\x00\x00w\x00\x00\x00\x08Browser\x00\x00\x00\x00\x01'\
+        b'\x00\x00\x00\x0C\x00\x00\x00\x00Z\x00\x00\x00\xFF\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x01\x90\x00\x00\x00\x04\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00'\
+        b'\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF\xFF\x00\x00\x00\x81\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x01\x90\x00\x00\x00\x04\x00'\
+        b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09C'\
+        b'olormap\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01'\
+        b'\x00\x00\x00\x04log\x00'
 
     def testAvoidRestoreRegression_Version1(self):
         if qt.qVersion().startswith("5."):
-            self._testAvoidRestoreRegression_Qt5_Version1()
+            state = self.STATE_VERSION1_QT5
         elif qt.qVersion().startswith("4."):
-            self._testAvoidRestoreRegression_Qt4_Version1()
+            state = self.STATE_VERSION1_QT4
+        else:
+            assert(False)
 
-    def _testAvoidRestoreRegression_Qt5_Version1(self):
-        state = b'\x00\x00\x00'\
-            b'1silx.gui.dialog.ImageFileDialog.ImageFileDialog\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0c\x00\x00\x00\x00#\x00\x00\x00\xff\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x03\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'\
-            b'\xff\xff\x01\xff\xff\xff\xff\x01\x00\x00\x00\x01\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x13E:/valls/silx.git\x00\x00'\
-            b'\x00\x00\x0c\x00\x00\x00\x00t\x00\x00\x00\x08Browser\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0c\x00\x00\x00\x00W\x00\x00\x00\xff\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00d\xff\xff\xff\xff\x00\x00\x00'\
-            b'\x81\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\xe8\x00\xff\xff'\
-            b'\xff\xff\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\tColormap'\
-            b'\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05gray\x00\x01\x01\x00\x00'\
-            b'\x00\x04log\x00'
         state = qt.QByteArray(state)
-
-        dialog = self.createDialog()
-        result = dialog.restoreState(state)
-        self.assertTrue(result)
-        colormap = dialog.colormap()
-        self.assertTrue(colormap.getNormalization(), "log")
-
-    def _testAvoidRestoreRegression_Qt4_Version1(self):
-        state = b'\x00\x00\x00'\
-            b'1silx.gui.dialog.ImageFileDialog.ImageFileDialog\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00"\x00\x00\x00\xFF\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x03\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'\
-            b'\xFF\xFF\xFF\x01\x00\x00\x00\x05\x01\x00\x00\x00\x01\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x13E:/valls/silx.git\x00\x00'\
-            b'\x00\x00\x0C\x00\x00\x00\x00k\x00\x00\x00\x08Browser\x00\x00'\
-            b'\x00\x00\x01\x00\x00\x00\x0C\x00\x00\x00\x00N\x00\x00\x00\xFF'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00'\
-            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00d\xFF\xFF\xFF'\
-            b'\xFF\x00\x00\x00\x81\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\
-            b'\x01\x00\x00\x00\x01\x00\x00\x00\x09Colormap\x00\x00\x00\x00\x01'\
-            b'\x00\x00\x00\x00\x05gray\x00\x01\x01\x00\x00\x00\x04log\x00'
-        state = qt.QByteArray(state)
-
         dialog = self.createDialog()
         result = dialog.restoreState(state)
         self.assertTrue(result)