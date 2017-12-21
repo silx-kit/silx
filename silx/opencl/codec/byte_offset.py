3fa98de5b1f51fba843ef960bb14575ab1b57035
Thomas VINCENT <thomas.vincent@esrf.fr>
2017-12-21 16:08:15 +0100

    Add initial version of CBF compression (WIP)

--- silx/opencl/codec/byte_offset.py
+++ silx/opencl/codec/byte_offset.py
@@ -91,6 +91,8 @@ class ByteOffset(OpenclProcessing):
         self.allocate_buffers(buffers, use_array=True)
         self.compile_kernels([os.path.join("codec", "byte_offset")])
         self.kernels.__setattr__("scan", self._init_double_scan())
+        self.kernels.__setattr__("compression_scan",
+                                 self._init_compression_scan())
 
     def _init_double_scan(self):
         "generates a double scan on indexes and values in one operation"
@@ -221,3 +223,109 @@ class ByteOffset(OpenclProcessing):
         return out
 
     __call__ = decode
+
+    def _init_compression_scan(self):
+        """Initialize CBF compression scan kernels"""
+        preamble = """
+        int compressed_size(int diff) {
+            int abs_diff = abs(diff);
+
+            if (abs_diff < 128) {
+                return 1;
+            }
+            else if (abs_diff < 32768) {
+                return 3;
+            }
+            else {
+                return 7;
+            }
+        }
+
+        void write(const int index,
+                   const int diff,
+                   global char *output) {
+            int abs_diff = abs(diff);
+
+            if (abs_diff < 128) {
+                output[index] = (char) diff;
+            }
+            else if (abs_diff < 32768) {
+                output[index] = -128;
+                output[index + 1] = (char) (diff >> 0);
+                output[index + 2] = (char) (diff >> 8);
+            }
+            else {
+                output[index] = -128;
+                output[index + 1] = 0;
+                output[index + 2] = -128;
+                output[index + 3] = (char) (diff >> 0);
+                output[index + 4] = (char) (diff >> 8);
+                output[index + 5] = (char) (diff >> 16);
+                output[index + 6] = (char) (diff >> 24);
+            }
+        }
+        """
+        arguments = "__global const int *data, __global char *compressed, __global int *size"
+        input_expr = "compressed_size((i == 0) ? data[0] : (data[i] - data[i - 1]))"
+        scan_expr = "a+b"
+        neutral = "0"
+        output_statement = """
+        if (prev_item == 0) { // 1st thread store compressed data size
+            size[0] = last_item;
+        }
+        write(prev_item, (i == 0) ? data[0] : (data[i] - data[i - 1]), compressed);
+        """
+
+        if self.block_size >= 64:
+            knl = GenericScanKernel(self.ctx,
+                                    dtype=numpy.int32,
+                                    preamble=preamble,
+                                    arguments=arguments,
+                                    input_expr=input_expr,
+                                    scan_expr=scan_expr,
+                                    neutral=neutral,
+                                    output_statement=output_statement)
+        else:  # MacOS on CPU
+            knl = GenericDebugScanKernel(self.ctx,
+                                         dtype=numpy.int32,
+                                         preamble=preamble,
+                                         arguments=arguments,
+                                         input_expr=input_expr,
+                                         scan_expr=scan_expr,
+                                         neutral=neutral,
+                                         output_statement=output_statement)
+        return knl
+
+    def encode(self, data, out=None):
+        """This function compress provided data to CBF.
+
+        :param numpy.ndarray data: The data as a numpy array of int32.
+        :param pyopencl.array out: pyopencl array in which to place the result.
+        :return: The compressed data as a pyopencl array.
+        :rtype: pyopencl.array
+        """
+        # TODO also support pyopencl array as input
+        # TODO as_bytes argument
+        # TODO record a maximum number of events
+        # TODO use semaphore
+        # TODO reuse buffers? and use those of decompression
+        # TODO write test + sample code
+
+        data = numpy.ascontiguousarray(data, dtype=numpy.int32).ravel()
+        size = data.size
+
+        d_data = pyopencl.array.to_device(self.queue, data)
+        d_compressed = pyopencl.array.empty(
+            self.queue, shape=(size * 7,), dtype=numpy.int8)
+        d_size = pyopencl.array.zeros(self.queue, (1,), dtype=numpy.int32)
+
+        self.kernels.compression_scan(d_data, d_compressed, d_size)
+        byte_count = d_size.get()[0]
+
+        if out is None:
+            out = pyopencl.array.empty(
+                self.queue, shape=(byte_count,), dtype=numpy.int8)
+
+        pyopencl.enqueue_copy_buffer(
+            self.queue, d_compressed.data, out.data, byte_count=byte_count)
+        return out
