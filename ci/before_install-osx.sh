0af6513b2171304119b979e07925b26eaf16e940
Thomas VINCENT <thomas.vincent@esrf.fr>
2018-03-05 13:28:47 +0000

    Update before_install-osx.sh

--- ci/before_install-osx.sh
+++ ci/before_install-osx.sh
@@ -3,7 +3,7 @@
 VENV_DIR=./venv
 
 # Use brew for python3
-brew install python@3
+brew upgrade python@3
 PYTHON_EXE=`brew list python@3 | grep "bin/python3$" | head -n 1`
 # Create virtual env
 $PYTHON_EXE -m venv $VENV_DIR
