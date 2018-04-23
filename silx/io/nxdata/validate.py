1a7f7feb15030f34cc296530fabddd9adaf9847b
Pierre <pierre.knobel@esrf.fr>
2018-04-23 14:43:40 +0200

    start splitting large function

--- silx/io/nxdata/validate.py
+++ silx/io/nxdata/validate.py
@@ -57,6 +57,50 @@ def _nxdata_warning(msg, group_name=""):
     _logger.warning(warning_prefix + msg)
 
 
+def _get_signal_name(group):
+    """Return the name of the (main) signal in a NXdata group.
+    Return None if this info is missing (invalid NXdata).
+
+    """
+    signal_name = get_attr_as_unicode(group, "signal", default=None)
+    if signal_name is None:
+        _logger.info("NXdata group %s does not define a signal attr. "
+                     "Testing legacy specification.", group.name)
+        for key in group:
+            if "signal" in group[key].attrs:
+                signal_name = key
+                signal_attr = group[key].attrs["signal"]
+                if signal_attr in [1, b"1", u"1"]:
+                    # This is the main (default) signal
+                    break
+    return signal_name
+
+
+def _get_auxiliary_signals_names(group):
+    """Return list of auxiliary signals names"""
+    auxiliary_signals_names = get_attr_as_unicode(group, "auxiliary_signals",
+                                                  default=[])
+    if isinstance(auxiliary_signals_names, (six.text_type, six.binary_type)):
+        auxiliary_signals_names = [auxiliary_signals_names]
+    return auxiliary_signals_names
+
+
+def _validate_auxiliary_signals(group, signal_name, auxiliary_signals_names):
+    """Check data dimensionality and size. Return False if invalid."""
+    for asn in auxiliary_signals_names:
+        if asn not in group or not is_dataset(group[asn]):
+            _nxdata_warning(
+                "Cannot find auxiliary signal dataset '%s'" % asn,
+                group.name)
+            return False
+        if group[signal_name].shape != group[asn].shape:
+            _nxdata_warning("Auxiliary signal dataset '%s' does not" % asn +
+                            " have the same shape as the main signal.",
+                            group.name)
+            return False
+    return True
+
+
 def is_valid_nxdata(group):   # noqa
     """Check if a h5py group is a **valid** NX_data group.
 
@@ -75,24 +119,13 @@ def is_valid_nxdata(group):   # noqa
         raise TypeError("group must be a h5py-like group")
     if get_attr_as_unicode(group, "NX_class") != "NXdata":
         return False
-    if "signal" not in group.attrs:
-        _logger.info("NXdata group %s does not define a signal attr. "
-                     "Testing legacy specification.", group.name)
-        signal_name = None
-        for key in group:
-            if "signal" in group[key].attrs:
-                signal_name = key
-                signal_attr = group[key].attrs["signal"]
-                if signal_attr in [1, b"1", u"1"]:
-                    # This is the main (default) signal
-                    break
-        if signal_name is None:
-            _nxdata_warning("No @signal attribute on the NXdata group, "
-                            "and no dataset with a @signal=1 attr found",
-                            group.name)
-            return False
-    else:
-        signal_name = get_attr_as_unicode(group, "signal")
+
+    signal_name = _get_signal_name(group)
+    if signal_name is None:
+        _nxdata_warning("No @signal attribute on the NXdata group, "
+                        "and no dataset with a @signal=1 attr found",
+                        group.name)
+        return False
 
     if signal_name not in group or not is_dataset(group[signal_name]):
         _nxdata_warning(
@@ -100,21 +133,8 @@ def is_valid_nxdata(group):   # noqa
             group.name)
         return False
 
-    auxiliary_signals_names = get_attr_as_unicode(group, "auxiliary_signals",
-                                                  default=[])
-    if isinstance(auxiliary_signals_names, (six.text_type, six.binary_type)):
-        auxiliary_signals_names = [auxiliary_signals_names]
-    for asn in auxiliary_signals_names:
-        if asn not in group or not is_dataset(group[asn]):
-            _nxdata_warning(
-                "Cannot find auxiliary signal dataset '%s'" % asn,
-                group.name)
-            return False
-        if group[signal_name].shape != group[asn].shape:
-            _nxdata_warning("Auxiliary signal dataset '%s' does not" % asn +
-                            " have the same shape as the main signal.",
-                            group.name)
-            return False
+    auxiliary_signals_names = _get_auxiliary_signals_names(group)
+    _validate_auxiliary_signals(group, signal_name, auxiliary_signals_names)
 
     ndim = len(group[signal_name].shape)
 
