4b14852271e03cb8d2ba25ae7d21b896262f2f3d
Valentin Valls <valentin.valls@esrf.fr>
2018-05-04 14:49:26 +0200

    First step to dnd datasets

--- silx/app/view/CustomNxdataWidget.py
+++ silx/app/view/CustomNxdataWidget.py
@@ -26,11 +26,44 @@
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "03/05/2018"
+__date__ = "04/05/2018"
 
 from silx.gui import qt
 from silx.io import commonh5
 import silx.io.nxdata
+
+
+class _DatasetItemRow(qt.QStandardItem):
+
+    def __init__(self, label, dataset=None):
+        super(_DatasetItemRow, self).__init__(label)
+        self.setEditable(False)
+        self.setDropEnabled(False)
+        self.setDragEnabled(False)
+
+        self.__name = qt.QStandardItem()
+        self.__name.setEditable(False)
+        self.__name.setDropEnabled(True)
+        self.setDataset(dataset)
+
+    def setDataset(self, dataset):
+        self.__dataset = dataset
+        if self.__dataset is not None:
+            name = self.__dataset.name
+        else:
+            name = ""
+        self.__name.setText(name)
+        self.__name.setDragEnabled(self.__dataset is not None)
+
+        parent = self.parent()
+        if parent is not None:
+            self.parent()._datasetUpdated()
+
+    def getDataset(self):
+        return self.__dataset
+
+    def getRow(self):
+        return [self, self.__name]
 
 
 class _NxDataItem(qt.QStandardItem):
@@ -40,44 +73,21 @@ class _NxDataItem(qt.QStandardItem):
         self.__error = None
         self.__title = None
         self.__signal = None
-        self.__axes = []
+        self.__axes = None
         self.__virtual = None
         self.setEditable(False)
+        self.setDragEnabled(False)
+        self.setDropEnabled(False)
 
     def getRow(self):
         dataset = qt.QStandardItem("")
+        dataset.setEditable(False)
+        dataset.setDragEnabled(False)
+        dataset.setDropEnabled(False)
         return [self, dataset]
 
-    def _invalidate(self):
-        self.removeRows(0, self.rowCount())
+    def _datasetUpdated(self):
         self.__virtual = None
-
-        if self.__signal is not None:
-            name = qt.QStandardItem("Signal")
-            name.setEditable(False)
-            value = qt.QStandardItem(self.__signal.name)
-            value.setEditable(False)
-            self.appendRow([name, value])
-        else:
-            name = qt.QStandardItem("Signal")
-            name.setEditable(False)
-            value = qt.QStandardItem("")
-            value.setEditable(False)
-            self.appendRow([name, value])
-
-        for i in range(len(self.__axes)):
-            name = qt.QStandardItem("Axis %d" % (i + 1))
-            name.setEditable(False)
-            axis = None
-            if i < len(self.__axes):
-                axis = self.__axes[i]
-            if axis is not None:
-                value = qt.QStandardItem(axis.name)
-                value.setEditable(False)
-            else:
-                value = qt.QStandardItem("")
-                value.setEditable(False)
-            self.appendRow([name, value])
 
     def createVirtualGroup(self):
         name = ""
@@ -90,27 +100,29 @@ class _NxDataItem(qt.QStandardItem):
             virtual.attrs["title"] = self.__title
 
         if self.__signal is not None:
-            # Could be done using a link instead of a copy
-            node = commonh5.Dataset("signal", self.__signal[...])
-            virtual.attrs["signal"] = "signal"
-            virtual.add_node(node)
+            signal = self.__signal.getDataset()
+            if signal is not None:
+                # Could be done using a link instead of a copy
+                node = commonh5.Dataset("signal", signal[...])
+                virtual.attrs["signal"] = "signal"
+                virtual.add_node(node)
 
         axesAttr = []
-        for i in range(len(self.__axes)):
-            axis = None
-            if i < len(self.__axes):
-                axis = self.__axes[i]
-            if axis is not None:
-                # Could be done using a link instead of a copy
-                axis = axis[...]
-            if axis is not None:
-                name = "axis%d" % i
-                node = commonh5.Dataset(name, axis)
-                virtual.add_node(node)
-            else:
-                name = "."
-            axesAttr.append(name)
-        virtual.attrs["axes"] = axesAttr
+        if self.__axes is not None:
+            for i, axis in enumerate(self.__axes):
+                if axis is None:
+                    name = "."
+                else:
+                    axis = axis.getDataset()
+                    if axis is None:
+                        name = "."
+                    else:
+                        name = "axis%d" % i
+                        axis = axis[...]
+                        node = commonh5.Dataset(name, axis)
+                        virtual.add_node(node)
+                axesAttr.append(name)
+            virtual.attrs["axes"] = axesAttr
 
         if not silx.io.nxdata.is_valid_nxdata(virtual):
             self.setError("This NXdata is not consistant")
@@ -145,19 +157,109 @@ class _NxDataItem(qt.QStandardItem):
         return self.__error
 
     def setSignal(self, dataset):
-        self.__signal = dataset
-        self._invalidate()
+        # TODO: Do it well, bad design
+        if self.__signal is None:
+            item = _DatasetItemRow("Signal", dataset)
+            self.appendRow(item.getRow())
+            self.__signal = item
+        else:
+            self.__signal.setDataset(dataset)
+        self._datasetUpdated()
 
     def setAxes(self, datasets):
-        self.__axes = datasets
-        self._invalidate()
+        # TODO: Do it well, bad design
+        if self.__axes is None:
+            self.__axes = []
+            for i, dataset in enumerate(datasets):
+                label = "Axis %d" % (i + 1)
+                item = _DatasetItemRow(label, dataset)
+                self.__axes.append(item)
+                self.appendRow(item.getRow())
+        else:
+            for i, dataset in enumerate(datasets):
+                self.__axes[i].setDataset(dataset)
+        self._datasetUpdated()
+
+
+class Hdf5DatasetMimeData(qt.QMimeData):
+    """Mimedata class to identify an internal drag and drop of a Hdf5Node."""
+
+    MIME_TYPE = "application/x-internal-h5py-dataset"
+
+    def __init__(self, dataset=None):
+        qt.QMimeData.__init__(self)
+        self.__dataset = dataset
+        self.setData(self.MIME_TYPE, "".encode(encoding='utf-8'))
+
+    def getDataset(self):
+        return self.__dataset
+
+
+class _Model(qt.QStandardItemModel):
+
+    def __init__(self, parent=None):
+        qt.QStandardItemModel.__init__(self, parent)
+        root = self.invisibleRootItem()
+        root.setDropEnabled(False)
+        root.setDragEnabled(False)
+
+    def supportedDropActions(self):
+        return qt.Qt.CopyAction | qt.Qt.MoveAction
+
+    def mimeTypes(self):
+        return [Hdf5DatasetMimeData.MIME_TYPE]
+
+    def mimeData(self, indexes):
+        """
+        Returns an object that contains serialized items of data corresponding
+        to the list of indexes specified.
+
+        :param List[qt.QModelIndex] indexes: List of indexes
+        :rtype: qt.QMimeData
+        """
+        if len(indexes) > 1:
+            return None
+        if len(indexes) == 0:
+            return None
+
+        qindex = indexes[0]
+        qindex = self.index(qindex.row(), 0, parent=qindex.parent())
+        item = self.itemFromIndex(qindex)
+        if isinstance(item, _DatasetItemRow):
+            dataset = item.getDataset()
+            if dataset is None:
+                return None
+            else:
+                mimeData = Hdf5DatasetMimeData(dataset=item.getDataset())
+        else:
+            mimeData = None
+        return mimeData
+
+    def dropMimeData(self, mimedata, action, row, column, parentIndex):
+        if action == qt.Qt.IgnoreAction:
+            return True
+
+        if mimedata.hasFormat(Hdf5DatasetMimeData.MIME_TYPE):
+            if row != -1 or column != -1:
+                # It is not a drop on a specific item
+                return False
+            item = self.itemFromIndex(parentIndex)
+            item = item.parent().child(item.row(), 0)
+            if not isinstance(item, _DatasetItemRow):
+                return False
+
+            dataset = mimedata.getDataset()
+            item.setDataset(dataset)
+            return True
+
+        return False
 
 
 class CustomNxdataWidget(qt.QTreeView):
 
     def __init__(self, parent=None):
         qt.QTreeView.__init__(self, parent=None)
-        self.__model = qt.QStandardItemModel(self)
+        self.__model = _Model(self)
         self.__model.setColumnCount(2)
         self.__model.setHorizontalHeaderLabels(["Name", "Dataset"])
         self.setModel(self.__model)
@@ -168,6 +270,38 @@ class CustomNxdataWidget(qt.QTreeView):
         else:
             setResizeMode = header.setSectionResizeMode
         setResizeMode(0, qt.QHeaderView.ResizeToContents)
+
+        self.setSelectionMode(qt.QAbstractItemView.SingleSelection)
+        self.setDropIndicatorShown(True)
+        self.setDragDropOverwriteMode(True)
+        self.setDragEnabled(True)
+        self.viewport().setAcceptDrops(True)
+
+        self.setContextMenuPolicy(qt.Qt.CustomContextMenu)
+        self.customContextMenuRequested[qt.QPoint].connect(self.__executeContextMenu)
+
+    def defaultContextMenu(self, point):
+        qindex = self.indexAt(point)
+        qindex = self.__model.index(qindex.row(), 0, parent=qindex.parent())
+        item = self.__model.itemFromIndex(qindex)
+        if isinstance(item, _DatasetItemRow):
+            if item.getDataset() is not None:
+                menu = qt.QMenu()
+                action = qt.QAction("Remove this dataset", menu)
+                action.triggered.connect(lambda: self.__removeItemDataset(item))
+                menu.addAction(action)
+                return menu
+
+        return None
+
+    def __removeItemDataset(self, item):
+        item.setDataset(None)
+
+    def __executeContextMenu(self, point):
+        menu = self.defaultContextMenu(point)
+        if menu is None:
+            return
+        menu.exec_(qt.QCursor.pos())
 
     def getNxdataByTitle(self, title):
         for row in range(self.__model.rowCount()):
