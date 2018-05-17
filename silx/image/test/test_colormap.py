91942a028c56bce10a4ce11cf06b21707963c2b3
Thomas VINCENT <thomas.vincent@esrf.fr>
2018-05-17 12:11:24 +0200

    Add comparison with alternative implementation

--- silx/image/test/test_colormap.py
+++ silx/image/test/test_colormap.py
@@ -24,6 +24,8 @@
 # ############################################################################*/
 """Test for colormap mapping implementation"""
 
+from __future__ import division
+
 __authors__ = ["T. Vincent"]
 __license__ = "MIT"
 __date__ = "16/05/2018"
@@ -43,6 +45,37 @@ _logger = logging.getLogger(__name__)
 class TestColormap(ParametricTestCase):
     """Test silx.image.colormap.cmap"""
 
+    def ref_colormap(self, data, colors, vmin, vmax, normalization, nan_color):
+        """Reference implementation of colormap
+
+        :param numpy.ndarray data: Data to convert
+        :param numpy.ndarray colors: Color look-up-table
+        :param float vmin: Lower bound of the colormap range
+        :param float vmax: Upper bound of the colormap range
+        :param str normalization: Normalization to use
+        :param numpy.ndarray nan_color: Color to use for NaN values
+        """
+        norm_functions = {'linear': lambda v: v,
+                          'log': numpy.log10,
+                          'arcsinh': numpy.arcsinh,
+                          'sqrt': numpy.sqrt}
+
+        norm_function = norm_functions[normalization]
+        data, vmin, vmax = map(norm_function, (data, vmin, vmax))
+
+        nb_colors = len(colors)
+        scale = nb_colors / (vmax - vmin)
+
+        # Substraction must be done in float to avoid overflow with uint
+        indices = numpy.clip(scale * (data - float(vmin)), 0, nb_colors - 1)
+        indices[numpy.isnan(indices)] = nb_colors  # Use an extra index for NaN
+        indices = indices.astype('uint')
+
+        # Add NaN color to array
+        colors = numpy.append(colors, numpy.atleast_2d(nan_color), axis=0)
+
+        return colors[indices]
+
     def test(self):
         """Test all dtypes with finite data"""
         colors = numpy.zeros((256, 4), dtype=numpy.uint8)
@@ -50,8 +83,8 @@ class TestColormap(ParametricTestCase):
         colors[:, 3] = 255
 
         nan_color = (0, 0, 0, 0)
-        vmin = 1.
-        vmax = 7.
+        vmin = 1
+        vmax = 10
 
         # Generates (u)int and floats types
         dtypes = [e + k + i for e in '<>' for k in 'uif' for i in '1248'
@@ -62,11 +95,16 @@ class TestColormap(ParametricTestCase):
             for dtype in dtypes:
                 with self.subTest(dtype=dtype, normalization=normalization):
                     _logger.info('dtype: %s normalization: %s', dtype, normalization)
-                    data = numpy.arange(-10, 10, dtype=dtype).reshape(4, 5)
+                    data = numpy.arange(-5, 15, dtype=dtype).reshape(4, 5)
                     image = colormap.cmap(
                         data, colors, vmin, vmax, normalization, nan_color)
-                    self.assertEqual(image.shape, (4, 5, 4))
+
+                    ref_image = self.ref_colormap(
+                        data, colors, vmin, vmax, normalization, nan_color)
+
+                    self.assertTrue(numpy.allclose(ref_image, image))
                     self.assertEqual(image.dtype, colors.dtype)
+                    self.assertEqual(image.shape, (4, 5, 4))
 
 
 def suite():
