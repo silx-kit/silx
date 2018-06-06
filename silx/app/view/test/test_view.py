bf94577b9cf9422d3210c2612650677148581af8
Valentin Valls <valentin.valls@esrf.fr>
2018-06-06 16:44:19 +0200

    Test DataPanel

--- silx/app/view/test/test_view.py
+++ silx/app/view/test/test_view.py
@@ -31,10 +31,47 @@ __date__ = "06/06/2018"
 
 import unittest
 import weakref
+import numpy
+import tempfile
+import shutil
+import os.path
+try:
+    import h5py
+except ImportError:
+    h5py = None
 
 from silx.app.view.Viewer import Viewer
 from silx.app.view.About import About
+from silx.app.view.DataPanel import DataPanel
 from silx.gui.test.utils import TestCaseQt
+
+_tmpDirectory = None
+
+
+def setUpModule():
+    global _tmpDirectory
+    _tmpDirectory = tempfile.mkdtemp(prefix=__name__)
+
+    if h5py is not None:
+        # create h5 data
+        filename = _tmpDirectory + "/data.h5"
+        f = h5py.File(filename, "w")
+        g = f.create_group("arrays")
+        g.create_dataset("scalar", data=10)
+        f.close()
+
+        # create h5 data
+        filename = _tmpDirectory + "/data2.h5"
+        f = h5py.File(filename, "w")
+        g = f.create_group("arrays")
+        g.create_dataset("scalar", data=20)
+        f.close()
+
+
+def tearDownModule():
+    global _tmpDirectory
+    shutil.rmtree(_tmpDirectory)
+    _tmpDirectory = None
 
 
 class TestViewer(TestCaseQt):
@@ -70,11 +107,94 @@ class TestAbout(TestCaseQt):
         self.qWaitForDestroy(ref)
 
 
+class TestDataPanel(TestCaseQt):
+
+    def testConstruct(self):
+        widget = DataPanel()
+        self.qWaitForWindowExposed(widget)
+
+    def testDestroy(self):
+        widget = DataPanel()
+        ref = weakref.ref(widget)
+        widget = None
+        self.qWaitForDestroy(ref)
+
+    def testHeaderLabelPaintEvent(self):
+        widget = DataPanel()
+        data = numpy.array([1, 2, 3, 4, 5])
+        widget.setData(data)
+        # Expected to execute HeaderLabel.paintEvent
+        widget.setVisible(True)
+        self.qWaitForWindowExposed(widget)
+
+    def testData(self):
+        widget = DataPanel()
+        data = numpy.array([1, 2, 3, 4, 5])
+        widget.setData(data)
+        self.assertIs(widget.getData(), data)
+        self.assertIs(widget.getCustomNxdataItem(), None)
+
+    def testDataNone(self):
+        widget = DataPanel()
+        widget.setData(None)
+        self.assertIs(widget.getData(), None)
+        self.assertIs(widget.getCustomNxdataItem(), None)
+
+    def testCustomDataItem(self):
+        class CustomDataItemMock(object):
+            def getVirtualGroup(self):
+                return None
+
+            def text(self):
+                return ""
+
+        data = CustomDataItemMock()
+        widget = DataPanel()
+        widget.setCustomDataItem(data)
+        self.assertIs(widget.getData(), None)
+        self.assertIs(widget.getCustomNxdataItem(), data)
+
+    def testCustomDataItemNone(self):
+        data = None
+        widget = DataPanel()
+        widget.setCustomDataItem(data)
+        self.assertIs(widget.getData(), None)
+        self.assertIs(widget.getCustomNxdataItem(), data)
+
+    @unittest.skipIf(h5py is None, "Could not import h5py")
+    def testRemoveDatasetsFrom(self):
+        f = h5py.File(os.path.join(_tmpDirectory, "data.h5"))
+        try:
+            widget = DataPanel()
+            widget.setData(f["arrays/scalar"])
+            widget.removeDatasetsFrom(f)
+            self.assertIs(widget.getData(), None)
+        finally:
+            widget.setData(None)
+            f.close()
+
+    @unittest.skipIf(h5py is None, "Could not import h5py")
+    def testReplaceDatasetsFrom(self):
+        f = h5py.File(os.path.join(_tmpDirectory, "data.h5"))
+        f2 = h5py.File(os.path.join(_tmpDirectory, "data2.h5"))
+        try:
+            widget = DataPanel()
+            widget.setData(f["arrays/scalar"])
+            self.assertEqual(widget.getData()[()], 10)
+            widget.replaceDatasetsFrom(f, f2)
+            self.assertEqual(widget.getData()[()], 20)
+        finally:
+            widget.setData(None)
+            f.close()
+            f2.close()
+
+
 def suite():
     test_suite = unittest.TestSuite()
     loader = unittest.defaultTestLoader.loadTestsFromTestCase
     test_suite.addTest(loader(TestViewer))
     test_suite.addTest(loader(TestAbout))
+    test_suite.addTest(loader(TestDataPanel))
     return test_suite
 
 
