cb1af7fd697df8420b6af21f70d5835e12160b02
Valentin Valls <valentin.valls@esrf.fr>
2018-06-07 12:10:14 +0200

    Add tests for CustomNxdataWidget

--- silx/app/view/test/test_view.py
+++ silx/app/view/test/test_view.py
@@ -26,7 +26,7 @@
 
 __authors__ = ["V. Valls"]
 __license__ = "MIT"
-__date__ = "06/06/2018"
+__date__ = "07/06/2018"
 
 
 import unittest
@@ -40,10 +40,14 @@ try:
 except ImportError:
     h5py = None
 
+from silx.gui import qt
 from silx.app.view.Viewer import Viewer
 from silx.app.view.About import About
 from silx.app.view.DataPanel import DataPanel
+from silx.app.view.CustomNxdataWidget import CustomNxdataWidget
+from silx.gui.hdf5._utils import Hdf5DatasetMimeData
 from silx.gui.test.utils import TestCaseQt
+from silx.io import commonh5
 
 _tmpDirectory = None
 
@@ -58,6 +62,7 @@ def setUpModule():
         f = h5py.File(filename, "w")
         g = f.create_group("arrays")
         g.create_dataset("scalar", data=10)
+        g.create_dataset("integers", data=numpy.array([10, 20, 30]))
         f.close()
 
         # create h5 data
@@ -65,6 +70,7 @@ def setUpModule():
         f = h5py.File(filename, "w")
         g = f.create_group("arrays")
         g.create_dataset("scalar", data=20)
+        g.create_dataset("integers", data=numpy.array([10, 20, 30]))
         f.close()
 
 
@@ -189,12 +195,206 @@ class TestDataPanel(TestCaseQt):
             f2.close()
 
 
+class TestCustomNxdataWidget(TestCaseQt):
+
+    def testConstruct(self):
+        widget = CustomNxdataWidget()
+        self.qWaitForWindowExposed(widget)
+
+    def testDestroy(self):
+        widget = CustomNxdataWidget()
+        ref = weakref.ref(widget)
+        widget = None
+        self.qWaitForDestroy(ref)
+
+    def testCreateNxdata(self):
+        widget = CustomNxdataWidget()
+        model = widget.model()
+        model.createNewNxdata()
+        model.createNewNxdata("Foo")
+        widget.setVisible(True)
+        self.qWaitForWindowExposed(widget)
+
+    def testCreateNxdataFromDataset(self):
+        widget = CustomNxdataWidget()
+        model = widget.model()
+        signal = commonh5.Dataset("foo", data=numpy.array([[[5]]]))
+        model.createFromSignal(signal)
+        widget.setVisible(True)
+        self.qWaitForWindowExposed(widget)
+
+    def testCreateNxdataFromNxdata(self):
+        widget = CustomNxdataWidget()
+        model = widget.model()
+        data = numpy.array([[[5]]])
+        nxdata = commonh5.Group("foo")
+        nxdata.attrs["NX_class"] = "NXdata"
+        nxdata.attrs["signal"] = "signal"
+        nxdata.create_dataset("signal", data=data)
+        model.createFromNxdata(nxdata)
+        widget.setVisible(True)
+        self.qWaitForWindowExposed(widget)
+
+    def testCreateBadNxdata(self):
+        widget = CustomNxdataWidget()
+        model = widget.model()
+        signal = commonh5.Dataset("foo", data=numpy.array([[[5]]]))
+        model.createFromSignal(signal)
+        axis = commonh5.Dataset("foo", data=numpy.array([[[5]]]))
+        nxdataIndex = model.index(0, 0)
+        item = model.itemFromIndex(nxdataIndex)
+        item.setAxesDatasets([axis])
+        nxdata = item.getVirtualGroup()
+        self.assertIsNotNone(nxdata)
+        self.assertFalse(item.isValid())
+
+    @unittest.skipIf(h5py is None, "Could not import h5py")
+    def testRemoveDatasetsFrom(self):
+        f = h5py.File(os.path.join(_tmpDirectory, "data.h5"))
+        try:
+            widget = CustomNxdataWidget()
+            model = widget.model()
+            dataset = f["arrays/integers"]
+            model.createFromSignal(dataset)
+            widget.removeDatasetsFrom(f)
+        finally:
+            model.clear()
+            f.close()
+
+    @unittest.skipIf(h5py is None, "Could not import h5py")
+    def testReplaceDatasetsFrom(self):
+        f = h5py.File(os.path.join(_tmpDirectory, "data.h5"))
+        f2 = h5py.File(os.path.join(_tmpDirectory, "data2.h5"))
+        try:
+            widget = CustomNxdataWidget()
+            model = widget.model()
+            dataset = f["arrays/integers"]
+            model.createFromSignal(dataset)
+            widget.replaceDatasetsFrom(f, f2)
+        finally:
+            model.clear()
+            f.close()
+            f2.close()
+
+
+class TestCustomNxdataWidgetInteraction(TestCaseQt):
+    """Test CustomNxdataWidget with user interaction"""
+
+    def setUp(self):
+        TestCaseQt.setUp(self)
+
+        self.widget = CustomNxdataWidget()
+        self.model = self.widget.model()
+        data = numpy.array([[[5]]])
+        dataset = commonh5.Dataset("foo", data=data)
+        self.model.createFromSignal(dataset)
+        self.selectionModel = self.widget.selectionModel()
+
+    def tearDown(self):
+        self.selectionModel = None
+        self.model.clear()
+        self.model = None
+        self.widget = None
+        TestCaseQt.tearDown(self)
+
+    def testSelectedNxdata(self):
+        index = self.model.index(0, 0)
+        self.selectionModel.setCurrentIndex(index, qt.QItemSelectionModel.ClearAndSelect)
+        nxdata = self.widget.selectedNxdata()
+        self.assertEqual(len(nxdata), 1)
+        self.assertIsNot(nxdata[0], None)
+
+    def testSelectedItems(self):
+        index = self.model.index(0, 0)
+        self.selectionModel.setCurrentIndex(index, qt.QItemSelectionModel.ClearAndSelect)
+        items = self.widget.selectedItems()
+        self.assertEqual(len(items), 1)
+        self.assertIsNot(items[0], None)
+        self.assertIsInstance(items[0], qt.QStandardItem)
+
+    def testRowsAboutToBeRemoved(self):
+        self.model.removeRow(0)
+        self.qWaitForWindowExposed(self.widget)
+
+    def testPaintItems(self):
+        self.widget.expandAll()
+        self.widget.setVisible(True)
+        self.qWaitForWindowExposed(self.widget)
+
+    def testCreateDefaultContextMenu(self):
+        nxDataIndex = self.model.index(0, 0)
+        menu = self.widget.createDefaultContextMenu(nxDataIndex)
+        self.assertIsNot(menu, None)
+        self.assertIsInstance(menu, qt.QMenu)
+
+        signalIndex = self.model.index(0, 0, nxDataIndex)
+        menu = self.widget.createDefaultContextMenu(signalIndex)
+        self.assertIsNot(menu, None)
+        self.assertIsInstance(menu, qt.QMenu)
+
+        axesIndex = self.model.index(1, 0, nxDataIndex)
+        menu = self.widget.createDefaultContextMenu(axesIndex)
+        self.assertIsNot(menu, None)
+        self.assertIsInstance(menu, qt.QMenu)
+
+    def testDropNewDataset(self):
+        dataset = commonh5.Dataset("foo", numpy.array([1, 2, 3, 4]))
+        mimedata = Hdf5DatasetMimeData(dataset=dataset)
+        self.model.dropMimeData(mimedata, qt.Qt.CopyAction, -1, -1, qt.QModelIndex())
+        self.assertEqual(self.model.rowCount(qt.QModelIndex()), 2)
+
+    def testDropNewNxdata(self):
+        data = numpy.array([[[5]]])
+        nxdata = commonh5.Group("foo")
+        nxdata.attrs["NX_class"] = "NXdata"
+        nxdata.attrs["signal"] = "signal"
+        nxdata.create_dataset("signal", data=data)
+        mimedata = Hdf5DatasetMimeData(dataset=nxdata)
+        self.model.dropMimeData(mimedata, qt.Qt.CopyAction, -1, -1, qt.QModelIndex())
+        self.assertEqual(self.model.rowCount(qt.QModelIndex()), 2)
+
+    def testDropAxisDataset(self):
+        dataset = commonh5.Dataset("foo", numpy.array([1, 2, 3, 4]))
+        mimedata = Hdf5DatasetMimeData(dataset=dataset)
+        nxDataIndex = self.model.index(0, 0)
+        axesIndex = self.model.index(1, 0, nxDataIndex)
+        self.model.dropMimeData(mimedata, qt.Qt.CopyAction, -1, -1, axesIndex)
+        self.assertEqual(self.model.rowCount(qt.QModelIndex()), 1)
+        item = self.model.itemFromIndex(axesIndex)
+        self.assertIsNot(item.getDataset(), None)
+
+    def testMimeData(self):
+        nxDataIndex = self.model.index(0, 0)
+        signalIndex = self.model.index(0, 0, nxDataIndex)
+        mimeData = self.model.mimeData([signalIndex])
+        self.assertIsNot(mimeData, None)
+        self.assertIsInstance(mimeData, qt.QMimeData)
+
+    def testRemoveNxdataItem(self):
+        nxdataIndex = self.model.index(0, 0)
+        item = self.model.itemFromIndex(nxdataIndex)
+        self.model.removeNxdataItem(item)
+
+    def testAppendAxisToNxdataItem(self):
+        nxdataIndex = self.model.index(0, 0)
+        item = self.model.itemFromIndex(nxdataIndex)
+        self.model.appendAxisToNxdataItem(item)
+
+    def testRemoveAxisItem(self):
+        nxdataIndex = self.model.index(0, 0)
+        axesIndex = self.model.index(1, 0, nxdataIndex)
+        item = self.model.itemFromIndex(axesIndex)
+        self.model.removeAxisItem(item)
+
+
 def suite():
     test_suite = unittest.TestSuite()
     loader = unittest.defaultTestLoader.loadTestsFromTestCase
     test_suite.addTest(loader(TestViewer))
     test_suite.addTest(loader(TestAbout))
     test_suite.addTest(loader(TestDataPanel))
+    test_suite.addTest(loader(TestCustomNxdataWidget))
+    test_suite.addTest(loader(TestCustomNxdataWidgetInteraction))
     return test_suite
 
 
