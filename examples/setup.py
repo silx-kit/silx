870b131bfa66e31da3664ff45da565731bbb776c
Valentin Valls <valentin.valls@esrf.fr>
2018-07-26 17:28:06 +0200

    Ship examples as silx.examples

--- examples/setup.py
+++ examples/setup.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 # /*##########################################################################
-#
-# Copyright (c) 2015-2018 European Synchrotron Radiation Facility
+# Copyright (C) 2016 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,9 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-# ###########################################################################*/
+# ############################################################################*/
 
-__authors__ = ["T. Vincent"]
+__authors__ = ["V. Valls"]
 __license__ = "MIT"
 __date__ = "26/07/2018"
 
@@ -31,25 +30,10 @@ from numpy.distutils.misc_util import Configuration
 
 
 def configuration(parent_package='', top_path=None):
-    config = Configuration('silx', parent_package, top_path)
-    config.add_subpackage('gui')
-    config.add_subpackage('io')
-    config.add_subpackage('math')
-    config.add_subpackage('image')
-    config.add_subpackage('opencl')
-    config.add_subpackage('resources')
-    config.add_subpackage('sx')
-    config.add_subpackage('sx.test')
-    config.add_subpackage('test')
-    config.add_subpackage('third_party')
-    config.add_subpackage('utils')
-    config.add_subpackage('app')
-    config.add_subpackage("examples", "../examples")
-
+    config = Configuration('examples', parent_package, top_path)
     return config
 
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
-
     setup(configuration=configuration)
