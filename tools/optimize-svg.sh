52ec43685f8ec6c77588f2a98ce9ecdeb33604a2
Valentin Valls <valentin.valls@esrf.fr>
2018-07-27 11:39:58 +0200

    Add tools to manage resources

---
+++ tools/optimize-svg.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+#    Project: Silx
+#             https://github.com/silx-kit/silx
+#
+#    Copyright (C) 2018 European Synchrotron Radiation Facility, Grenoble, France
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE
+
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 [SVG_FILENAMES]"
+    echo
+    echo "Optimize a list of SVG files"
+    echo "The original files will be  lost"
+    exit
+fi
+
+
+scour_options=--enable-viewboxing --enable-id-stripping --enable-comment-stripping --shorten-ids --indent=none --remove-metadata --disable-embed-rasters
+
+for filename in "$@"
+do
+    echo "Optimize $filename"
+    python3.5 -m scour.scour -i $filename -o "${filename}__scour" $scour_options
+    rm $filename
+    mv "${filename}__scour" $filename
+done
+
