832b16f18093127b3ef9e3958a3a44f5f6233a0b
Thomas VINCENT <thomas.vincent@esrf.fr>
2018-09-05 09:46:48 +0000

    Set all sip API to v2

--- silx/gui/qt/_qt.py
+++ silx/gui/qt/_qt.py
@@ -101,6 +101,11 @@ if BINDING == 'PyQt4':
 
             sip.setapi("QString", 2)
             sip.setapi("QVariant", 2)
+            sip.setapi('QDate', 2)
+            sip.setapi('QDateTime', 2)
+            sip.setapi('QTextStream', 2)
+            sip.setapi('QTime', 2)
+            sip.setapi('QUrl', 2)
         except:
             _logger.warning("Cannot set sip API")
 
