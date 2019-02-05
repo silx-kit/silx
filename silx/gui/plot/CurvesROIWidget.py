4b042237fbf43b3872b505f483c8424e21328112
V. Armando Solé <vasole@users.noreply.github.com>
2019-02-05 16:04:11 +0100

    Update CurvesROIWidget.py
    Closes #2388

--- silx/gui/plot/CurvesROIWidget.py
+++ silx/gui/plot/CurvesROIWidget.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2004-2018 European Synchrotron Radiation Facility
+# Copyright (c) 2004-2019 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -195,8 +195,6 @@ class CurvesROIWidget(qt.QWidget):
     def getRois(self, order=None):
         return self.roiTable.getRois(order)
 
-    @deprecation.deprecated(reason="Moved to ROITable",
-                            since_version="0.10.0")
     def setMiddleROIMarkerFlag(self, flag=True):
         return self.roiTable.setMiddleROIMarkerFlag(flag)
 
