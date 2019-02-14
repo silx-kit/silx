295939831e71a7d08f6bdd4370258e80917e24b4
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-02-14 14:28:49 +0100

    Set default symbol to 'o' and override in curves to ''

--- silx/gui/plot/items/curve.py
+++ silx/gui/plot/items/curve.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2017-2018 European Synchrotron Radiation Facility
+# Copyright (c) 2017-2019 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -159,6 +159,9 @@ class Curve(Points, ColorMixIn, YAxisMixIn, FillMixIn, LabelsMixIn, LineMixIn):
 
     _DEFAULT_SELECTABLE = True
     """Default selectable state for curves"""
+
+    _DEFAULT_SYMBOL = ''
+    """Default symbol of the curve"""
 
     _DEFAULT_LINEWIDTH = 1.
     """Default line width of the curve"""
