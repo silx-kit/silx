1525bd91a90677a177580b2e5b6e4d2afa019f56
payno <henri.payno@esrf.fr>
2019-02-20 11:23:38 +0100

    Move to 0.11 dev

--- version.py
+++ version.py
@@ -67,9 +67,9 @@ RELEASE_LEVEL_VALUE = {"dev": 0,
                        "final": 15}
 
 MAJOR = 0
-MINOR = 10
+MINOR = 11
 MICRO = 0
-RELEV = "final"  # <16
+RELEV = "dev"  # <16
 SERIAL = 0  # <16
 
 date = __date__
