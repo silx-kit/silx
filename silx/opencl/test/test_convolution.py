9ad98bac0ad97bc88b291a8a1a6a7ccd65c75639
Pierre Paleo <pierre.paleo@esrf.fr>
2019-02-27 11:39:55 +0100

    Refactor test for less code duplication

--- silx/opencl/test/test_convolution.py
+++ silx/opencl/test/test_convolution.py
@@ -77,10 +77,12 @@ class TestConvolution(unittest.TestCase):
     def setUpClass(cls):
         super(TestConvolution, cls).setUpClass()
         cls.image = np.ascontiguousarray(ascent()[:, :511], dtype="f")
+        cls.data1d = cls.image[0]
+        cls.data2d = cls.image
         cls.data3d = np.tile(cls.image[224:-224, 224:-224], (62, 1, 1))
-        cls.kernel = gaussian_kernel(1.)
-        cls.kernel2d = np.outer(cls.kernel, cls.kernel)
-        cls.kernel3d = np.multiply.outer(cls.kernel2d, cls.kernel)
+        cls.kernel1d = gaussian_kernel(1.)
+        cls.kernel2d = np.outer(cls.kernel1d, cls.kernel1d)
+        cls.kernel3d = np.multiply.outer(cls.kernel2d, cls.kernel1d)
         cls.ctx = ocl.create_context()
         cls.tol = {
             "1D": 1e-4,
@@ -90,7 +92,8 @@ class TestConvolution(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        cls.image = cls.data3d = cls.kernel = cls.kernel2d = cls.kernel3d = None
+        cls.data1d = cls.data2d = cls.data3d = cls.image = None
+        cls.kernel1d = cls.kernel2d = cls.kernel3d = None
 
     @staticmethod
     def compare(arr1, arr2):
@@ -137,67 +140,76 @@ class TestConvolution(unittest.TestCase):
         )
         return C
 
+    def get_data_and_kernel(self, test_name):
+        dims = {
+            "test_1D": (1, 1),
+            "test_separable_2D": (2, 1),
+            "test_separable_3D": (3, 1),
+            "test_nonseparable_2D": (2, 2),
+            "test_nonseparable_3D":  (3, 3),
+        }
+        dim_data = {
+            1: self.data1d,
+            2: self.data2d,
+            3: self.data3d
+        }
+        dim_kernel = {
+            1: self.kernel1d,
+            2: self.kernel2d,
+            3: self.kernel3d,
+        }
+        dd, kd = dims[test_name]
+        return dim_data[dd], dim_kernel[kd]
+
+    def get_reference_function(self, test_name):
+        ref_func = {
+            "test_1D":
+                lambda x, y : scipy_convolve1d(x, y, mode=self.mode),
+            "test_separable_2D":
+                lambda x, y : scipy_convolve1d(
+                    scipy_convolve1d(x, y, mode=self.mode, axis=1),
+                    y, mode=self.mode, axis=0
+                ),
+            "test_separable_3D":
+                lambda x, y: scipy_convolve1d(
+                    scipy_convolve1d(
+                        scipy_convolve1d(x, y, mode=self.mode, axis=2),
+                        y, mode=self.mode, axis=1),
+                    y, mode=self.mode, axis=0
+                ),
+            "test_nonseparable_2D":
+                lambda x, y: scipy_convolve(x, y, mode=self.mode),
+            "test_nonseparable_3D":
+                lambda x, y : scipy_convolve(x, y, mode=self.mode),
+        }
+        return ref_func[test_name]
+
+    def template_test(self, test_name):
+        data, kernel = self.get_data_and_kernel(test_name)
+        conv = self.instantiate_convol(data.shape, kernel)
+        res = conv(data)
+        ref_func = self.get_reference_function(test_name)
+        ref = ref_func(data, kernel)
+        metric = self.compare(res, ref)
+        logger.info("%s: max error = %.2e" % (test_name, metric))
+        tol = self.tol[str("%dD" % kernel.ndim)]
+        self.assertLess(metric, tol, self.print_err(conv))
 
     def test_1D(self):
-        data = self.image[0]
-        conv = self.instantiate_convol(data.shape, self.kernel)
-        res = conv(data)
-        ref = scipy_convolve1d(data, self.kernel, mode=self.mode)
-        metric = self.compare(res, ref)
-        logger.info("test_1D: max error = %.2e" % metric)
-        self.assertLess(metric, self.tol["1D"], self.print_err(conv))
+        self.template_test("test_1D")
 
-
-    #~ @unittest.skipUnless(False, "WIP")
     def test_separable_2D(self):
-        data = self.image
-        conv = self.instantiate_convol(data.shape, self.kernel)
-        res = conv(data)
-        ref1 = scipy_convolve1d(data, self.kernel, mode=self.mode, axis=1)
-        ref = scipy_convolve1d(ref1, self.kernel, mode=self.mode, axis=0)
-        metric = self.compare(res, ref)
-        logger.info("test_separable_2D: max error = %.2e" % metric)
-        self.assertLess(metric, self.tol["1D"], self.print_err(conv))
+        self.template_test("test_separable_2D")
 
-
-    #~ @unittest.skipUnless(False, "WIP")
     def test_separable_3D(self):
-        data = np.tile(self.image, (64, 1, 1))
-        conv = self.instantiate_convol(data.shape, self.kernel)
-        res = conv(data)
-        ref1 = scipy_convolve1d(data, self.kernel, mode=self.mode, axis=2)
-        ref2 = scipy_convolve1d(ref1, self.kernel, mode=self.mode, axis=1)
-        ref = scipy_convolve1d(ref2, self.kernel, mode=self.mode, axis=0)
-        metric = self.compare(res, ref)
-        logger.info("test_separable_3D: max error = %.2e" % metric)
-        self.assertLess(metric, self.tol["1D"], self.print_err(conv))
+        self.template_test("test_separable_3D")
 
-
-    #~ @unittest.skipUnless(False, "WIP")
     def test_nonseparable_2D(self):
-        data = self.image
-        kernel = np.outer(self.kernel, self.kernel) # "non-separable" kernel
-        conv = self.instantiate_convol(data.shape, kernel)
-        res = conv(data)
-        ref = scipy_convolve(data, kernel, mode=self.mode)
-        metric = self.compare(res, ref)
-        logger.info("test_nonseparable_2D: max error = %.2e" % metric)
-        self.assertLess(metric, self.tol["2D"], self.print_err(conv))
+        self.template_test("test_nonseparable_2D")
 
-
-    #~ @unittest.skipUnless(False, "WIP")
     def test_nonseparable_3D(self):
-        data = self.data3d
-        kernel = self.kernel3d
-        conv = self.instantiate_convol(data.shape, kernel)
-        res = conv(data)
-        ref = scipy_convolve(data, kernel, mode=self.mode)
-        metric = self.compare(res, ref)
-        logger.info("test_nonseparable_3D: max error = %.2e" % metric)
-        self.assertLess(metric, self.tol["3D"], self.print_err(conv))
+        self.template_test("test_nonseparable_3D")
 
-
-    #~ @unittest.skipUnless(False, "WIP")
     def test_batched_2D(self):
         """
         Test batched (nonseparable) 2D convolution on 3D data.
