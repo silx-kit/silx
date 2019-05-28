c0f9783b1e443758a0a2940325f13fd5db47a898
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-05-28 17:03:57 +0200

    Avoid silx deprecation warning in test

--- silx/gui/test/test_colors.py
+++ silx/gui/test/test_colors.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2015-2018 European Synchrotron Radiation Facility
+# Copyright (c) 2015-2019 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -226,7 +226,7 @@ class TestObjectAPI(ParametricTestCase):
     def testCopy(self):
         """Make sure the copy function is correctly processing
         """
-        colormapObject = Colormap(name='red',
+        colormapObject = Colormap(name=None,
                                   colors=numpy.array([[1., 0., 0.],
                                                       [0., 1., 0.],
                                                       [0., 0., 1.]]),
