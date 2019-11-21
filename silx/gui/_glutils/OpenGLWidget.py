06b58133499367f3635033150a524251e1e87e38
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-11-21 09:15:58 +0100

    Add try except around first OpenGL call (exception can occur here)

--- silx/gui/_glutils/OpenGLWidget.py
+++ silx/gui/_glutils/OpenGLWidget.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2017 European Synchrotron Radiation Facility
+# Copyright (c) 2017-2019 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -192,7 +192,11 @@ else:
 
             # Check OpenGL version
             if self.getOpenGLVersion() >= self.getRequestedOpenGLVersion():
-                version = gl.glGetString(gl.GL_VERSION)
+                try:
+                    version = gl.glGetString(gl.GL_VERSION)
+                except:
+                    version = None
+
                 if version:
                     self.__isValid = True
                 else:
