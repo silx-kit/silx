c06084da6e11a09374903a035fab929ed8e8fc3b
payno <henri.payno@gmail.com>
2020-05-18 14:16:35 +0200

    move segments_intersection and lines_intersection into silx.gui.plot.utils.intesections

--- silx/gui/plot/items/roi.py
+++ silx/gui/plot/items/roi.py
@@ -44,6 +44,7 @@ from ...colors import rgba
 import silx.utils.deprecation
 from silx.image._boundingbox import _BoundingBox
 from ....utils.proxy import docstring
+from ..utils.intersections import segments_intersection
 
 
 logger = logging.getLogger(__name__)
@@ -938,71 +939,6 @@ class LineROI(_HandleBasedROI, items.LineMixIn):
         bb1 = _BoundingBox.from_points(self._points)
         if bb1.contains(position) is False:
             return False
-
-        def lines_intersection(line1_pt1, line1_pt2, line2_pt1, line2_pt2):
-            """
-            line segment intersection using vectors (Computer Graphics by F.S. Hill)
-
-            :param tuple line1_pt1:
-            :param tuple line1_pt2:
-            :param tuple line2_pt1:
-            :param tuple line2_pt2:
-            :return: Union[None,numpy.array]
-            """
-            dir_line1 = line1_pt2[0] - line1_pt1[0], line1_pt2[1] - line1_pt1[1]
-            dir_line2 = line2_pt2[0] - line2_pt1[0], line2_pt2[1] - line2_pt1[1]
-            dp = line1_pt1 - line2_pt1
-            def perp(a):
-                b = numpy.empty_like(a)
-                b[0] = -a[1]
-                b[1] = a[0]
-                return b
-
-            dap = perp(dir_line1)
-            denom = numpy.dot(dap, dir_line2)
-            num = numpy.dot(dap, dp)
-            if denom == 0:
-                return None
-            return (
-                (num / denom.astype(float)) * dir_line2[0] + line2_pt1[0],
-                (num / denom.astype(float)) * dir_line2[1] + line2_pt1[1])
-
-        def segments_intersection(seg1_start_pt, seg1_end_pt, seg2_start_pt,
-                                  seg2_end_pt):
-            """
-            Compute intersection between two segments
-
-            :param seg1_start_pt:
-            :param seg1_end_pt:
-            :param seg2_start_pt:
-            :param seg2_end_pt:
-            :return: numpy.array if an intersection exists, else None
-            :rtype: Union[None,numpy.array]
-            """
-            intersection = lines_intersection(line1_pt1=seg1_start_pt,
-                                              line1_pt2=seg1_end_pt,
-                                              line2_pt1=seg2_start_pt,
-                                              line2_pt2=seg2_end_pt)
-            if intersection is not None:
-                max_x_seg1 = max(seg1_start_pt[0], seg1_end_pt[0])
-                max_x_seg2 = max(seg2_start_pt[0], seg2_end_pt[0])
-                max_y_seg1 = max(seg1_start_pt[1], seg1_end_pt[1])
-                max_y_seg2 = max(seg2_start_pt[1], seg2_end_pt[1])
-
-                min_x_seg1 = min(seg1_start_pt[0], seg1_end_pt[0])
-                min_x_seg2 = min(seg2_start_pt[0], seg2_end_pt[0])
-                min_y_seg1 = min(seg1_start_pt[1], seg1_end_pt[1])
-                min_y_seg2 = min(seg2_start_pt[1], seg2_end_pt[1])
-
-                min_tmp_x = max(min_x_seg1, min_x_seg2)
-                max_tmp_x = min(max_x_seg1, max_x_seg2)
-                min_tmp_y = max(min_y_seg1, min_y_seg2)
-                max_tmp_y = min(max_y_seg1, max_y_seg2)
-                if (min_tmp_x <= intersection[0] <= max_tmp_x and
-                        min_tmp_y <= intersection[1] <= max_tmp_y):
-                    return intersection
-                else:
-                    return None
 
         return (
                 segments_intersection(seg1_start_pt=line_pt1, seg1_end_pt=line_pt2,
