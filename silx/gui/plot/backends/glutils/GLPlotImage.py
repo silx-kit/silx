a8c16db7aeba8dbd80b4ff9ed8726c8dee5e2b7f
Thomas VINCENT <thomas.vincent@esrf.fr>
2020-05-19 14:40:34 +0200

    fixes issues in OpenGL rendering

--- silx/gui/plot/backends/glutils/GLPlotImage.py
+++ silx/gui/plot/backends/glutils/GLPlotImage.py
@@ -197,7 +197,7 @@ class GLPlotColormap(_GLPlotData2D):
                 cmap_parameter);
         } else if (cmap_normalization == 4) { /* arcsinh mapping */
             /* asinh = log(x + sqrt(x*x + 1) for compatibility with GLSL 1.20 */
-             value = clamp(cmap.oneOverRange * (log(value + sqrt(value*value + 1)) - cmap.min), 0., 1.);
+             value = clamp(cmap_oneOverRange * (log(value + sqrt(value*value + 1)) - cmap_min), 0., 1.);
         } else { /*Linear mapping and fallback*/
             value = clamp(cmap_oneOverRange * (value - cmap_min), 0., 1.);
         }
@@ -338,6 +338,7 @@ class GLPlotColormap(_GLPlotData2D):
 
     def _setCMap(self, prog):
         dataMin, dataMax = self.cmapRange  # If log, it is stricly positive
+        param = 0.
 
         if self.data.dtype in (numpy.uint16, numpy.uint8):
             # Using unsigned int as normalized integer in OpenGL
@@ -348,12 +349,10 @@ class GLPlotColormap(_GLPlotData2D):
         if self.normalization == 'log':
             dataMin = math.log10(dataMin)
             dataMax = math.log10(dataMax)
-            param = 0.
             normID = 1
         elif self.normalization == 'sqrt':
             dataMin = math.sqrt(dataMin)
             dataMax = math.sqrt(dataMax)
-            param = 0.
             normID = 2
         elif self.normalization == 'gamma':
             # Keep dataMin, dataMax as is
@@ -364,7 +363,6 @@ class GLPlotColormap(_GLPlotData2D):
             dataMax = numpy.arcsinh(dataMax)
             normID = 4
         else:  # Linear and fallback
-            param = 0.
             normID = 0
 
         gl.glUniform1i(prog.uniforms['cmap_texture'],
