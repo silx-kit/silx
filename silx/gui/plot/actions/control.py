95988108d8403a778f68c86438f48d055a5ba932
Valentin Valls <valentin.valls@esrf.fr>
2020-11-09 15:17:46 +0100

    Create an OpenGLAction action

--- silx/gui/plot/actions/control.py
+++ silx/gui/plot/actions/control.py
@@ -632,3 +632,61 @@ class ClosePolygonInteractionAction(PlotAction):
 
     def _actionTriggered(self, checked=False):
         self.plot._eventHandler.validate()
+
+
+class OpenGLAction(PlotAction):
+    """QAction controlling rendering of a :class:`.PlotWidget`.
+
+    For now it can enable or not the OpenGL backend.
+
+    :param plot: :class:`.PlotWidget` instance on which to operate
+    :param parent: See :class:`QAction`
+    """
+
+    def __init__(self, plot, parent=None):
+        # Uses two images for checked/unchecked states
+        self._states = {
+            "opengl": (icons.getQIcon('backend-opengl'),
+                       "OpenGL rendering (fast)"),
+            "matplotlib": (icons.getQIcon('backend-opengl'),
+                           "Matplotlib rendering (safe)"),
+            "unknown": (icons.getQIcon('backend-opengl'),
+                        "Custom rendering")
+        }
+
+        name = self._getBackendName(plot)
+        icon, tooltip = self._states[name]
+        super(OpenGLAction, self).__init__(
+            plot,
+            icon=icon,
+            text='Enable/disable OpenGL rendering',
+            tooltip=tooltip,
+            triggered=self._actionTriggered,
+            checkable=True,
+            parent=parent)
+
+    def _backendUpdated(self):
+        name = self._getBackendName(self.plot)
+        icon, tooltip = self._states[name]
+        self.setIcon(icon)
+        self.setToolTip(tooltip)
+        self.setChecked(name == "opengl")
+
+    def _getBackendName(self, plot):
+        backend = plot.getBackend()
+        name = type(backend).__name__.lower()
+        if "opengl" in name:
+            return "opengl"
+        elif "matplotlib" in name:
+            return "matplotlib"
+        else:
+            return "unknown"
+
+    def _actionTriggered(self, checked=False):
+        plot = self.plot
+        name = self._getBackendName(self.plot)
+        if name is not "opengl":
+            plot.setBackend("opengl")
+        else:
+            plot.setBackend("matplotlib")
+        self._backendUpdated()
