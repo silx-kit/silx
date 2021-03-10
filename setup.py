0cda8f2892d00ec46945b14c5da7fd60c21fb966
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-03-10 16:27:52 +0100

    update debian_src target to retrieve version

--- setup.py
+++ setup.py
@@ -86,14 +86,14 @@ export LC_ALL=en_US.utf-8
 """)
 
 
-def get_version():
+def get_version(debian=False):
     """Returns current version number from _version.py file"""
     dirname = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "src", PROJECT)
     sys.path.insert(0, dirname)
     import _version
     sys.path = sys.path[1:]
-    return _version.strictversion
+    return _version.debianversion if debian else _version.strictversion
 
 
 def get_readme():
@@ -763,8 +763,7 @@ class sdist_debian(sdist):
 
     @staticmethod
     def get_debian_name():
-        import version
-        name = "%s_%s" % (PROJECT, version.debianversion)
+        name = "%s_%s" % (PROJECT, get_version(debian=True))
         return name
 
     def prune_file_list(self):
