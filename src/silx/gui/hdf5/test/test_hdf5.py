a42f8e78623bb8cf93996907ddb2f617be98d877
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-05-06 11:28:27 +0200

    make virtual datasets tests conditional

--- src/silx/gui/hdf5/test/test_hdf5.py
+++ src/silx/gui/hdf5/test/test_hdf5.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2016-2020 European Synchrotron Radiation Facility
+# Copyright (c) 2016-2021 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -34,6 +34,7 @@ import os
 import unittest
 import tempfile
 import numpy
+from pkg_resources import parse_version
 import shutil
 from contextlib import contextmanager
 from silx.gui import qt
@@ -44,9 +45,12 @@ from silx.io import commonh5
 import weakref
 
 import h5py
+import pytest
 
 
 _tmpDirectory = None
+
+h5pyHasVirtualDataset = parse_version(h5py.version.version) >= parse_version('2.9.0')
 
 
 def setUpModule():
@@ -628,11 +632,12 @@ class _TestModelBase(TestCaseQt):
         h5["broken_link/external_broken_link"] = h5py.ExternalLink(extH5FileName, "/target/not_exists")
         h5["broken_link/soft_broken_link"] = h5py.SoftLink("/group/not_exists")
         h5["broken_link/soft_link_to_broken_link"] = h5py.SoftLink("/group/not_exists")
-        layout = h5py.VirtualLayout((2,2), dtype=int)
-        layout[0] = h5py.VirtualSource("base__external.h5", name="/ext/vds0", shape=(2,), dtype=int)
-        layout[1] = h5py.VirtualSource("base__external.h5", name="/ext/vds1", shape=(2,), dtype=int)
         h5.create_group("/ext")
-        h5["/ext"].create_virtual_dataset("virtual", layout)
+        if h5pyHasVirtualDataset:
+            layout = h5py.VirtualLayout((2,2), dtype=int)
+            layout[0] = h5py.VirtualSource("base__external.h5", name="/ext/vds0", shape=(2,), dtype=int)
+            layout[1] = h5py.VirtualSource("base__external.h5", name="/ext/vds1", shape=(2,), dtype=int)
+            h5["/ext"].create_virtual_dataset("virtual", layout)
         external = [("base__external.dat", 0, 2*8), ("base__external.dat", 4*8, 2*8)]
         h5["/ext"].create_dataset("raw", shape=(2,2), dtype=int, external=external)
         h5.close()
@@ -761,6 +766,7 @@ class TestH5Item(_TestModelBase):
 
         self.assertEqual(h5item.dataLink(qt.Qt.DisplayRole), "")
 
+    @pytest.mark.skipif(not h5pyHasVirtualDataset, reason="h5py does not support virtual dataset")
     def testExternalVirtual(self):
         path = ["base.h5", "ext", "virtual"]
         h5item = self.getH5ItemFromPath(self.model, path)
@@ -941,6 +947,7 @@ class TestH5Node(_TestModelBase):
         self.assertEqual(h5node.local_basename, "dataset")
         self.assertEqual(h5node.local_name, "/link/soft_link_to_file/link/soft_link_to_group/dataset")
 
+    @pytest.mark.skipif(not h5pyHasVirtualDataset, reason="h5py does not support virtual dataset")
     def testExternalVirtual(self):
         path = ["base.h5", "ext", "virtual"]
         h5node = self.getH5NodeFromPath(self.model, path)
