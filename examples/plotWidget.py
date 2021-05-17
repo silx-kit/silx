067eb778741ff6e6bbdc7aecbaf302bb622949c5
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-05-17 15:56:16 +0200

    replace obsolete QPalette enum value

--- examples/plotWidget.py
+++ examples/plotWidget.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2017-2018 European Synchrotron Radiation Facility
+# Copyright (c) 2017-2021 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -64,7 +64,7 @@ class MyPlotWindow(qt.QMainWindow):
         # Make ColorBarWidget background white by changing its palette
         colorBar.setAutoFillBackground(True)
         palette = colorBar.palette()
-        palette.setColor(qt.QPalette.Background, qt.Qt.white)
+        palette.setColor(qt.QPalette.Window, qt.Qt.white)
         palette.setColor(qt.QPalette.Window, qt.Qt.white)
         colorBar.setPalette(palette)
 
