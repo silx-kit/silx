a99eba9d7b7d28a7bfc195a465695a7973d1fabf
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-09-01 13:57:50 +0200

    Rename PowerNormalization -> GammaNormalization

--- src/silx/math/test/test_colormap.py
+++ src/silx/math/test/test_colormap.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2018-2020 European Synchrotron Radiation Facility
+# Copyright (c) 2018-2021 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -113,8 +113,8 @@ class TestColormap(ParametricTestCase):
         'sqrt',
         colormap.LinearNormalization(),
         colormap.LogarithmicNormalization(),
-        colormap.PowerNormalization(2.),
-        colormap.PowerNormalization(0.5))
+        colormap.GammaNormalization(2.),
+        colormap.GammaNormalization(0.5))
 
     @staticmethod
     def ref_colormap(data, colors, vmin, vmax, normalization, nan_color):
