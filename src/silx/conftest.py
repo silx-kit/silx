8e94bc1a33a19438a2921d8f72b0f0c49dd3b8dc
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-09-01 15:55:58 +0200

    Add PySide6 to pytest --qt-binding option

--- src/silx/conftest.py
+++ src/silx/conftest.py
@@ -15,13 +15,16 @@ def _set_qt_binding(binding):
         elif binding == "pyside2":
             logger.info("Force using PySide2")
             import PySide2.QtCore  # noqa
+        elif binding == "pyside6":
+            logger.info("Force using PySide6")
+            import PySide6.QtCore  # noqa
         else:
             raise ValueError("Qt binding '%s' is unknown" % binding)
 
 
 def pytest_addoption(parser):
     parser.addoption("--qt-binding", type=str, default=None, dest="qt_binding",
-                     help="Force using a Qt binding: 'PyQt5', 'PySide2'")
+                     help="Force using a Qt binding: 'PyQt5', 'PySide2', 'PySide6'")
     parser.addoption("--no-gui", dest="gui", default=True,
                      action="store_false",
                      help="Disable the test of the graphical use interface")
