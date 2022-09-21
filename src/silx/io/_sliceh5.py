5c69fa83c559dd9cde8ba7cb1d4ae510317ac363
Thomas VINCENT <thomas.vincent@esrf.fr>
2022-09-21 11:21:15 +0200

    Improvelazy-loading of data

--- src/silx/io/_sliceh5.py
+++ src/silx/io/_sliceh5.py
@@ -21,22 +21,121 @@
 #
 # ############################################################################*/
 """Provides a wrapper to expose a dataset slice as a `commonh5.Dataset`."""
-from typing import Union
+from typing import Tuple, Union
+
+import h5py
+import numpy
+
 from . import commonh5
 from . import utils
 
-__authors__ = ["T. Vincent"]
-__license__ = "MIT"
-__date__ = "12/09/2022"
+
+IndexType = Union[int, slice, type(Ellipsis)]
+IndicesType = Union[IndexType, Tuple[IndexType, ...]]
+NormalisedIndicesType = Tuple[Union[int, slice], ...]
 
 
-class DatasetSlice(commonh5.LazyLoadableDataset):
-    """Lazy-loaded wrapper of a dataset's slice.
+def _expand_indices(
+    ndim: int,
+    indices: IndicesType,
+) -> NormalisedIndicesType:
+    """Replace Ellipsis and complete indices to match ndim"""
+    if not isinstance(indices, tuple):
+        indices = (indices,)
+
+    nb_ellipsis = indices.count(Ellipsis)
+    if nb_ellipsis > 1:
+        raise IndexError("an index can only have a single ellipsis ('...')")
+    if nb_ellipsis == 1:
+        ellipsis_index = indices.index(Ellipsis)
+        return (
+            indices[:ellipsis_index]
+            + (slice(None),) * max(0, (ndim - len(indices) + 1))
+            + indices[ellipsis_index + 1 :]
+        )
+
+    if len(indices) > ndim:
+        raise IndexError(
+            f"too many indices ({len(indices)}) for the number of dimensions ({ndim})"
+        )
+    return indices + (slice(None),) * (ndim - len(indices))
+
+
+def _get_selection_shape(
+    shape: Tuple[int, ...],
+    indices: NormalisedIndicesType,
+) -> Tuple[int, ...]:
+    """Returns the shape of the selection of indices in a dataset of the given shape"""
+    assert len(shape) == len(indices)
+
+    selected_indices = (
+        index.indices(length)
+        for length, index in zip(shape, indices)
+        if isinstance(index, slice)
+    )
+    return tuple(
+        int(max(0, numpy.ceil((stop - start) / stride)))
+        for start, stop, stride in selected_indices
+    )
+
+
+def _combine_indices(
+    outer_shape: Tuple[int, ...],
+    outer_indices: NormalisedIndicesType,
+    indices: IndicesType,
+) -> NormalisedIndicesType:
+    """Returns the combination of outer_indices and indices"""
+    inner_shape = _get_selection_shape(outer_shape, outer_indices)
+    inner_indices = _expand_indices(len(inner_shape), indices)
+    inner_iter = zip(range(len(inner_shape)), inner_shape, inner_indices)
+
+    combined_indices = []
+    for outer_length, outer_index in zip(outer_shape, outer_indices):
+        if isinstance(outer_index, int):
+            combined_indices.append(outer_index)
+            continue
+
+        outer_start, outer_stop, outer_stride = outer_index.indices(outer_length)
+        inner_axis, inner_length, inner_index = next(inner_iter)
+
+        if isinstance(inner_index, int):
+            if inner_index < -inner_length or inner_index >= inner_length:
+                raise IndexError(
+                    f"index {inner_index} is out of bounds for axis {inner_axis} with size {inner_length}"
+                )
+            index = outer_start + outer_stride * inner_index
+            if inner_index < 0:
+                index += outer_stride * inner_length
+            combined_indices.append(index)
+            continue
+
+        inner_start, inner_stop, inner_stride = inner_index.indices(inner_length)
+        combined_indices.append(
+            slice(
+                outer_start + outer_stride * inner_start,
+                outer_start + outer_stride * inner_stop,
+                outer_stride * inner_stride,
+            )
+        )
+
+    return tuple(combined_indices)
+
+
+class DatasetSlice(commonh5.Dataset):
+    """Wrapper a dataset indexed selection as a commonh5.Dataset.
 
     :param h5file: h5py-like file containing the dataset
     :param dataset: h5py-like dataset from which to access a slice
+    :param indices: The indexing to select
     """
-    def __init__(self, name: str, h5_file, dataset, data_slice: Union[int, slice]):
+
+    def __init__(
+        self,
+        name: str,
+        h5_file: Union[h5py.File, commonh5.File],
+        dataset: Union[h5py.Dataset, commonh5.Dataset],
+        indices: IndicesType,
+    ):
         if not utils.is_file(h5_file):
             raise ValueError(f"Unsupported h5_file '{h5_file}'")
         if not utils.is_dataset(dataset):
@@ -44,18 +143,60 @@ class DatasetSlice(commonh5.LazyLoadableDataset):
 
         self.__file = h5_file
         self.__dataset = dataset
-        self.__data_slice = data_slice
-        super().__init__(name)
+        self.__indices = _expand_indices(len(self.__dataset.shape), indices)
+        self.__shape = _get_selection_shape(self.__dataset.shape, self.__indices)
+        super().__init__(name, data=None, parent=h5_file)
 
-    def _create_data(self):
-        return self.__dataset[self.__data_slice]
+    def _get_data(self) -> Union[h5py.Dataset, commonh5.Dataset]:
+        # Give access to the underlying (h5py) dataset, not the selected data
+        # All commonh5.Dataset methods using _get_data must be overridden
+        return self.__dataset
 
     @property
-    def file(self):
+    def dtype(self) -> numpy.dtype:
+        return self.__dataset.dtype
+
+    @property
+    def shape(self) -> Tuple[int, ...]:
+        return self.__shape
+
+    @property
+    def size(self) -> int:
+        return numpy.prod(self.shape)
+
+    def __len__(self) -> int:
+        return self.shape[0]
+
+    def __getitem__(self, item):
+        if item is Ellipsis:
+            return numpy.array(self.__dataset[self.__indices], copy=False)
+        if item == ():
+            return self.__dataset[self.__indices]
+
+        if not self.__shape:
+            raise IndexError("invalid index to scalar variable.")
+
+        return self.__dataset[
+            _combine_indices(
+                self.__dataset.shape,
+                self.__indices,
+                item,
+            )
+        ]
+
+    @property
+    def value(self):
+        return self[()]
+
+    def __iter__(self):
+        return self[()].__iter__()
+
+    @property
+    def file(self) -> Union[h5py.File, commonh5.File]:
         return self.__file
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.basename
 
     def __enter__(self):
