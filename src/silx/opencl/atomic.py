bf74b9e40cfe8d71f9da919e10b2d029d5bd825c
Pierre Paleo <pierre.paleo@esrf.fr>
2023-06-15 17:03:01 +0200

    Slight change to header

--- src/silx/opencl/atomic.py
+++ src/silx/opencl/atomic.py
@@ -1,8 +1,8 @@
 #
-#    Project: Azimuthal integration
-#             https://github.com/silx-kit/pyFAI
+#    Project: S I L X project
+#             https://github.com/silx-kit/silx
 #
-#    Copyright (C) 2023-2023 European Synchrotron Radiation Facility, Grenoble, France
+#    Copyright (C) 2012-2023 European Synchrotron Radiation Facility, Grenoble, France
 #
 #    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
 #
