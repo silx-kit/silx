988928374a291613a76494ebda92b8ac77af0354
woutdenolf <woutdenolf@users.sf.net>
2023-07-13 09:54:48 +0200

    separate unit tests for peak evaluation and search

--- src/silx/math/fit/test/test_peaks.py
+++ src/silx/math/fit/test/test_peaks.py
@@ -24,115 +24,118 @@
 Tests for peaks module
 """
 
-import unittest
 import numpy
+import pytest
 
 from silx.math.fit import functions
 from silx.math.fit import peaks
 
-class Test_peak_search(unittest.TestCase):
-    """
-    Unit tests of peak_search on various types of multi-peak functions.
-    """
-    def setUp(self):
-        self.x = numpy.arange(5000)
-        # (height1, center1, fwhm1, ...)
-        self.h_c_fwhm = (50, 500, 100,
-                         50, 600, 80,
-                         20, 2000, 100,
-                         50, 2250, 110,
-                         40, 3000, 99,
-                         23, 4980, 80)
-        # (height1, center1, fwhm1, eta1 ...)
-        self.h_c_fwhm_eta = (50, 500, 100, 0.4,
-                             50, 600, 80, 0.5,
-                             20, 2000, 100, 0.6,
-                             50, 2250, 110, 0.7,
-                             40, 3000, 99, 0.8,
-                             23, 4980, 80, 0.3,)
-        # (height1, center1, fwhm11, fwhm21, ...)
-        self.h_c_fwhm_fwhm = (50, 500, 100, 85,
-                              50, 600, 80, 110,
-                              20, 2000, 100, 100,
-                              50, 2250, 110, 99,
-                              40, 3000, 99, 110,
-                              23, 4980, 80, 80,)
-        # (height1, center1, fwhm11, fwhm21, eta1 ...)
-        self.h_c_fwhm_fwhm_eta = (50, 500, 100, 85, 0.4,
-                                  50, 600, 80, 110, 0.5,
-                                  20, 2000, 100, 100, 0.6,
-                                  50, 2250, 110, 99, 0.7,
-                                  40, 3000, 99, 110, 0.8,
-                                  23, 4980, 80, 80, 0.3,)
-        # (height1, center1, fwhm11, fwhm21, eta11, eta21 ...)
-        self.h_c_fwhm_fwhm_eta_eta = (50, 500, 100, 85, 0.4, 0.7,
-                                  50, 600, 80, 110, 0.5, 0.3,
-                                  20, 2000, 100, 100, 0.6, 0.4,
-                                  50, 2250, 110, 99, 0.7, 1,
-                                  40, 3000, 99, 110, 0.8, 0,
-                                  23, 4980, 80, 80, 0.3, 0.5,)
-        # (area1, center1, fwhm1, ...)
-        self.a_c_fwhm = (2550, 500, 100,
-                         2000, 600, 80,
-                         500, 2000, 100,
-                         4000, 2250, 110,
-                         2300, 3000, 99,
-                         3333, 4980, 80)
-        # (area1, center1, fwhm1, eta1 ...)
-        self.a_c_fwhm_eta = (500, 500, 100, 0.4,
-                             500, 600, 80, 0.5,
-                             200, 2000, 100, 0.6,
-                             500, 2250, 110, 0.7,
-                             400, 3000, 99, 0.8,
-                             230, 4980, 80, 0.3,)
-        # (area, position, fwhm, st_area_r, st_slope_r, lt_area_r, lt_slope_r, step_height_r)
-        self.hypermet_params = (1000, 500, 200, 0.2, 100, 0.3, 100, 0.05,
-                                1000, 1000, 200, 0.2, 100, 0.3, 100, 0.05,
-                                1000, 2000, 200, 0.2, 100, 0.3, 100, 0.05,
-                                1000, 2350, 200, 0.2, 100, 0.3, 100, 0.05,
-                                1000, 3000, 200, 0.2, 100, 0.3, 100, 0.05,
-                                1000, 4900, 200, 0.2, 100, 0.3, 100, 0.05,)
+_PEAK_PARAMETERS = {
+    "sum_gauss": (50, 500, 100,
+                    50, 600, 80,
+                    20, 2000, 100,
+                    50, 2250, 110,
+                    40, 3000, 99,
+                    23, 4980, 80),
+    "sum_lorentz": (50, 500, 100,
+                    50, 600, 80,
+                    20, 2000, 100,
+                    50, 2250, 110,
+                    40, 3000, 99,
+                    23, 4980, 80),
+    "sum_pvoigt": (50, 500, 100, 0.4,
+                        50, 600, 80, 0.5,
+                        20, 2000, 100, 0.6,
+                        50, 2250, 110, 0.7,
+                        40, 3000, 99, 0.8,
+                        23, 4980, 80, 0.3,),
+    "sum_splitgauss": (50, 500, 100, 85,
+                        50, 600, 80, 110,
+                        20, 2000, 100, 100,
+                        50, 2250, 110, 99,
+                        40, 3000, 99, 110,
+                        23, 4980, 80, 80,),
+    "sum_splitlorentz": (50, 500, 100, 85,
+                        50, 600, 80, 110,
+                        20, 2000, 100, 100,
+                        50, 2250, 110, 99,
+                        40, 3000, 99, 110,
+                        23, 4980, 80, 80,),
+    "sum_splitpvoigt": (50, 500, 100, 85, 0.4,
+                            50, 600, 80, 110, 0.5,
+                            20, 2000, 100, 100, 0.6,
+                            50, 2250, 110, 99, 0.7,
+                            40, 3000, 99, 110, 0.8,
+                            23, 4980, 80, 80, 0.3,),
+    "sum_splitpvoigt2": (50, 500, 100, 85, 0.4, 0.7,
+                            50, 600, 80, 110, 0.5, 0.3,
+                            20, 2000, 100, 100, 0.6, 0.4,
+                            50, 2250, 110, 99, 0.7, 1,
+                            40, 3000, 99, 110, 0.8, 0,
+                            23, 4980, 80, 80, 0.3, 0.5,),
+    "sum_agauss": (2550, 500, 100,
+                    2000, 600, 80,
+                    500, 2000, 100,
+                    4000, 2250, 110,
+                    2300, 3000, 99,
+                    3333, 4980, 80),
+    "sum_fastagauss": (2550, 500, 100,
+                    2000, 600, 80,
+                    500, 2000, 100,
+                    4000, 2250, 110,
+                    2300, 3000, 99,
+                    3333, 4980, 80),
+    "sum_alorentz": (2550, 500, 100,
+                    2000, 600, 80,
+                    500, 2000, 100,
+                    4000, 2250, 110,
+                    2300, 3000, 99,
+                    3333, 4980, 80),
+    "sum_apvoigt": (500, 500, 100, 0.4,
+                        500, 600, 80, 0.5,
+                        200, 2000, 100, 0.6,
+                        500, 2250, 110, 0.7,
+                        400, 3000, 99, 0.8,
+                        230, 4980, 80, 0.3,),
+    "sum_ahypermet": (1000, 500, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 1000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 2000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 2350, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 3000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 4900, 200, 0.2, 100, 0.3, 100, 0.05,),
+    "sum_fastahypermet": (1000, 500, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 1000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 2000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 2350, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 3000, 200, 0.2, 100, 0.3, 100, 0.05,
+                        1000, 4900, 200, 0.2, 100, 0.3, 100, 0.05,)
+}
 
 
-    def tearDown(self):
-        pass
+@pytest.mark.parametrize("peak_profile", list(_PEAK_PARAMETERS))
+def test_peak_functions(peak_profile):
+    x = numpy.arange(5000)
+    peak_params = _PEAK_PARAMETERS[peak_profile]
+    func = getattr(functions, peak_profile)
 
-    def get_peaks(self, function, params):
-        """
+    with pytest.raises(IndexError):
+        func(x)
+    with pytest.raises(IndexError):
+        func(x, *peak_params, 0)
 
-        :param function: Multi-peak function
-        :param params: Parameter for this function
-        :return: list of (peak, relevance) tuples
-        """
-        y = function(self.x, *params)
-        return peaks.peak_search(y=y, fwhm=100, relevance_info=True)
+    y = func(x, *peak_params)
+    assert x.shape == y.shape
 
-    def testPeakSearch_various_functions(self):
-        """Run peak search on a variety of synthetic functions, and
-        check that result falls within +-25 samples of the actual peak
-        (reasonable delta considering a fwhm of ~100 samples) and effects
-        of overlapping peaks)."""
-        f_p = ((functions.sum_gauss, self.h_c_fwhm ),
-               (functions.sum_lorentz, self.h_c_fwhm),
-               (functions.sum_pvoigt, self.h_c_fwhm_eta),
-               (functions.sum_splitgauss, self.h_c_fwhm_fwhm),
-               (functions.sum_splitlorentz, self.h_c_fwhm_fwhm),
-               (functions.sum_splitpvoigt, self.h_c_fwhm_fwhm_eta),
-               (functions.sum_splitpvoigt2, self.h_c_fwhm_fwhm_eta_eta),
-               (functions.sum_agauss, self.a_c_fwhm),
-               (functions.sum_fastagauss, self.a_c_fwhm),
-               (functions.sum_alorentz, self.a_c_fwhm),
-               (functions.sum_apvoigt, self.a_c_fwhm_eta),
-               (functions.sum_ahypermet, self.hypermet_params),
-               (functions.sum_fastahypermet, self.hypermet_params),)
 
-        for function, params in f_p:
-            peaks = self.get_peaks(function, params)
+@pytest.mark.parametrize("peak_profile", list(_PEAK_PARAMETERS))
+def test_peak_search(peak_profile):
+    x = numpy.arange(5000)
+    peak_params = _PEAK_PARAMETERS[peak_profile]
+    func = getattr(functions, peak_profile)
+    y = func(x, *peak_params)
+    estimated_peak_params = peaks.peak_search(y=y, fwhm=100, relevance_info=True)
 
-            self.assertEqual(len(peaks), 6,
-                             "Wrong number of peaks detected")
-
-            for i in range(6):
-                theoretical_peak_index = params[i*(len(params)//6) + 1]
-                found_peak_index = peaks[i][0]
-                self.assertLess(abs(found_peak_index - theoretical_peak_index), 25)
+    assert len(estimated_peak_params)==6, "Wrong number of peaks detected"
+    for i, (peak_position, *_) in enumerate(estimated_peak_params):
+        theoretical_peak_position = peak_params[i*(len(peak_params)//6) + 1]
+        assert abs(peak_position - theoretical_peak_position) < 25
