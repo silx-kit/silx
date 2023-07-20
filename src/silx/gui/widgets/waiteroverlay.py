0b95d5fa73f6fb46e667d6992127de92c6bd7ecc
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-07-20 17:14:31 +0200

    Refactor parent handling, add support of backend update, use visibility instead of setWaiting

--- src/silx/gui/widgets/waiteroverlay.py
+++ src/silx/gui/widgets/waiteroverlay.py
@@ -2,6 +2,7 @@ import weakref
 from typing import Optional
 from silx.gui.widgets.WaitingPushButton import WaitingPushButton
 from silx.gui import qt
+from silx.gui.qt import inspect as qt_inspect
 from silx.gui.plot import PlotWidget
 
 
@@ -13,55 +14,73 @@ class WaiterOverlay(qt.QWidget):
 
     def __init__(self, parent: qt.QWidget) -> None:
         super().__init__(parent)
-        if isinstance(parent, PlotWidget):
-            parent = parent.getWidgetHandle()
+        self.setContentsMargins(0, 0, 0, 0)
 
-        if not isinstance(parent, qt.QWidget):
-            raise TypeError(f"parent must be an instance of QWidget. {type(parent)} provided.")
-
-        self._waitingButton = WaitingPushButton(
-            parent=parent,
-        )
+        self._waitingButton = WaitingPushButton(self)
         self._waitingButton.setDown(True)
         self._waitingButton.setVisible(False)
         self._waitingButton.setStyleSheet("QPushButton { background-color: rgba(150, 150, 150, 40); border: 0px; border-radius: 10px; }")
-        self._resize()
-        # register to resize event
-        parent.installEventFilter(self)
+        self._registerParent(parent)
 
     def setText(self, text: str):
         self._waitingButton.setText(text)
-    
-    def setParent(self, parent: qt.QWidget):
+
+    def _listenedWidget(self, parent: qt.QWidget) -> qt.QWidget:
+        """Returns widget to register event filter to according to parent"""
         if isinstance(parent, PlotWidget):
-            parent = parent.getWidgetHandle()
+            return parent.getWidgetHandle()
+        return parent
 
-        if self.parent() is not None:
-            self.parent().removeEventFilter(self)
+    def _backendChanged(self):
+        self._listenedWidget(self.parent()).installEventFilter(self)
+        self._resizeLater()
+
+    def _registerParent(self, parent: Optional[qt.QWidget]):
+        if parent is None:
+            return
+        self._listenedWidget(parent).installEventFilter(self)
+        if isinstance(parent, PlotWidget):
+            parent.sigBackendChanged.connect(self._backendChanged)
+        self._resize()
+
+    def _unregisterParent(self, parent: Optional[qt.QWidget]):
+        if parent is None:
+            return
+        if isinstance(parent, PlotWidget):
+            parent.sigBackendChanged.disconnect(self._backendChanged)
+        self._listenedWidget(parent).removeEventFilter(self)
+
+    def setParent(self, parent: qt.QWidget):
+        self._unregisterParent(self.parent())
         super().setParent(parent)
-        self._waitingButton.setParent(parent)
-        parent.installEventFilter(self)
+        self._registerParent(parent)
 
-    def close(self):
+    def showEvent(self, event: qt.QShowEvent):
+        super().showEvent(event)
+        self._waitingButton.setWaiting(True)
+        self._waitingButton.setVisible(True)
+
+    def hideEvent(self, event: qt.QHideEvent):
+        super().hideEvent(event)
         self._waitingButton.setWaiting(False)
-        super().close()
+        self._waitingButton.setVisible(False)
 
-    def setWaiting(self, activate: bool = True):
-        self._waitingButton.setWaiting(activate)
-        self._waitingButton.setVisible(activate)
-    
     def _resize(self):
+        if not qt_inspect.isValid(self):
+            return  # For _resizeLater in case the widget has been deleted
+
         parent = self.parent()
         if parent is None:
             return
 
         size = self._waitingButton.sizeHint()
         if isinstance(parent, PlotWidget):
+            offset = parent.getWidgetHandle().mapTo(parent, qt.QPoint(0, 0))
             left, top, width, height = parent.getPlotBoundsInPixels()
             rect = qt.QRect(
                 qt.QPoint(
-                    int(left + width / 2 - size.width() / 2),
-                    int(top + height / 2 - size.height() / 2),
+                    int(offset.x() + left + width / 2 - size.width() / 2),
+                    int(offset.y() + top + height / 2 - size.height() / 2),
                 ),
                 size,
             )
@@ -69,9 +88,14 @@ class WaiterOverlay(qt.QWidget):
             position = parent.size()
             position = (position - size) / 2
             rect = qt.QRect(qt.QPoint(position.width(), position.height()), size)
-        self._waitingButton.setGeometry(rect)
+        self.setGeometry(rect)
+        self.raise_()
+
+    def _resizeLater(self):
+        qt.QTimer.singleShot(0, self._resize)
 
     def eventFilter(self, watched: qt.QWidget, event: qt.QEvent):
         if event.type() == qt.QEvent.Resize:
             self._resize()
+            self._resizeLater()  # Defer resize for the receiver to have handled it
         return super().eventFilter(watched, event)
