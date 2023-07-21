c36bef0e32977ff9e33056665f769d591adcbfed
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-07-21 11:42:21 +0200

    update WaiterOverlay test

--- src/silx/gui/widgets/test/test_waiteroverlay.py
+++ src/silx/gui/widgets/test/test_waiteroverlay.py
@@ -1,24 +1,27 @@
 import pytest
 from silx.gui import qt
-from silx.gui.widgets.waiteroverlay import WaiterOverlay
+from silx.gui.widgets.WaiterOverlay import WaiterOverlay
 from silx.gui.plot import Plot2D
 from silx.gui.plot.PlotWidget import PlotWidget
 
 
 @pytest.mark.parametrize("widget_parent", (Plot2D, qt.QFrame))
-def test_show(widget_parent):
-    # simple test of the WaiterOverlay component
+def test_show(qapp, qapp_utils, widget_parent):
+    """Simple test of the WaiterOverlay component"""
     widget = widget_parent()
-    waitingOverlay = WaiterOverlay(widget)
-    waitingOverlay.setWaiting(True)
-    widget.show()
-    waitingOverlay.setWaiting(False)
+    widget.setAttribute(qt.Qt.WA_DeleteOnClose)
 
-    assert waitingOverlay.parent() is widget
-    if isinstance(widget, PlotWidget):
-        assert waitingOverlay._waitingButton.parent() is widget.getWidgetHandle()
-    else:
-        assert waitingOverlay._waitingButton.parent() is widget
+    waitingOverlay = WaiterOverlay(widget)
+    waitingOverlay.setAttribute(qt.Qt.WA_DeleteOnClose)
+
+    widget.show()
+    qapp_utils.qWaitForWindowExposed(widget)
+    assert waitingOverlay._waitingButton.isWaiting()
+
+    waitingOverlay.hide()
+    qapp.processEvents()
+    assert not waitingOverlay._waitingButton.isWaiting()
 
     widget.close()
     waitingOverlay.close()
+    qapp.processEvents()
