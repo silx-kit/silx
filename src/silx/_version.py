d8a4ebe2ee12ca6075614ac6c9b6076e93907114
Thomas VINCENT <thomas.vincent@esrf.fr>
2024-04-18 13:23:27 +0200

    Bump to v2.1.0

--- src/silx/_version.py
+++ src/silx/_version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # /*##########################################################################
 #
-# Copyright (c) 2015-2022 European Synchrotron Radiation Facility
+# Copyright (c) 2015-2024 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -70,7 +70,7 @@ PRERELEASE_NORMALIZED_NAME = {"dev": "a", "alpha": "a", "beta": "b", "candidate"
 MAJOR = 2
 MINOR = 1
 MICRO = 0
-RELEV = "dev"  # <16
+RELEV = "final"  # <16
 SERIAL = 0  # <16
 
 date = __date__
