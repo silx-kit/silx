efffc2f7192f3fc5aade87fe7482b3122b3b9721
Rebelo Alexis <rebelo.alexis@gmail.com>
2024-05-31 11:10:12 +0200

    First working plot select

--- examples/plotSelection.py
+++ examples/plotSelection.py
@@ -29,10 +29,13 @@ class _HashDropZones(qt.QStyledItemDelegate):
         displayDropZone = False
         if index.isValid():
             model = index.model()
-            rowIndex = model.index(index.row(), 0, index.parent())
+            rowIndex = model.index(index.row(), 1, index.parent())
             rowItem = model.itemFromIndex(rowIndex)
             if not rowItem.data(qt.Qt.DisplayRole):
-                displayDropZone = True
+                parentIndex = model.index(index.parent().row(), 1, index.parent().parent())
+                parentItem = model.itemFromIndex(parentIndex)
+                if parentItem and parentItem.text() not in ["Y"] or index.row() == 0:
+                    displayDropZone = True
         if displayDropZone:
             painter.save()
 
@@ -52,27 +55,9 @@ class _HashDropZones(qt.QStyledItemDelegate):
         else:
             qt.QStyledItemDelegate.paint(self, painter, option, index)
 
-class _IconDropZones(_HashDropZones):
-    """Delegate item displaying a LegendIcon when the item contains one."""
-
-    def paint(self, painter, option, index):
-        """
-        Paint the item
-
-        :param qt.QPainter painter: A painter
-        :param qt.QStyleOptionViewItem option: Options of the item to paint
-        :param qt.QModelIndex index: Index of the item to paint
-        """
-        if index.isValid():
-            legendIcon = index.data(qt.Qt.UserRole)
-            if isinstance(legendIcon, LegendIcon):
-                legendIcon.paint(painter, option.rect, option.palette)
-                return
-        super(_IconDropZones, self).paint(painter, option, index)
-
 
 class _FileListModel(qt.QStandardItemModel):
-    def __init__(self, parent=None):
+    def __init__(self, plot, parent=None):
         """Constructor"""
         super().__init__(parent)
         root = self.invisibleRootItem()
@@ -80,7 +65,26 @@ class _FileListModel(qt.QStandardItemModel):
         root.setDragEnabled(False)
 
         self.setColumnCount(3)
-        self.setHorizontalHeaderLabels(["File Name", "Icon", ""])
+        self.setHorizontalHeaderLabels(["", "File Name", ""])
+
+        self._xParent = qt.QStandardItem("X")
+        root.appendRow(self._xParent)
+
+        self._yParent = qt.QStandardItem("Y")
+        root.appendRow(self._yParent)
+
+        self._plot1D = plot
+        self._index = 0
+
+        self._xDataset = None
+
+        self._addXFile("")
+        self._addYFile("")
+
+        self.addUrl(silx.io.url.DataUrl())
+
+
+        self.rowsAboutToBeRemoved.connect(self._rowAboutToBeRemoved)
 
     def supportedDropActions(self):
         """Inherited method to redefine supported drop actions."""
@@ -90,150 +94,262 @@ class _FileListModel(qt.QStandardItemModel):
         """Inherited method to redefine draggable mime types."""
         return [Hdf5DatasetMimeData.MIME_TYPE]
     
-    def addFile(self, file_name, legendIcon):
-        file_item = qt.QStandardItem(file_name)
-        icon_item = qt.QStandardItem()
-        icon_item.setData(legendIcon, qt.Qt.UserRole)
-        remove_item = qt.QStandardItem()
-        
+    def getXParent(self):
+        return self._xParent
+    
+    def getYParent(self):
+        return self._yParent
 
-        
-        icon_item.setEditable(False)
+    def _addXFile(self, file_name, curve=None):
+        fileItem, iconItem, removeItem = self._createRowItems(file_name, curve)
 
-        if self.rowCount() == 0:
-            self.appendRow([file_item])
+        xIndex = self._findRowContainingText("X")
+        print(f"xIndex : {xIndex}")
+        if xIndex is not None:
+            self.setItem(xIndex, 1, fileItem) 
+            self.setItem(xIndex, 2, removeItem)
+        
+        print(f"Added X file: {fileItem.data(qt.Qt.DisplayRole)}")
+
+    def _addYFile(self, file_name, curve=None, node="X"):
+        fileItem, iconItem, removeItem = self._createRowItems(file_name, curve)
+
+        if self.getYParent().rowCount() == 0:
+            self.getYParent().appendRow([None, fileItem])
             return
-        self.insertRow(self.rowCount()-1, [file_item, icon_item, remove_item])
+    
+        self.getYParent().insertRow(self.getYParent().rowCount()-1, [iconItem, fileItem, removeItem])
+        print(f"Added Y file: {fileItem.data(qt.Qt.DisplayRole)}")
 
-        icon_index = icon_item.index()
-        remove_index = icon_index.siblingAtColumn(2)
-        
-        print(f"Added file: {file_item.data(qt.Qt.DisplayRole)}")
-        return remove_index
+    def _findRowContainingText(self, text):
+        for row in range(self.rowCount()):
+            item = self.item(row, 0)
+            if item and item.text() == text:
+                return row
+        return None
+
+    def _createRowItems(self, file_name, curve):
+        fileItem = qt.QStandardItem(file_name)
+        fileItem.setData(curve, qt.Qt.UserRole)
+
+        iconItem = qt.QStandardItem()
+        removeItem = qt.QStandardItem()
+        iconItem.setEditable(False)
+
+        return fileItem, iconItem, removeItem
 
     def fileExists(self, file_name):
-        for row in range(self.rowCount()):
-            item = self.item(row, 0)  
+        for row in range(self.getXParent().rowCount()):
+            item = self.item(row, 1) 
+            print(f"File exist : item {item}")
             if item and item.text() == file_name:
+                print("File exist : True")
                 return True
+        print("File exist : False")
         return False
     
-class _DropTreeView(qt.QTreeView):
-    """TreeView widget for displaying dropped file names"""
-
-    def __init__(self, plot, parent=None):
-        super().__init__(parent)
-        self.__model = _FileListModel()
-        self.setModel(self.__model)
-
-        self.setItemDelegateForColumn(0, _HashDropZones(self))
-        self.setItemDelegateForColumn(1, _IconDropZones(self))
-        self.__model.addFile("", None)
-
-        header = self.header()
-        header.setSectionResizeMode(0, qt.QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(1, qt.QHeaderView.Stretch)
-
-        self.setUrl(silx.io.url.DataUrl())
-        self.plot = plot
-        self.curves = []
-
-        self.setDropIndicatorShown(True)
-        self.setDragDropOverwriteMode(True)
-        self.setDragEnabled(True)
-        self.viewport().setAcceptDrops(True)
-
-    def setPlot(self, dataset):
-        x_values = numpy.arange(0, len(dataset))
-        legend_name = "Curve {}".format(len(self.curves) + 1)
-        curve = self.plot.addCurve(x=x_values, y=dataset, legend=legend_name)
-        legendIcon = LegendIcon(self, curve)
-        self.curves.append(curve)
-        return legendIcon
-
-    def setUrl(self, url):
+    def addUrl(self, url, node="X"):
         if url.file_path() is not None:
             file = silx.io.open(url.file_path())
             data = file[url.data_path()]
-            if silx.io.is_dataset(data):
-                if data.ndim == 1:
-                    legendIcon = self.setPlot(data)
-                    self.__model.addFile(url.data_path(), legendIcon)
-                    self.createRemoveButton(self.__model.rowCount() - 2)
-                
-    def createRemoveButton(self, row):
-        remove_item = self.__model.item(row, 2)
-        print(f"Remove item : {remove_item}, row : {row}")       
-        if remove_item:
-            print("Tesyt")
-            button = qt.QPushButton(self)
-            button.setIcon(icons.getQIcon("remove"))
-            button.clicked.connect(functools.partial(self.removeFile, remove_item))
-            self.setIndexWidget(remove_item.index(), button)
+            if silx.io.is_dataset(data) and data.ndim == 1:
 
-    def removeFile(self, remove_item):
-        if remove_item:
-                row = remove_item.row()
-                print(f"Row :  {row}")
-                legend = self.curves.pop(row)
-                self.plot.removeCurve(legend)
-                
-                self.model().removeRow(row)
+                    if node == "X":
+                        self._xDataset = data
+
+                        for item in self._plot1D.getItems():
+                            y = item.getData()[1]
+                            item.setData(self._xDataset, y)
+
+                        self._addXFile(url.data_path())
+                    else:
+                        if data is None:
+                            print("No data")
+                            return
+                        curve = self._addPlot(self._xDataset, data)
+                        self._addYFile(url.data_path(), curve)
+            else:
+                print(f"Data is not a valid dataset or not 1D : {data}")
+
+    def _addPlot(self, x, y):
+        if x is None:
+            print("X dataset is not set")
+            x = numpy.arange(len(y))
+        
+        if len(x) != len(y):
+            print("x and y have different length")
+            length = min(len(x), len(y))
+            x = x[:length]
+            y = y[:length]
+
+        legend_name = "Curve {}".format(self._index)
+        self._index += 1
+
+        curve = self._plot1D.addCurve(x=x, y=y, legend=legend_name)
+        return curve 
     
+    def _rowAboutToBeRemoved(self, parentIndex, first, last):
+        print("rowAboutToBeRemoved")
+        parentItem = self.itemFromIndex(parentIndex)
+        for row in range(first, last+1):
+            fileItem = parentItem.child(row, 1)
+            curve = fileItem.data(qt.Qt.UserRole)
+            if curve is None:
+                return
+            self._plot1D.removeItem(curve)
+
+    def _updateYCurvesWithDefaultX(self):
+        for item in self._plot1D.getItems():
+            y = item.getData()[1]
+            x = numpy.arange(len(y))
+            item.setData(x, y)
+            self._plot1D.resetZoom()
+            
+
+class _DropTreeView(qt.QTreeView):
+    """TreeView widget for displaying dropped file names"""
+
+    (
+        _DESCRIPTION_COLUMN,
+        _FILE_COLUMN,
+        _REMOVE_COLUMN
+    ) = range(3)
+
+    def __init__(self, model, parent=None):
+        super().__init__(parent)
+        self._model = model
+        self.setModel(self._model)
+
+        self.setItemDelegateForColumn(1, _HashDropZones(self))
+        
+        header = self.header()
+        header.setStretchLastSection(False)
+        header.setSectionResizeMode(0, qt.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, qt.QHeaderView.Stretch)           
+        header.setSectionResizeMode(2, qt.QHeaderView.ResizeToContents)     
+        self.expandAll()
+        
+        self.setSelectionMode(qt.QAbstractItemView.SingleSelection)
+        self.setDropIndicatorShown(True)
+        self.setDragDropOverwriteMode(True)
+        self.setDragEnabled(False)
+        self.viewport().setAcceptDrops(True)
+
+    def _createIconWidget(self, row, parentItem):
+        fileItem = parentItem.child(row, 1)
+        iconItem = parentItem.child(row, 0)
+        curve = fileItem.data(qt.Qt.UserRole)
+        if curve is None:
+            return
+
+        legendIcon = LegendIcon(None, curve)
+        widget = qt.QWidget()
+        layout = qt.QHBoxLayout(widget)
+        layout.addWidget(legendIcon)
+        layout.setContentsMargins(4, 0, 4, 0)
+        self.setIndexWidget(iconItem.index(), widget)
+        
+    def _createRemoveButton(self, row, parentItem):
+        if parentItem is None:
+            parentItem = self._model.getXParent()
+            index = self.model().index(0, 2)  
+            button = qt.QToolButton(self)
+            button.setIcon(icons.getQIcon("remove"))
+            button.clicked.connect(functools.partial(self._removeFile, None, parentItem))
+            self.setIndexWidget(index, button)
+            return
+
+        removeItem = parentItem.child(row, 2)
+        print(f"Trying to create remove button at row {row}")
+        print(f"Remove item: {removeItem}, row: {row}")
+        if removeItem:
+            button = qt.QToolButton(self)
+            button.setIcon(icons.getQIcon("remove"))
+            button.clicked.connect(functools.partial(self._removeFile, removeItem, parentItem))
+            self.setIndexWidget(removeItem.index(), button)
+        else:
+            print(f"Remove item is None at row {row}")
+
+    def _removeFile(self, removeItem, parentItem):
+        if removeItem is None:
+            row = 0
+            removeItem = self._model.getXParent()
+
+        if removeItem:
+            row = removeItem.row()
+            print(f"removeFile : {row}")
+
+            if parentItem is None:
+                parentItem = self._model.getXParent()
+    
+            parentItem.removeRow(row)
+
+            if parentItem == self._model.getXParent():
+                self._model._xDataset = None
+                self._model._addXFile("")
+                index = self.model().index(0, 2)  
+                self.setIndexWidget(index, None)
+                self._model._updateYCurvesWithDefaultX()
+
     def dragEnterEvent(self, event):
+        print("dragEnterEvent called")
         if event.mimeData().hasFormat("application/x-silx-uri"):
             byteString = event.mimeData().data("application/x-silx-uri")
             url = silx.io.url.DataUrl(byteString.data().decode("utf-8"))
             with silx.io.open(url.file_path()) as file:
                 data = file[url.data_path()]
-                if silx.io.is_dataset(data) and data.ndim == 1 and not self.__model.fileExists(url.data_path()):
+                if silx.io.is_dataset(data) and data.ndim == 1 and not self._model.fileExists(url.data_path()):
                     event.acceptProposedAction()
-        print("dragEnterEvent called")
+        else:
+            event.ignore()
     
     def dropEvent(self, event):
+        print("dropEvent called")
         byteString = event.mimeData().data("application/x-silx-uri")
         url = silx.io.url.DataUrl(byteString.data().decode("utf-8"))
-        self.setUrl(url)
+
+        dropPosition = self.indexAt(event.pos())
+        if not dropPosition.isValid():
+            return
+
+        dropItem = self._model.itemFromIndex(dropPosition)
+        parentItem = dropItem.parent()
+
+        # X condition
+        if parentItem is None:
+            targetNode = "X"
+            node = self._model.getXParent()
+            self._model.addUrl(url, node=targetNode)
+            self._createRemoveButton(node, parentItem)
+            self._model._plot1D.resetZoom()
+            return
+
+        targetNode = "Y"
+        parentNode = self._model.getYParent()   
+        self._model.addUrl(url, node=targetNode)
+        self._createIconWidget(parentNode.rowCount() - 2, parentItem)
+        self._createRemoveButton(parentNode.rowCount() - 2, parentItem)
+        self._model._plot1D.resetZoom()
         event.acceptProposedAction()
-        print("dropEvent called")
-
-class ToolBar(qt.QToolBar):
-    def __init__(self, tree_view, plot, parent=None):
-        super().__init__(parent)
-        self.tree_view = tree_view
-        self.plot = plot
-
-        removeAction = self.addAction(icons.getQIcon("remove"), "Remove File")
-        removeAction.triggered.connect(self.removeFile)
-
-    def removeFile(self):
-        selected_indexes = self.tree_view.selectedIndexes()
-        for index in selected_indexes:
-            if index.column() == 0:
-                row = index.row()
-                self.plot.removeCurve(self.tree_view.curves[row])
-                self.tree_view.model().removeRow(row)
-    
+        
 class MainWindow(qt.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Drag And Drop Project")
 
         self.plot1D = plot.Plot1D()
-        self.treeView = _DropTreeView(self.plot1D, self)
-        self.toolBar = ToolBar(self.treeView, self.plot1D)
-
-        centralWidget = qt.QWidget()
-        mainLayout = qt.QHBoxLayout(centralWidget)
-
-        verticalLayout = qt.QVBoxLayout()
-        verticalLayout.addWidget(self.toolBar)
-        verticalLayout.addWidget(self.treeView)
+        model = _FileListModel(self.plot1D)
+        self.treeView = _DropTreeView(model, self)
         
+        centralWidget = qt.QSplitter()
 
-        mainLayout.addLayout(verticalLayout)
-        mainLayout.addWidget(self.plot1D)
+        centralWidget.addWidget(self.treeView)
+        centralWidget.addWidget(self.plot1D)
 
+        centralWidget.setCollapsible(0, False)
+        centralWidget.setCollapsible(1, False)
+        centralWidget.setStretchFactor(1, 1)
+        
         self.setCentralWidget(centralWidget)
 
 if __name__ == "__main__":
