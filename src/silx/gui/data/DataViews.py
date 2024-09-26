73e566f2f64c228fa95ef4db4a5890ecbfbe8405
Edgar Gutierrez Fernandez <edgar.gutierrez-fernandez@esrf.fr>
2024-09-26 14:02:58 +0200

    set aggregation mode in set data

--- src/silx/gui/data/DataViews.py
+++ src/silx/gui/data/DataViews.py
@@ -1115,6 +1115,7 @@ class _Plot2dView(DataView):
     
         if imageItem is None:
             imageItem = ImageDataAggregated()
+            imageItem.setAggregationMode(self.getAggregationModeAction().getAggregationMode())            
             imageItem.setName("data")
             imageItem.setColormap(plot.getDefaultColormap())
             plot.addItem(imageItem)
@@ -1827,7 +1828,30 @@ class _NXdataImageView(_NXdataBaseDataView):
         widget.getPlot().getColormapAction().setColormapDialog(
             self.defaultColorDialog()
         )
+
+        self.__aggregationModeAction = AggregationModeAction(parent=widget.getPlot())
+        widget.getPlot().toolBar().addAction(self.__aggregationModeAction)
+        self.__aggregationModeAction.setVisible(True)
+        #self.__aggregationModeAction.sigAggregationModeChanged.connect(self._aggregationModeChanged)
+
         return widget
+
+    def getAggregationModeAction(self):
+        """Action toggling the aggregation mode action
+
+        :rtype: actions.PlotAction
+        """
+        return self.__aggregationModeAction
+
+    def _aggregationModeChanged(self):
+        plot = self.getWidget().getPlot()
+        item = plot._getItem("image")
+
+        if item is None:
+            return
+
+        aggregationMode = self.getAggregationModeAction().getAggregationMode()
+        item.setAggregationMode(aggregationMode)
 
     def axesNames(self, data, info):
         # disabled (used by default axis selector widget in Hdf5Viewer)
