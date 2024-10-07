390b5d6952a3d5e734a745d90639cce6d2861337
Thomas VINCENT <thomas.vincent@esrf.fr>
2024-10-07 14:13:02 +0200

    Apply suggestions from code review

--- src/silx/io/fioh5.py
+++ src/silx/io/fioh5.py
@@ -167,10 +167,9 @@ dtypeConverter = {
 }
 
 def _bytestobool (val):
-    """Convert a byte of truth to true (1) or false (0).
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
+    """Convert bytes of a truth value to bool.
+
+    Raises ValueError if 'val' is not supported.
     """
     val = val.decode().lower()
     if val in ('y', 'yes', 't', 'true', 'on', '1'):
@@ -178,7 +177,7 @@ def _bytestobool (val):
     elif val in ('n', 'no', 'f', 'false', 'off', '0'):
         return False
     else:
-        raise ValueError("invalid truth value %r" % (val,))
+        raise ValueError("Invalid truth value %r" % val)
 
 
 def is_fiofile(filename):
