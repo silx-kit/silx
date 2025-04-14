29b2ad5227995d0d0b912cb6e840286b700cb281
Jerome Kieffer <kieffer@mac13.esrf.fr>
2025-04-14 10:39:00 +0200

    mesonify most of the silx-project
    Those `meson.build` files are generated by the mesonify tool

---
+++ tools/mesonify.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import os
+
+def mesonify(where, top=None):    
+    if top==None:
+        top = os.path.join(where,"src")
+    # print(where, "top: ", top)
+    txt = []
+    pyfiles = []
+    for smth in os.listdir(where):
+        path = os.path.join(where,smth)
+        if os.path.isdir(path):
+            txt.append(f"subdir('{smth}')")
+            mesonify(path, top)
+        elif smth.endswith(".py"):
+            pyfiles.append(smth)
+
+    if pyfiles:
+        pyfiles.sort()
+        txt.append("")
+        txt.append("py.install_sources([")        
+        for f in pyfiles:
+            txt.append(f"    '{f}',")
+        txt.append("]")
+        txt.append("pure: false,    # Will be installed next to binaries")
+        if len(path)>len(top):
+            txt.append(f"subdir: '{where[len(top)+1:]}',  # Folder relative to site-packages to install to")
+        txt.append(")")
+        txt.append("")
+
+    dst = os.path.join(where,"meson.build")
+    print(dst)
+    with open(dst,"w") as w:
+        w.write("\n".join(txt))
+
+if __name__ == "__main__":
+    base = os.path.abspath(os.path.join(__file__, "..", "..", "src"))
+    print(base)
+    mesonify(base, base)
