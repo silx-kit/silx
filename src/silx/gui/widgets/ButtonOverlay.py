83584cf06a523d72b4727e48395a22d412c88f6f
payno <henri.payno@gmail.com>
2025-04-15 11:46:20 +0200

    Add ButtonOverlay which is the implementation of '_OverlayMixIn' for a button

---
+++ src/silx/gui/widgets/ButtonOverlay.py
@@ -0,0 +1,19 @@
+from silx.gui import qt
+
+from .OverlayMixIn import _OverlayMixIn
+
+
+class ButtonOverlay(_OverlayMixIn, qt.QPushButton):
+    """Display a Label on top of a PlotWidget"""
+
+    def __init__(self, parent: qt.QWidget, *args, **kwargs) -> None:
+        qt.QPushButton.__init__(self, parent, *args, **kwargs)
+        _OverlayMixIn.__init__(self, parent)
+
+    def showEvent(self, event: qt.QShowEvent):
+        super().showEvent(event)
+        self.setVisible(True)
+
+    def hideEvent(self, event: qt.QHideEvent):
+        super().hideEvent(event)
+        self.setVisible(False)
