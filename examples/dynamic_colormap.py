2846eea41650bd100c7b03b63e39597b9df95ff3
Jerome Lesaint <jerome.lesaint@esrf.fr>
2025-04-16 16:43:51 +0200

    Add doc to the dynamic_colormap example

--- examples/dynamic_colormap.py
+++ examples/dynamic_colormap.py
@@ -1,3 +1,38 @@
+# /*##########################################################################
+#
+# Copyright (c) 2018-2021 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+"""This script shows how to dyamically adjust the colormap to a small region 
+around the cursor position. The DynamicColormapMode can be activated either by
+the icon in the widget toolbar or by simply pressing the w-key.
+The image has 4 regions of different gaussian distribution. When activated, the 
+DynamicColormap mode will adjust the colormap to enhance the contrast in the 
+region close to the cursor.
+
+The pan and zoom modes (the two other interaction modes) can be activated either 
+by their respective icon or by pressing the P- and Z-key respectively.
+"""
+
+
 import numpy
 from silx.gui import qt
 from silx.gui.plot import Plot2D
