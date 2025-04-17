77a0a9088d7bdcecb79b962e6aab7ae06806f0b6
Marius Retegan <marius.s.retegan@gmail.com>
2025-04-17 21:17:23 +0200

    Fix shell scripts

--- package/windows/create-dmg.sh
+++ package/windows/create-dmg.sh
@@ -7,44 +7,44 @@ set -e
 echo "Setting environment variables."
 APP_NAME="silx-view"
 ROOT="${PWD}"
-APP="${ROOT}"/dist/${APP_NAME}.app
+APP="${ROOT}/dist/${APP_NAME}.app"
 RESOURCES="${ROOT}" # The path to resources (volume icon, background, ...)."
 
 echo "Setting derived environment variables."
-ARTIFACTS="${ROOT}"/artifacts
-TEMPLATE="${ARTIFACTS}"/template
-TEMPLATE_DMG="${ARTIFACTS}"/template.dmg
-APP_DMG="${ARTIFACTS}"/${APP_NAME}.dmg
+ARTIFACTS="${ROOT}/artifacts"
+TEMPLATE="${ARTIFACTS}/template"
+TEMPLATE_DMG="${ARTIFACTS}/template.dmg"
+APP_DMG="${ARTIFACTS}/${APP_NAME}.dmg"
 
 SLEEP_INTERVAL=5
 
 echo "Checking if artifacts folder exists."
-[ -d "$ARTIFACTS" ] && rm -rf "$ARTIFACTS"
+[ -d "${ARTIFACTS}" ] && rm -rf "${ARTIFACTS}"
 
 echo "Creating the artifacts folder."
-mkdir -p "$ARTIFACTS"
+mkdir -p "${ARTIFACTS}"
 
 echo "Removing any previous images."
 if [[ -e "${TEMPLATE}" ]]; then rm -rf "${TEMPLATE}"; fi
 if [[ -e "${APP_DMG}" ]]; then rm -rf "${APP_DMG}"; fi
 
 echo "Copying required files."
-mkdir -p "${TEMPLATE}"/.background
-cp -a "${RESOURCES}"/background.pdf "${TEMPLATE}"/.background/background.pdf
-cp -a "${RESOURCES}"/silx.icns "${TEMPLATE}"/.VolumeIcon.icns
-cp -a "${RESOURCES}"/DS_Store "${TEMPLATE}"/.DS_Store
-cp -a "${APP}" "${TEMPLATE}"/${APP_NAME}.app
-ln -s /Applications/ "${TEMPLATE}"/Applications
+mkdir -p "${TEMPLATE}/.background"
+cp -a "${RESOURCES}/background.pdf" "${TEMPLATE}/.background/background.pdf"
+cp -a "${RESOURCES}/silx.icns" "${TEMPLATE}/.VolumeIcon.icns"
+cp -a "${RESOURCES}/DS_Store" "${TEMPLATE}/.DS_Store"
+cp -a "${APP}" "${TEMPLATE}/${APP_NAME}.app"
+ln -s "/Applications/" "${TEMPLATE}/Applications"
 
 echo "Creating a regular .fseventsd/no_log file."
-mkdir "${TEMPLATE}"/.fseventsd
-touch "${TEMPLATE}"/.fseventsd/no_log
+mkdir "${TEMPLATE}/.fseventsd"
+touch "${TEMPLATE}/.fseventsd/no_log"
 
 echo "Sleeping for a few seconds."
 sleep "${SLEEP_INTERVAL}"
 
 echo "Creating the temporary disk image."
-hdiutil create -verbose -format UDRW -volname ${APP_NAME} -fs APFS \
+hdiutil create -verbose -format UDRW -volname "${APP_NAME}" -fs APFS \
        -srcfolder "${TEMPLATE}" \
        "${TEMPLATE_DMG}"
 
@@ -52,7 +52,7 @@ echo "Sleeping for a few seconds."
 sleep "${SLEEP_INTERVAL}"
 
 echo "Detaching the temporary disk image if still attached."
-hdiutil detach -verbose /Volumes/${APP_NAME} -force || true
+hdiutil detach -verbose "/Volumes/${APP_NAME}" -force || true
 
 echo "Attaching the temporary disk image in read/write mode."
 MOUNT_OUTPUT=$(hdiutil attach -readwrite -noverify -noautoopen "${TEMPLATE_DMG}" | grep '^/dev/')
@@ -63,7 +63,7 @@ echo "Fixing permissions."
 chmod -Rf go-w "${TEMPLATE}" || true
 
 echo "Hiding the background directory even more."
-SetFile -a V "${MOUNT_POINT}"/.background
+SetFile -a V "${MOUNT_POINT}/.background"
 
 echo "Seting the custom icon volume flag so that volume has nice icon."
 SetFile -a C "${MOUNT_POINT}"
@@ -75,20 +75,20 @@ APPLICATIONS_ICON_POSITION="{386, 124}"
 
 osascript <<EOF
 tell application "Finder"
-    tell disk $APP_NAME
+    tell disk "${APP_NAME}"
         open
         set current view of container window to icon view
         set toolbar visible of container window to false
         set statusbar visible of container window to false
-        set bounds of container window to $WINDOW_SIZE
+        set bounds of container window to ${WINDOW_SIZE}
 
         set viewOptions to the icon view options of container window
         set arrangement of viewOptions to not arranged
         set icon size of viewOptions to 96
         set background picture of viewOptions to file ".background:background.pdf"
 
-        set position of item $APP_NAME to $APP_ICON_POSITION
-        set position of item "Applications" to $APPLICATIONS_ICON_POSITION
+        set position of item "${APP_NAME}" to ${APP_ICON_POSITION}
+        set position of item "Applications" to ${APPLICATIONS_ICON_POSITION}
 
         close
         open
--- package/windows/notarize.sh
+++ package/windows/notarize.sh
@@ -7,12 +7,12 @@ set -e
 echo "Setting the required environment variables."
 APP_NAME="silx-view"
 ROOT="${PWD}"
-APP_DMG="${ROOT}"/artifacts/${APP_NAME}.dmg
+APP_DMG="${ROOT}/artifacts/${APP_NAME}.dmg"
 
 echo "Submiting the application for notarization."
-xcrun notarytool submit --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APPLICATION_SPECIFIC_PASSWORD  --wait "$APP_DMG"
+xcrun notarytool submit --apple-id "${APPLE_ID}" --team-id "${APPLE_TEAM_ID}" --password "${APPLICATION_SPECIFIC_PASSWORD}" --wait "${APP_DMG}"
 
 echo "Stapling the notarization ticket to the application bundle."
-xcrun stapler staple "$APP_DMG"
+xcrun stapler staple "${APP_DMG}"
 
 echo "Notarization completed successfully."
