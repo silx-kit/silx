b2c47920ad3b130155b252e7773faaa9fb500b98
Marius Retegan <marius.s.retegan@gmail.com>
2025-04-17 13:30:26 +0200

    Add scripts for macOS

---
+++ package/windows/notarize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# The script submits the application for notarization to Apple.
+
+set -e
+
+echo "Setting the required environment variables."
+APP_NAME="silx-view"
+ROOT="${PWD}"
+APP_DMG="${ROOT}"/artifacts/${APP_NAME}.dmg
+APPLE_ID="marius.retegan@esrf.fr"
+
+echo "Submiting the application for notarization."
+xcrun notarytool submit --apple-id $APPLE_ID --team-id $APPLE_TEAM_ID --password $APP_SPECIFIC_PASSWORD  --wait "$APP_DMG"
+
+echo "Stapling the notarization ticket to the application bundle."
+xcrun stapler staple "$APP_DMG"
+
+echo "Notarization completed successfully."
