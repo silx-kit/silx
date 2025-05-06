736a1a2b75e930af0e780173987d2858a899c8d4
Thomas VINCENT <thomas.vincent@esrf.fr>
2025-05-06 13:41:58 +0200

    Check for all files rather than only .py and look for meson.build in sub-folders

--- tools/check_meson.py
+++ tools/check_meson.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """
-Script to check if all python source files are actually registered in meson
+Script to check if all source files are actually registered in meson
 """
+from functools import cache
 import os
 import subprocess
 import sys
 
 
+EXCLUDED_FILES = [
+    "src/silx/io/specfile/src/sfwrite.c",
+    "src/silx/math/histogramnd/README",
+    "src/silx/math/histogramnd/src/histogramnd_template.c",
+]
+"""Files to ignore during the check"""
 
 
 def get_all_files(root):
@@ -21,6 +28,13 @@ def get_all_files(root):
     res = process.stdout.decode()
     return  [i.strip() for i in res.split(os.linesep)]
 
+
+@cache
+def read_file(filename: str):
+    with open(filename) as fd:
+        return fd.read()
+
+
 def main(root):
     """print out all python filenames not properly registered in meson.build
 
@@ -32,27 +46,36 @@ def main(root):
     meson = []
 
     for line in get_all_files(root):
-        if line.endswith(".py") and line.startswith("src"):
-            src.append(line)
-        elif line.endswith("meson.build"):
+        if line.endswith("meson.build"):
             meson.append(line)
-    mesoncache = {}
-    for f in src:
-        base, fn = os.path.split(f)
-        mesonfile = os.path.join(base,"meson.build")
-        if mesonfile in meson:
-            if mesonfile not in mesoncache:
-                with open(mesonfile) as fd:
-                    mesoncache[mesonfile] = fd.read()
         else:
-            print(f"File `{f}` is in a directory without `meson.build` file")
+            extension = os.path.splitext(line)[1]
+            if line.startswith("src") and extension not in (".h", ".hpp", ".pxd"):
+                src.append(line)
+
+    for filename in src:
+        if filename in EXCLUDED_FILES:
+            print(f"File excluded from the check: {filename}")
+            continue
+
+        filename_parts = filename.split(os.sep)
+        for index in range(len(filename_parts) - 1, 0, -1):
+            base_folder = os.path.join(*filename_parts[:index])
+            unix_file_path = "/".join(filename_parts[index:])
+
+            mesonfile = os.path.join(base_folder, "meson.build")
+            if mesonfile in meson:
+                break
+        else:
+            print(f"ERROR: File `{filename}` is in a directory without `meson.build` file")
             cnt += 1
             continue
-        mesondata = mesoncache[mesonfile]
-        if fn not in mesondata:
-            print(f"File `{f}` is not listed in `{mesonfile}` file")
+
+        if f"'{unix_file_path}'" not in read_file(mesonfile):
+            print(f"ERROR: File `{unix_file_path}` is not listed in `{mesonfile}` file")
             cnt += 1
-    print(f"Analyzed {len(src)} python source files in {len(meson)} submodules and found {cnt} issues to investigate")
+
+    print(f"Analyzed {len(src)} source files in {len(meson)} submodules and found {cnt} issues to investigate")
     return cnt
 
 
