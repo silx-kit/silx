07676a3a747c8cceaa2ecba0472adb1f0b847087
payno <henri.payno@esrf.fr>
2025-07-17 15:07:37 +0200

    OverlayMixIn: allow users to provide an alignment and an offset

--- src/silx/gui/widgets/LabelOverlay.py
+++ src/silx/gui/widgets/LabelOverlay.py
@@ -6,6 +6,12 @@ from .OverlayMixIn import OverlayMixIn as _OverlayMixIn
 class LabelOverlay(_OverlayMixIn, qt.QLabel):
     """Display a Label on top of a widget"""
 
-    def __init__(self, parent: qt.QWidget | None = None) -> None:
+    def __init__(
+        self,
+        parent: qt.QWidget | None = None,
+    ) -> None:
         qt.QLabel.__init__(self, parent)
-        _OverlayMixIn.__init__(self, parent)
+        _OverlayMixIn.__init__(
+            self,
+            parent,
+        )
