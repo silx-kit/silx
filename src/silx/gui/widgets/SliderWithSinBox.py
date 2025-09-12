8aa2c868249f9ec7c0cb1499b431fa2eafb4b31b
payno <henri.payno@gmail.com>
2025-09-12 11:36:43 +0200

    silx.gui.plot: Generalize code to use any percentile value and not only 1-99
    # Conflicts:
    #	src/silx/utils/cache.py

---
+++ src/silx/gui/widgets/SliderWithSinBox.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from silx.gui import qt
+from silx.gui.utils import blockSignals
+
+
+class SliderWithSpinBox(qt.QWidget):
+    """
+    Widget with a slider and a spin box for a float
+    """
+
+    valueChanged = qt.Signal(int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setLayout(qt.QHBoxLayout())
+
+        self._slider = qt.QSlider(qt.Qt.Horizontal, self)
+        self.layout().addWidget(self._slider)
+
+        self._spinBox = qt.QDoubleSpinBox(self)
+        self.layout().addWidget(self._spinBox)
+
+        self.setRange(0, 100)
+
+        # connect signal / slot
+        self._slider.valueChanged.connect(self.setValue)
+        self._spinBox.valueChanged.connect(self.setValue)
+
+    def setValue(self, value: float):
+        with blockSignals(self._slider, self._spinBox):
+            self._slider.setValue(value)
+            self._spinBox.setValue(value)
+        self.valueChanged.emit(self.value())
+
+    def value(self) -> float:
+        return self._slider.value()
+
+    def setRange(self, min: float, max: float):
+        self._slider.setRange(min, max)
+        self._spinBox.setRange(min, max)
+
+    # expose API
+    def setTickPosition(self, position):
+        self._slider.setTickPosition(position)
+
+    def setTracking(self, enable: bool):
+        self._slider.setTracking(enable)
