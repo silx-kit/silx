935ff0b289866fa6e9857fd9e7413d52ff66b16f
Loic Huder <loic.huder@esrf.fr>
2026-01-07 11:54:15 +0100

    silx.gui.data.test.test_dataviewer: migrate to pytest

--- src/silx/gui/data/test/test_dataviewer.py
+++ src/silx/gui/data/test/test_dataviewer.py
@@ -25,10 +25,7 @@ __authors__ = ["V. Valls"]
 __license__ = "MIT"
 __date__ = "19/02/2019"
 
-import os
-import tempfile
 import pytest
-from contextlib import contextmanager
 
 import numpy
 from ..DataViewer import DataViewer
@@ -60,136 +57,173 @@ class _DataViewMock(DataView):
         return 0
 
 
+# https://docs.pytest.org/en/stable/how-to/fixtures.html#parametrizing-fixtures
+@pytest.fixture(params=[DataViewer, DataViewerFrame])
+def data_viewer_widget(request):
+    widget = request.param(parent=None)
+    yield widget
+
+
+def test_text_data(data_viewer_widget):
+    data_list = ["aaa", int, 8, object]
+    for data in data_list:
+        data_viewer_widget.setData(data)
+        assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+
+
+def test_plot_1d_data(data_viewer_widget):
+    data = numpy.arange(3**1)
+    data.shape = [3] * 1
+    data_viewer_widget.setData(data)
+    availableModes = {v.modeId() for v in data_viewer_widget.currentAvailableViews()}
+    assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+    assert DataViews.PLOT1D_MODE in availableModes
+
+
+def test_image_data(data_viewer_widget):
+    data = numpy.arange(3**2)
+    data.shape = [3] * 2
+    data_viewer_widget.setData(data)
+    availableModes = {v.modeId() for v in data_viewer_widget.currentAvailableViews()}
+    assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+    assert DataViews.PLOT2D_MODE in availableModes
+
+
+def test_image_bool(data_viewer_widget):
+    data = numpy.zeros((10, 10), dtype=bool)
+    data[::2, ::2] = True
+    data_viewer_widget.setData(data)
+    availableModes = {v.modeId() for v in data_viewer_widget.currentAvailableViews()}
+    assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+    assert DataViews.PLOT2D_MODE in availableModes
+
+
+def test_image_complex_data(data_viewer_widget):
+    data = numpy.arange(3**2, dtype=numpy.complex64)
+    data.shape = [3] * 2
+    data_viewer_widget.setData(data)
+    availableModes = {v.modeId() for v in data_viewer_widget.currentAvailableViews()}
+    assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+    assert DataViews.COMPLEX_PLOT2D_MODE in availableModes
+
+
+def test_plot_3d_data(data_viewer_widget):
+    data = numpy.arange(3**3)
+    data.shape = [3] * 3
+    data_viewer_widget.setData(data)
+    availableModes = {v.modeId() for v in data_viewer_widget.currentAvailableViews()}
+    try:
+        import silx.gui.plot3d  # noqa
+
+        assert DataViews.PLOT3D_MODE in availableModes
+    except ImportError:
+        assert DataViews.PLOT2D_MODE in availableModes
+    assert DataViews.RAW_MODE == data_viewer_widget.displayMode()
+
+
+def test_array_1d_data(data_viewer_widget):
+    data = numpy.array(["aaa"] * (3**1))
+    data.shape = [3] * 1
+    data_viewer_widget.setData(data)
+    assert DataViews.RAW_MODE == data_viewer_widget.displayedView().modeId()
+
+
+def test_array_2d_data(data_viewer_widget):
+    data = numpy.array(["aaa"] * (3**2))
+    data.shape = [3] * 2
+    data_viewer_widget.setData(data)
+    assert DataViews.RAW_MODE == data_viewer_widget.displayedView().modeId()
+
+
+def test_array_4d_data(data_viewer_widget):
+    data = numpy.array(["aaa"] * (3**4))
+    data.shape = [3] * 4
+    data_viewer_widget.setData(data)
+    assert DataViews.RAW_MODE == data_viewer_widget.displayedView().modeId()
+
+
+def test_record_4d_data(data_viewer_widget):
+    data = numpy.zeros(3**4, dtype="3int8, float32, (2,3)float64")
+    data.shape = [3] * 4
+    data_viewer_widget.setData(data)
+    assert DataViews.RAW_MODE == data_viewer_widget.displayedView().modeId()
+
+
+def test_3d_h5_dataset(data_viewer_widget, tmp_path):
+    data = numpy.arange(3 * 3 * 3)
+    data.shape = 3, 3, 3
+    with h5py.File(tmp_path / "tmp.h5", "w") as h5file:
+        h5file["data"] = data
+
+    with h5py.File(tmp_path / "tmp.h5", "r") as h5file:
+        dataset = h5file["data"]
+        data_viewer_widget.setData(dataset)
+
+
+def test_data_event(data_viewer_widget):
+    listener = SignalListener()
+    data_viewer_widget.dataChanged.connect(listener)
+    data_viewer_widget.setData(10)
+    data_viewer_widget.setData(None)
+    assert listener.callCount() == 2
+
+
+def test_display_mode_event(data_viewer_widget):
+    listener = SignalListener()
+    data_viewer_widget.displayedViewChanged.connect(listener)
+    data_viewer_widget.setData(10)
+    data_viewer_widget.setData(None)
+    modes = [v.modeId() for v in listener.arguments(argumentIndex=0)]
+    assert modes == [DataViews.RAW_MODE, DataViews.EMPTY_MODE]
+    listener.clear()
+
+
+def test_create_default_views(data_viewer_widget):
+    views = data_viewer_widget.createDefaultViews()
+    assert len(views) > 0
+
+
+def test_add_view(data_viewer_widget):
+    view = _DataViewMock(data_viewer_widget)
+    data_viewer_widget.addView(view)
+    assert view in data_viewer_widget.availableViews()
+    assert view in data_viewer_widget.currentAvailableViews()
+
+
+def test_remove_view(data_viewer_widget):
+    data_viewer_widget.setData("foobar")
+    view = data_viewer_widget.currentAvailableViews()[0]
+    data_viewer_widget.removeView(view)
+    assert view not in data_viewer_widget.availableViews()
+    assert view not in data_viewer_widget.currentAvailableViews()
+
+
+def test_replace_view(data_viewer_widget):
+    view = _DataViewMock(data_viewer_widget)
+    data_viewer_widget.replaceView(DataViews.RAW_MODE, view)
+    assert data_viewer_widget.getViewFromModeId(DataViews.RAW_MODE) is None
+    assert view in data_viewer_widget.availableViews()
+    assert view in data_viewer_widget.currentAvailableViews()
+
+
+def test_replace_view_in_composite(data_viewer_widget):
+    # replace a view that is a child of a composite view
+    view = _DataViewMock(data_viewer_widget)
+    replaced = data_viewer_widget.replaceView(DataViews.NXDATA_INVALID_MODE, view)
+    assert replaced
+    nxdata_view = data_viewer_widget.getViewFromModeId(DataViews.NXDATA_MODE)
+    assert DataViews.NXDATA_INVALID_MODE not in [
+        v.modeId() for v in nxdata_view.getViews()
+    ]
+    assert view in nxdata_view.getViews()
+
+
 class _TestAbstractDataViewer(TestCaseQt):
     __test__ = False  # ignore abstract class
 
     def create_widget(self):
         # Avoid to raise an error when testing the full module
         self.skipTest("Not implemented")
-
-    @contextmanager
-    def h5_temporary_file(self):
-        # create tmp file
-        fd, tmp_name = tempfile.mkstemp(suffix=".h5")
-        os.close(fd)
-        data = numpy.arange(3 * 3 * 3)
-        data.shape = 3, 3, 3
-        # create h5 data
-        h5file = h5py.File(tmp_name, "w")
-        h5file["data"] = data
-        yield h5file
-        # clean up
-        h5file.close()
-        os.unlink(tmp_name)
-
-    def test_text_data(self):
-        data_list = ["aaa", int, 8, self]
-        widget = self.create_widget()
-        for data in data_list:
-            widget.setData(data)
-            self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-
-    def test_plot_1d_data(self):
-        data = numpy.arange(3**1)
-        data.shape = [3] * 1
-        widget = self.create_widget()
-        widget.setData(data)
-        availableModes = {v.modeId() for v in widget.currentAvailableViews()}
-        self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-        self.assertIn(DataViews.PLOT1D_MODE, availableModes)
-
-    def test_image_data(self):
-        data = numpy.arange(3**2)
-        data.shape = [3] * 2
-        widget = self.create_widget()
-        widget.setData(data)
-        availableModes = {v.modeId() for v in widget.currentAvailableViews()}
-        self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-        self.assertIn(DataViews.PLOT2D_MODE, availableModes)
-
-    def test_image_bool(self):
-        data = numpy.zeros((10, 10), dtype=bool)
-        data[::2, ::2] = True
-        widget = self.create_widget()
-        widget.setData(data)
-        availableModes = {v.modeId() for v in widget.currentAvailableViews()}
-        self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-        self.assertIn(DataViews.PLOT2D_MODE, availableModes)
-
-    def test_image_complex_data(self):
-        data = numpy.arange(3**2, dtype=numpy.complex64)
-        data.shape = [3] * 2
-        widget = self.create_widget()
-        widget.setData(data)
-        availableModes = {v.modeId() for v in widget.currentAvailableViews()}
-        self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-        self.assertIn(DataViews.COMPLEX_PLOT2D_MODE, availableModes)
-
-    def test_plot_3d_data(self):
-        data = numpy.arange(3**3)
-        data.shape = [3] * 3
-        widget = self.create_widget()
-        widget.setData(data)
-        availableModes = {v.modeId() for v in widget.currentAvailableViews()}
-        try:
-            import silx.gui.plot3d  # noqa
-
-            self.assertIn(DataViews.PLOT3D_MODE, availableModes)
-        except ImportError:
-            self.assertIn(DataViews.PLOT2D_MODE, availableModes)
-        self.assertEqual(DataViews.RAW_MODE, widget.displayMode())
-
-    def test_array_1d_data(self):
-        data = numpy.array(["aaa"] * (3**1))
-        data.shape = [3] * 1
-        widget = self.create_widget()
-        widget.setData(data)
-        self.assertEqual(DataViews.RAW_MODE, widget.displayedView().modeId())
-
-    def test_array_2d_data(self):
-        data = numpy.array(["aaa"] * (3**2))
-        data.shape = [3] * 2
-        widget = self.create_widget()
-        widget.setData(data)
-        self.assertEqual(DataViews.RAW_MODE, widget.displayedView().modeId())
-
-    def test_array_4d_data(self):
-        data = numpy.array(["aaa"] * (3**4))
-        data.shape = [3] * 4
-        widget = self.create_widget()
-        widget.setData(data)
-        self.assertEqual(DataViews.RAW_MODE, widget.displayedView().modeId())
-
-    def test_record_4d_data(self):
-        data = numpy.zeros(3**4, dtype="3int8, float32, (2,3)float64")
-        data.shape = [3] * 4
-        widget = self.create_widget()
-        widget.setData(data)
-        self.assertEqual(DataViews.RAW_MODE, widget.displayedView().modeId())
-
-    def test_3d_h5_dataset(self):
-        with self.h5_temporary_file() as h5file:
-            dataset = h5file["data"]
-            widget = self.create_widget()
-            widget.setData(dataset)
-
-    def test_data_event(self):
-        listener = SignalListener()
-        widget = self.create_widget()
-        widget.dataChanged.connect(listener)
-        widget.setData(10)
-        widget.setData(None)
-        self.assertEqual(listener.callCount(), 2)
-
-    def test_display_mode_event(self):
-        listener = SignalListener()
-        widget = self.create_widget()
-        widget.displayedViewChanged.connect(listener)
-        widget.setData(10)
-        widget.setData(None)
-        modes = [v.modeId() for v in listener.arguments(argumentIndex=0)]
-        self.assertEqual(modes, [DataViews.RAW_MODE, DataViews.EMPTY_MODE])
-        listener.clear()
 
     def test_change_display_mode(self):
         listener = SignalListener()
@@ -200,7 +234,7 @@ class _TestAbstractDataViewer(TestCaseQt):
         widget.setData(data)
 
         widget.setDisplayMode(DataViews.PLOT1D_MODE)
-        self.assertEqual(widget.displayedView().modeId(), DataViews.PLOT1D_MODE)
+        assert widget.displayedView().modeId() == DataViews.PLOT1D_MODE
         self.qWait(200)
         assert listener.arguments() == [((0, 0, 0, slice(None)), None)]
         listener.clear()
@@ -222,46 +256,6 @@ class _TestAbstractDataViewer(TestCaseQt):
         self.qWait(200)
         assert listener.arguments() == [(None, None)]
         listener.clear()
-
-    def test_create_default_views(self):
-        widget = self.create_widget()
-        views = widget.createDefaultViews()
-        self.assertTrue(len(views) > 0)
-
-    def test_add_view(self):
-        widget = self.create_widget()
-        view = _DataViewMock(widget)
-        widget.addView(view)
-        self.assertTrue(view in widget.availableViews())
-        self.assertTrue(view in widget.currentAvailableViews())
-
-    def test_remove_view(self):
-        widget = self.create_widget()
-        widget.setData("foobar")
-        view = widget.currentAvailableViews()[0]
-        widget.removeView(view)
-        self.assertTrue(view not in widget.availableViews())
-        self.assertTrue(view not in widget.currentAvailableViews())
-
-    def test_replace_view(self):
-        widget = self.create_widget()
-        view = _DataViewMock(widget)
-        widget.replaceView(DataViews.RAW_MODE, view)
-        self.assertIsNone(widget.getViewFromModeId(DataViews.RAW_MODE))
-        self.assertTrue(view in widget.availableViews())
-        self.assertTrue(view in widget.currentAvailableViews())
-
-    def test_replace_view_in_composite(self):
-        # replace a view that is a child of a composite view
-        widget = self.create_widget()
-        view = _DataViewMock(widget)
-        replaced = widget.replaceView(DataViews.NXDATA_INVALID_MODE, view)
-        self.assertTrue(replaced)
-        nxdata_view = widget.getViewFromModeId(DataViews.NXDATA_MODE)
-        self.assertNotIn(
-            DataViews.NXDATA_INVALID_MODE, [v.modeId() for v in nxdata_view.getViews()]
-        )
-        self.assertTrue(view in nxdata_view.getViews())
 
 
 class TestDataViewer(_TestAbstractDataViewer):
