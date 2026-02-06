32cea5ed2cecbe5ed5b04c74ac481253e5cddbe6
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-02-06 17:28:40 +0100

    convert tests to pytest

--- src/silx/math/test/test_calibration.py
+++ src/silx/math/test/test_calibration.py
@@ -1,5 +1,5 @@
 # /*##########################################################################
-# Copyright (C) 2018-2023 European Synchrotron Radiation Facility
+# Copyright (C) 2018-2026 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,12 +24,11 @@
 
 __authors__ = ["P. Knobel"]
 __license__ = "MIT"
-__date__ = "14/05/2018"
+__date__ = "06/02/2026"
 
-
-import unittest
 
 import numpy
+import pytest
 
 from silx.math.calibration import (
     NoCalibration,
@@ -38,106 +37,87 @@ from silx.math.calibration import (
     FunctionCalibration,
 )
 
-X = numpy.array([3.14, 2.73, 1337])
+
+def testNoCalibration():
+    calib = NoCalibration()
+    assert calib.is_affine()
+    assert calib.get_slope() == 1.0
+    assert calib(0.0) == 0.0
+
+    values = numpy.array([3.14, 2.73, 1337])
+    assert numpy.array_equal(calib(values), values)
 
 
-class TestNoCalibration(unittest.TestCase):
-    def setUp(self):
-        self.calib = NoCalibration()
+def testLinearCalibration():
+    y_intercept = 1.5
+    slope = 2.5
+    calib = LinearCalibration(y_intercept=y_intercept, slope=slope)
 
-    def testIsAffine(self):
-        self.assertTrue(self.calib.is_affine())
-
-    def testSlope(self):
-        self.assertEqual(self.calib.get_slope(), 1.0)
-
-    def testYIntercept(self):
-        self.assertEqual(self.calib(0.0), 0.0)
-
-    def testCall(self):
-        self.assertTrue(numpy.array_equal(self.calib(X), X))
+    assert calib.is_affine()
+    assert calib.get_slope() == slope
+    assert calib(0.0) == y_intercept
+    values = numpy.array([3.14, 2.73, 1337])
+    assert numpy.array_equal(calib(values), y_intercept + slope * values)
 
 
-class TestLinearCalibration(unittest.TestCase):
-    def setUp(self):
-        self.y_intercept = 1.5
-        self.slope = 2.5
-        self.calib = LinearCalibration(y_intercept=self.y_intercept, slope=self.slope)
+class TestArrayCalibration:
+    def testAffineArray(self):
+        calib = ArrayCalibration([0.1, 0.2, 0.3])
+        assert calib.is_affine()
+        assert calib.get_slope() == 0.1
 
-    def testIsAffine(self):
-        self.assertTrue(self.calib.is_affine())
+    def testNotAffineArray(self):
+        array = numpy.array([45.2, 25.3, 666.0, -8.0])
+        calib = ArrayCalibration(array)
 
-    def testSlope(self):
-        self.assertEqual(self.calib.get_slope(), self.slope)
+        assert not calib.is_affine()
 
-    def testYIntercept(self):
-        self.assertEqual(self.calib(0.0), self.y_intercept)
+        with pytest.raises(AttributeError):
+            calib.get_slope()
 
-    def testCall(self):
-        self.assertTrue(
-            numpy.array_equal(self.calib(X), self.y_intercept + self.slope * X)
-        )
+        assert calib(0) == array[0]
 
+        values = numpy.array([3.14, 2.73, 1337])
+        with pytest.raises(ValueError):
+            # values is an array with a different shape
+            calib(values)
 
-class TestArrayCalibration(unittest.TestCase):
-    def setUp(self):
-        self.arr = numpy.array([45.2, 25.3, 666.0, -8.0])
-        self.calib = ArrayCalibration(self.arr)
-        self.affine_calib = ArrayCalibration([0.1, 0.2, 0.3])
-
-    def testIsAffine(self):
-        self.assertFalse(self.calib.is_affine())
-        self.assertTrue(self.affine_calib.is_affine())
-
-    def testSlope(self):
-        with self.assertRaises(AttributeError):
-            self.calib.get_slope()
-        self.assertEqual(self.affine_calib.get_slope(), 0.1)
-
-    def testYIntercept(self):
-        self.assertEqual(self.calib(0), self.arr[0])
-
-    def testCall(self):
-        with self.assertRaises(ValueError):
-            # X is an array with a different shape
-            self.calib(X)
-
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             # floats are not valid indices
-            self.calib(3.14)
+            calib(3.14)
 
-        self.assertTrue(numpy.array_equal(self.calib([1, 2, 3, 4]), self.arr))
+        assert numpy.array_equal(calib([1, 2, 3, 4]), array)
 
-        for idx, value in enumerate(self.arr):
-            self.assertEqual(self.calib(idx), value)
+        for idx, value in enumerate(array):
+            assert calib(idx) == value
 
     def testEmptyArray(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ArrayCalibration(numpy.array([]))
 
-    def testOneElementArray(self):
+    def testSingleElementArray(self):
         calib = ArrayCalibration(numpy.array([1]))
-        self.assertFalse(calib.is_affine())
+        assert not calib.is_affine()
 
 
-class TestFunctionCalibration(unittest.TestCase):
-    def setUp(self):
-        self.non_affine_fun = numpy.sin
-        self.non_affine_calib = FunctionCalibration(self.non_affine_fun)
+class TestFunctionCalibration:
+    def testAffineFunction(self):
+        def affine_function(x):
+            return 52.0 * x + 0.01
 
-        self.affine_fun = lambda x: 52.0 * x + 0.01
-        self.affine_calib = FunctionCalibration(self.affine_fun, is_affine=True)
+        calib = FunctionCalibration(affine_function, is_affine=True)
 
-    def testIsAffine(self):
-        self.assertFalse(self.non_affine_calib.is_affine())
-        self.assertTrue(self.affine_calib.is_affine())
+        assert calib.is_affine()
+        assert numpy.isclose(calib.get_slope(), 52.0)
+        for value in numpy.array([3.14, 2.73, 1337]):
+            assert numpy.isclose(calib(value), affine_function(value))
 
-    def testSlope(self):
-        with self.assertRaises(AttributeError):
-            self.non_affine_calib.get_slope()
-        self.assertAlmostEqual(self.affine_calib.get_slope(), 52.0)
+    def testNotAffineFunction(self):
+        non_affine_function = numpy.sin
+        calib = FunctionCalibration(non_affine_function)
 
-    def testCall(self):
-        for x in X:
-            self.assertAlmostEqual(self.non_affine_calib(x), self.non_affine_fun(x))
-            self.assertAlmostEqual(self.affine_calib(x), self.affine_fun(x))
+        assert not calib.is_affine()
+        with pytest.raises(AttributeError):
+            calib.get_slope()
+        for x in numpy.array([3.14, 2.73, 1337]):
+            assert numpy.isclose(calib(x), non_affine_function(x))
