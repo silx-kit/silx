6a4f0c1563c423a3dec8e92da2a47d7c6a42af9b
edgar1993a <edgar.gutierrez-fernandez@esrf.fr>
2026-03-13 09:43:47 +0100

    opengl not implemented

--- src/silx/gui/plot/backends/BackendOpenGL.py
+++ src/silx/gui/plot/backends/BackendOpenGL.py
@@ -1597,7 +1597,7 @@ class BackendOpenGL(BackendBase.BackendBase, glu.OpenGLWidget):
             self._plotFrame.y2Axis.isLog = flag
 
     def setYAxisArcsinh(self, flag):
-        ...
+        raise NotImplementedError("Plot OpenGL backend does not support arcsinh Y axis")
         
     def setYAxisInverted(self, flag):
         if flag != self._plotFrame.isYAxisInverted:
