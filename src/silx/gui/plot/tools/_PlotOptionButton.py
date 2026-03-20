517494d447237b31c43e7c8e3556797da1dc2337
payno <henri.payno@gmail.com>
2026-03-20 06:31:04 +0100

    PlotOptionButton: make it inherit from `PlotToolButton`

--- src/silx/gui/plot/tools/_PlotOptionButton.py
+++ src/silx/gui/plot/tools/_PlotOptionButton.py
@@ -1,23 +1,21 @@
 from __future__ import annotations
 
-from weakref import ref
 from typing import TYPE_CHECKING
 from silx.gui import qt
 import qtawesome
+from .PlotToolButton import PlotToolButton
+
 
 if TYPE_CHECKING:
     from ..PlotWindow import PlotWindow
 
-
-class PlotOptionButton(qt.QPushButton):
+class PlotOptionButton(PlotToolButton):
     """Button presented as a 'burger' menu to present user contextual actions such as 'Legend' or 'Region Of Interest'."""
 
     def __init__(self, parent: PlotWindow | None = None):
         super().__init__(parent)
         self._plot = None
 
-        self.setFlat(True)
-        # should be presented on the right of the toolbar and with the burger menu
         self.setIcon(qtawesome.icon("fa6s.bars"))
 
         self._menu = qt.QMenu(self)
@@ -25,22 +23,8 @@ class PlotOptionButton(qt.QPushButton):
         self.clicked.connect(self._showPlotActionMenu)
         self._menu.aboutToShow.connect(self._customControlButtonMenu)
 
-    def setPlot(self, plot: PlotWindow):
-        from ..PlotWindow import PlotWindow  # Avoid cyclic import
-
-        if not isinstance(plot, PlotWindow):
-            raise TypeError(
-                f"plot should be an instanec of {PlotWindow}. Got {type(plot)}"
-            )
-        self._plot = ref(plot)
-
-    def getPlot(self) -> PlotWindow | None:
-        if self._plot is None:
-            return None
-        return self._plot()
-
     def _customControlButtonMenu(self):
-        plot = self.getPlot()
+        plot = self.plot()
         if plot is None:
             raise RuntimeError(
                 "Plot not found. Either the plot is not set (make sure 'setPlot' is called) or the plot has been deleted."
