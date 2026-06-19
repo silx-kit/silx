7ba394e64594d1b195aa511155771e3b55443a60
Loic Huder <loic.huder@esrf.fr>
2026-06-19 10:13:16 +0200

    silx.gui.plot.test.test_stackview: Migrate test to pytest

--- src/silx/gui/plot/test/test_stackview.py
+++ src/silx/gui/plot/test/test_stackview.py
@@ -1,269 +1,198 @@
-# /*##########################################################################
-#
-# Copyright (c) 2016-2023 European Synchrotron Radiation Facility
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# ###########################################################################*/
-"""Basic tests for StackView"""
-
-__authors__ = ["P. Knobel"]
-__license__ = "MIT"
-__date__ = "20/03/2017"
-
-
 import numpy
+import pytest
 
-from silx.gui.utils.testutils import TestCaseQt, SignalListener
-
-from silx.gui import qt
-from silx.gui.plot import StackView
-from silx.gui.plot.StackView import StackViewMainWindow
-
-from silx.utils.array_like import ListOfImages
+from ...plot.StackView import StackView
+from ...plot.StackView import StackViewMainWindow
+from ...utils.testutils import SignalListener
+from ....utils.array_like import ListOfImages
 
 
-class TestStackView(TestCaseQt):
-    """Base class for tests of StackView."""
-
-    def setUp(self):
-        super().setUp()
-        self.stackview = StackView()
-        self.stackview.show()
-        self.qWaitForWindowExposed(self.stackview)
-        self.mystack = numpy.fromfunction(
-            lambda i, j, k: (
-                numpy.sin(i / 15.0) + numpy.cos(j / 4.0) + 2 * numpy.sin(k / 6.0)
-            ),
-            (10, 20, 30),
-        )
-
-    def tearDown(self):
-        self.stackview.setAttribute(qt.Qt.WA_DeleteOnClose)
-        self.stackview.close()
-        del self.stackview
-        super().tearDown()
-
-    def testScaleColormapRangeToStack(self):
-        """Test scaleColormapRangeToStack"""
-        self.stackview.setStack(self.mystack)
-        self.stackview.setColormap("viridis")
-        colormap = self.stackview.getColormap()
-
-        # Colormap autoscale to image
-        self.assertEqual(colormap.getVRange(), (None, None))
-        self.stackview.scaleColormapRangeToStack()
-
-        # Colormap range set according to stack range
-        self.assertEqual(colormap.getVRange(), (self.mystack.min(), self.mystack.max()))
-
-    def testSetStack(self):
-        self.stackview.setStack(self.mystack)
-        self.stackview.setColormap("viridis")
-        my_trans_stack = self.stackview.getData()
-        self.assertEqual(my_trans_stack.shape, self.mystack.shape)
-        self.assertTrue(numpy.array_equal(self.mystack, my_trans_stack))
-
-        colormap = self.stackview.getColormap()
-        self.assertEqual(colormap["name"], "viridis")
-
-    def testSetStackPerspective(self):
-        self.stackview.setStack(self.mystack, perspective=1)
-        my_trans_stack = self.stackview.getCurrentData()
-
-        # get stack returns the transposed data, depending on the perspective
-        self.assertEqual(
-            my_trans_stack.shape,
-            (self.mystack.shape[1], self.mystack.shape[0], self.mystack.shape[2]),
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                numpy.transpose(self.mystack, axes=(1, 0, 2)), my_trans_stack
-            )
-        )
-
-    def testSetStackListOfImages(self):
-        loi = [self.mystack[i] for i in range(self.mystack.shape[0])]
-
-        self.stackview.setStack(loi)
-        my_orig_stack = self.stackview.getData(returnNumpyArray=True)
-        my_trans_stack = self.stackview.getData(returnNumpyArray=True)
-        self.assertEqual(my_trans_stack.shape, self.mystack.shape)
-        self.assertTrue(numpy.array_equal(self.mystack, my_trans_stack))
-        self.assertTrue(numpy.array_equal(self.mystack, my_orig_stack))
-        self.assertIsInstance(my_trans_stack, numpy.ndarray)
-
-        self.stackview.setStack(loi, perspective=2)
-        my_orig_stack = self.stackview.getData(copy=False)
-        my_trans_stack = self.stackview.getCurrentData(copy=False)
-        # getData(copy=False) must return the object set in setStack
-        self.assertIs(my_orig_stack, loi)
-        # getCurrentData(copy=False) returns a ListOfImages whose .images
-        # attr is the original data
-        self.assertEqual(
-            my_trans_stack.shape,
-            (self.mystack.shape[2], self.mystack.shape[0], self.mystack.shape[1]),
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                numpy.array(my_trans_stack),
-                numpy.transpose(self.mystack, axes=(2, 0, 1)),
-            )
-        )
-        self.assertIsInstance(
-            my_trans_stack, ListOfImages
-        )  # returnNumpyArray=False by default in getStack
-        self.assertIs(my_trans_stack.images, loi)
-
-    def testPerspective(self):
-        self.stackview.setStack(numpy.arange(24).reshape((2, 3, 4)))
-        self.assertEqual(
-            self.stackview._perspective, 0, "Default perspective is not 0 (dim1-dim2)."
-        )
-
-        self.stackview._StackView__planeSelection.setPerspective(1)
-        self.assertEqual(
-            self.stackview._perspective,
-            1,
-            "Plane selection combobox not updating perspective",
-        )
-
-        self.stackview.setStack(numpy.arange(6).reshape((1, 2, 3)))
-        self.assertEqual(
-            self.stackview._perspective,
-            1,
-            "Perspective not preserved when calling setStack "
-            "without specifying the perspective parameter.",
-        )
-
-        self.stackview.setStack(numpy.arange(24).reshape((2, 3, 4)), perspective=2)
-        self.assertEqual(
-            self.stackview._perspective,
-            2,
-            "Perspective not set in setStack(..., perspective=2).",
-        )
-
-    def testDefaultTitle(self):
-        """Test that the plot title contains the proper Z information"""
-        self.stackview.setStack(
-            numpy.arange(24).reshape((4, 3, 2)),
-            calibrations=[(0, 1), (-10, 10), (3.14, 3.14)],
-        )
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=0")
-        self.stackview.setFrameNumber(2)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=2")
-
-        self.stackview._StackView__planeSelection.setPerspective(1)
-        self.stackview.setFrameNumber(0)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=-10")
-        self.stackview.setFrameNumber(2)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=10")
-
-        self.stackview._StackView__planeSelection.setPerspective(2)
-        self.stackview.setFrameNumber(0)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=3.14")
-        self.stackview.setFrameNumber(1)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Image z=6.28")
-
-    def testCustomTitle(self):
-        """Test setting the plot title with a user defined callback"""
-        self.stackview.setStack(
-            numpy.arange(24).reshape((4, 3, 2)),
-            calibrations=[(0, 1), (-10, 10), (3.14, 3.14)],
-        )
-
-        def title_callback(frame_idx):
-            return "Cubed index title %d" % (frame_idx**3)
-
-        self.stackview.setTitleCallback(title_callback)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Cubed index title 0")
-        self.stackview.setFrameNumber(2)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Cubed index title 8")
-
-        # perspective should not matter, only frame index
-        self.stackview._StackView__planeSelection.setPerspective(1)
-        self.stackview.setFrameNumber(0)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Cubed index title 0")
-        self.stackview.setFrameNumber(2)
-        self.assertEqual(self.stackview._plot.getGraphTitle(), "Cubed index title 8")
-
-        with self.assertRaises(TypeError):
-            # setTitleCallback should not accept non-callable objects like strings
-            self.stackview.setTitleCallback(
-                "Là, vous faites sirop de vingt-et-un et vous dites : "
-                "beau sirop, mi-sirop, siroté, gagne-sirop, sirop-grelot,"
-                " passe-montagne, sirop au bon goût."
-            )
-
-    def testStackFrameNumber(self):
-        self.stackview.setStack(self.mystack)
-        self.assertEqual(self.stackview.getFrameNumber(), 0)
-
-        listener = SignalListener()
-        self.stackview.sigFrameChanged.connect(listener)
-
-        self.stackview.setFrameNumber(1)
-        self.assertEqual(self.stackview.getFrameNumber(), 1)
-        self.assertEqual(listener.arguments(), [(1,)])
+@pytest.fixture()
+def stack_data() -> numpy.ndarray:
+    return numpy.fromfunction(
+        lambda i, j, k: (
+            numpy.sin(i / 15.0) + numpy.cos(j / 4.0) + 2 * numpy.sin(k / 6.0)
+        ),
+        (10, 20, 30),
+    )
 
 
-class TestStackViewMainWindow(TestCaseQt):
-    """Base class for tests of StackView."""
+@pytest.fixture()
+def stack_view(qWidgetFactory, qapp_utils) -> StackView:
+    widget = qWidgetFactory(StackView)
+    qapp_utils.qWaitForWindowExposed(widget)
+    return widget
 
-    def setUp(self):
-        super().setUp()
-        self.stackview = StackViewMainWindow()
-        self.stackview.show()
-        self.qWaitForWindowExposed(self.stackview)
-        self.mystack = numpy.fromfunction(
-            lambda i, j, k: (
-                numpy.sin(i / 15.0) + numpy.cos(j / 4.0) + 2 * numpy.sin(k / 6.0)
-            ),
-            (10, 20, 30),
-        )
 
-    def tearDown(self):
-        self.stackview.setAttribute(qt.Qt.WA_DeleteOnClose)
-        self.stackview.close()
-        del self.stackview
-        super().tearDown()
+def testScaleColormapRangeToStack(stack_view, stack_data):
+    """Test scaleColormapRangeToStack"""
+    stack_view.setStack(stack_data)
+    stack_view.setColormap("viridis")
+    colormap = stack_view.getColormap()
 
-    def testSetStack(self):
-        self.stackview.setStack(self.mystack)
-        self.stackview.setColormap("viridis")
-        my_trans_stack = self.stackview.getData()
-        self.assertEqual(my_trans_stack.shape, self.mystack.shape)
-        self.assertTrue(numpy.array_equal(self.mystack, my_trans_stack))
+    # Colormap autoscale to image
+    assert colormap.getVRange() == (None, None)
+    stack_view.scaleColormapRangeToStack()
 
-        colormap = self.stackview.getColormap()
-        self.assertEqual(colormap["name"], "viridis")
+    # Colormap range set according to stack range
+    assert colormap.getVRange() == (stack_data.min(), stack_data.max())
 
-    def testSetStackPerspective(self):
-        self.stackview.setStack(self.mystack, perspective=1)
-        my_trans_stack = self.stackview.getCurrentData()
-        # get stack returns the transposed data, depending on the perspective
-        self.assertEqual(
-            my_trans_stack.shape,
-            (self.mystack.shape[1], self.mystack.shape[0], self.mystack.shape[2]),
-        )
-        self.assertTrue(
-            numpy.array_equal(
-                numpy.transpose(self.mystack, axes=(1, 0, 2)), my_trans_stack
-            )
-        )
+
+def testSetStack(stack_view, stack_data):
+    stack_view.setStack(stack_data)
+    stack_view.setColormap("viridis")
+    transposed_stack = stack_view.getData()
+    assert transposed_stack.shape == stack_data.shape
+    assert numpy.array_equal(stack_data, transposed_stack)
+
+    colormap = stack_view.getColormap()
+    assert colormap["name"] == "viridis"
+
+
+def testSetStackPerspective(stack_view, stack_data):
+    stack_view.setStack(stack_data, perspective=1)
+    transposed_stack = stack_view.getCurrentData()
+
+    # get stack returns the transposed data, depending on the perspective
+    assert transposed_stack.shape == (
+        stack_data.shape[1],
+        stack_data.shape[0],
+        stack_data.shape[2],
+    )
+    numpy.array_equal(numpy.transpose(stack_data, axes=(1, 0, 2)), transposed_stack)
+
+
+def testSetStackListOfImages(stack_view, stack_data):
+    list_of_images = [stack_data[i] for i in range(stack_data.shape[0])]
+
+    stack_view.setStack(list_of_images)
+    original_stack_data = stack_view.getData(returnNumpyArray=True)
+    transposed_stack_data = stack_view.getData(returnNumpyArray=True)
+    assert transposed_stack_data.shape == stack_data.shape
+    assert numpy.array_equal(stack_data, transposed_stack_data)
+    assert numpy.array_equal(stack_data, original_stack_data)
+    assert isinstance(transposed_stack_data, numpy.ndarray)
+
+    stack_view.setStack(list_of_images, perspective=2)
+    # getData(copy=False) must return the object set in setStack
+    original_stack_data = stack_view.getData(copy=False)
+    assert original_stack_data is list_of_images
+    # getData(copy=False) returns a ListOfImages whose .images is the original data
+    transposed_stack_data = stack_view.getCurrentData(copy=False)
+    assert transposed_stack_data.shape == (
+        stack_data.shape[2],
+        stack_data.shape[0],
+        stack_data.shape[1],
+    )
+    assert numpy.array_equal(
+        numpy.array(transposed_stack_data),
+        numpy.transpose(stack_data, axes=(2, 0, 1)),
+    )
+    assert isinstance(transposed_stack_data, ListOfImages)
+    assert transposed_stack_data.images is list_of_images
+
+
+def testPerspective(stack_view):
+    stack_view.setStack(numpy.arange(24).reshape((2, 3, 4)))
+    assert stack_view.getPerspective() == 0, "Default perspective is not 0 (dim1-dim2)."
+
+    stack_view.setPerspective(1)
+    assert stack_view.getPerspective() == 1, (
+        "Plane selection combobox not updating perspective"
+    )
+
+    stack_view.setStack(numpy.arange(6).reshape((1, 2, 3)))
+    assert stack_view.getPerspective() == 1, (
+        "Perspective not preserved when calling setStack without specifying the perspective parameter."
+    )
+
+    stack_view.setStack(numpy.arange(24).reshape((2, 3, 4)), perspective=2)
+    assert stack_view.getPerspective() == 2, (
+        "Perspective not set in setStack(..., perspective=2)."
+    )
+
+
+def testPlotTitleContainsZInfo(stack_view):
+    stack_view.setStack(
+        numpy.arange(24).reshape((4, 3, 2)),
+        calibrations=[(0, 1), (-10, 10), (3.14, 3.14)],
+    )
+    assert stack_view.getGraphTitle() == "Image z=0"
+    stack_view.setFrameNumber(2)
+    assert stack_view.getGraphTitle() == "Image z=2"
+
+    stack_view.setPerspective(1)
+    stack_view.setFrameNumber(0)
+    assert stack_view.getGraphTitle() == "Image z=-10"
+    stack_view.setFrameNumber(2)
+    assert stack_view.getGraphTitle() == "Image z=10"
+
+    stack_view.setPerspective(2)
+    stack_view.setFrameNumber(0)
+    assert stack_view.getGraphTitle() == "Image z=3.14"
+    stack_view.setFrameNumber(1)
+    assert stack_view.getGraphTitle() == "Image z=6.28"
+
+
+def testSetTitleCallback(stack_view, stack_data):
+    """Test setting the plot title with a user defined callback"""
+    stack_view.setStack(
+        numpy.arange(24).reshape((4, 3, 2)),
+        calibrations=[(0, 1), (-10, 10), (3.14, 3.14)],
+    )
+
+    def title_callback(frame_idx):
+        return "Cubed index title %d" % (frame_idx**3)
+
+    stack_view.setTitleCallback(title_callback)
+    assert stack_view.getGraphTitle() == "Cubed index title 0"
+    stack_view.setFrameNumber(2)
+    assert stack_view.getGraphTitle() == "Cubed index title 8"
+
+    # perspective should not matter, only frame index
+    stack_view.setPerspective(1)
+    stack_view.setFrameNumber(0)
+    assert stack_view.getGraphTitle() == "Cubed index title 0"
+    stack_view.setFrameNumber(2)
+    assert stack_view.getGraphTitle() == "Cubed index title 8"
+
+    with pytest.raises(TypeError):
+        # setTitleCallback should not accept non-callable objects like strings
+        stack_view.setTitleCallback("This is a string")
+
+
+def testStackFrameNumber(stack_view, stack_data):
+    stack_view.setStack(stack_data)
+    assert stack_view.getFrameNumber() == 0
+
+    listener = SignalListener()
+    stack_view.sigFrameChanged.connect(listener)
+
+    stack_view.setFrameNumber(1)
+    assert stack_view.getFrameNumber() == 1
+    assert listener.arguments() == [(1,)]
+
+
+def testStackViewMainWindow(qWidgetFactory, qapp_utils, stack_data):
+    stackview = qWidgetFactory(StackViewMainWindow)
+    qapp_utils.qWaitForWindowExposed(stackview)
+    stackview.setStack(stack_data)
+    stackview.setColormap("viridis")
+    transposed_stack = stackview.getData()
+    assert transposed_stack.shape == stack_data.shape
+    assert numpy.array_equal(stack_data, transposed_stack)
+
+    colormap = stackview.getColormap()
+    assert colormap["name"] == "viridis"
+
+    stackview.setStack(stack_data, perspective=1)
+    transposed_stack = stackview.getCurrentData()
+    # get stack returns the transposed data, depending on the perspective
+    assert transposed_stack.shape == (
+        stack_data.shape[1],
+        stack_data.shape[0],
+        stack_data.shape[2],
+    )
+    assert numpy.array_equal(
+        numpy.transpose(stack_data, axes=(1, 0, 2)), transposed_stack
+    )
