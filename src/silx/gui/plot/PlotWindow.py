695b509c17884c3b1bc78174dbe44604e7cf82fe
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-06-19 15:44:51 +0200

    rename chooseAxis->axisScales and move buttons in toolbar

--- src/silx/gui/plot/PlotWindow.py
+++ src/silx/gui/plot/PlotWindow.py
@@ -109,7 +109,6 @@ class PlotWindow(PlotWidget):
         resetzoom=True,
         autoScale=True,
         logScale=True,
-        chooseScale=False,
         grid=True,
         curveStyle=True,
         colormap=True,
@@ -123,6 +122,7 @@ class PlotWindow(PlotWidget):
         roi=True,
         mask=True,
         fit=False,
+        axisScales=False,
     ):
         super().__init__(parent=parent, backend=backend)
         if parent is None:
@@ -186,14 +186,14 @@ class PlotWindow(PlotWidget):
         self.yAxisLogarithmicAction.setVisible(logScale)
         self.addAction(self.yAxisLogarithmicAction)
 
-        self.xAxisScaleButton = PlotToolButtons.XAxisScaleToolButton(
+        self._xAxisScaleButton = PlotToolButtons.XAxisScaleToolButton(
             parent=self, plot=self
         )
-        self.xAxisScaleButton.setVisible(chooseScale)
-        self.yAxisScaleButton = PlotToolButtons.YAxisScaleToolButton(
+        self._xAxisScaleButton.setVisible(axisScales)
+        self._yAxisScaleButton = PlotToolButtons.YAxisScaleToolButton(
             parent=self, plot=self
         )
-        self.yAxisScaleButton.setVisible(chooseScale)
+        self._yAxisScaleButton.setVisible(axisScales)
 
         self.gridAction = self.group.addAction(
             actions.control.GridAction(self, gridMode="both", parent=self)
@@ -487,12 +487,8 @@ class PlotWindow(PlotWidget):
         self.yAxisInvertedAction = toolbar.insertWidget(
             self.colorbarAction, self.yAxisInvertedButton
         )
-        self.xAxisScaleAction = toolbar.insertWidget(
-            self.colorbarAction, self.xAxisScaleButton
-        )
-        self.yAxisScaleAction = toolbar.insertWidget(
-            self.colorbarAction, self.yAxisScaleButton
-        )
+        toolbar.insertWidget(self.gridAction, self._xAxisScaleButton)
+        toolbar.insertWidget(self.gridAction, self._yAxisScaleButton)
         return toolbar
 
     def saveGraph(
@@ -782,6 +778,12 @@ class PlotWindow(PlotWidget):
         """
         return self.yAxisLogarithmicAction
 
+    def getXAxisScaleButton(self) -> PlotToolButtons.XAxisScaleToolButton:
+        return self._xAxisScaleButton
+
+    def getYAxisScaleButton(self) -> PlotToolButtons.YAxisScaleToolButton:
+        return self._yAxisScaleButton
+
     def getGridAction(self):
         """Action to toggle the grid visibility in the plot
 
@@ -923,7 +925,7 @@ class Plot1D(PlotWindow):
             resetzoom=True,
             autoScale=True,
             logScale=False,
-            chooseScale=True,
+            axisScales=True,
             grid=True,
             curveStyle=True,
             colormap=False,
@@ -975,7 +977,7 @@ class Plot2D(PlotWindow):
             resetzoom=True,
             autoScale=False,
             logScale=False,
-            chooseScale=False,
+            axisScales=False,
             grid=False,
             curveStyle=False,
             colormap=True,
