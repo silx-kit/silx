83c4f6523f124c4da571da0f4a5d2df7e380f826
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-07-28 17:51:50 +0200

    add axis_scale helper module

---
+++ src/silx/gui/plot/_utils/axis_scale.py
@@ -0,0 +1,74 @@
+import numpy
+from ..items.types import AxisScaleType
+
+
+# Float 32 info ###############################################################
+# Using min/max value below limits of float32
+# so operation with such value (e.g., max - min) do not overflow
+
+FLOAT32_SAFE_MIN = -1e37
+FLOAT32_MINPOS = numpy.finfo(numpy.float32).tiny
+FLOAT32_SAFE_MAX = 1e37
+
+
+def isValid(axisScale: AxisScaleType, value: float) -> bool:
+    if not numpy.isfinite(value):
+        return False
+
+    if axisScale == "linear":
+        return True
+    elif axisScale == "log":
+        return value > 0.0
+    elif axisScale == "asinh":
+        return True
+    else:
+        raise ValueError(f"Unsupported axis scale: {axisScale}")
+
+
+# TODO valueerror, overflowerror, warnings, safe range?
+def apply(axisScale: AxisScaleType, value: float) -> float:
+    if axisScale == "linear":
+        return value
+    elif axisScale == "log":
+        if value > 0.0:
+            return numpy.log10(value)
+        else:
+            return float("nan")
+    elif axisScale == "asinh":
+        return numpy.asinh(value)
+    else:
+        raise ValueError(f"Unsupported axis scale: {axisScale}")
+
+
+# TODO valueerror, overflowerror, warnings, safe range?
+def revert(axisScale: AxisScaleType, value: float) -> float:
+    if axisScale == "linear":
+        return value
+    elif axisScale == "log":
+        return pow(10.0, value)
+    elif axisScale == "asinh":
+        return numpy.sinh(value)
+    else:
+        raise ValueError(f"Unsupported axis scale: {axisScale}")
+
+
+def safeRange(axisScale: AxisScaleType) -> tuple[float, float]:
+    """Return axis range (min, max) below limits of float32 so that max - min does not overflow"""
+    if axisScale == "linear":
+        return FLOAT32_SAFE_MIN, FLOAT32_SAFE_MAX
+    elif axisScale == "log":
+        return FLOAT32_MINPOS, FLOAT32_SAFE_MAX
+    elif axisScale == "asinh":
+        return FLOAT32_SAFE_MIN, FLOAT32_SAFE_MAX
+    else:
+        raise ValueError(f"Unsupported axis scale: {axisScale}")
+
+
+def inSafeRange(axisScale: AxisScaleType, value: float) -> bool:
+    min_, max_ = safeRange(axisScale)
+    return min_ <= value <= max_
+
+
+def clipToSafeRange(axisScale: AxisScaleType, value: float) -> float:
+    axisMin, axisMax = safeRange(axisScale)
+    return numpy.clip(value, axisMin, axisMax)
