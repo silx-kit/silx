ffafed0704a10a74286aa2a533d022503f9d12cf
Henri Payno <henri.payno@esrf.fr>
2026-08-12 08:39:39 +0200

    FitWidget: fix invalid corrections

--- src/silx/gui/fit/FitWidget.py
+++ src/silx/gui/fit/FitWidget.py
@@ -594,19 +594,14 @@ class FitWidget(qt.QWidget):
         if bgtheory in self.fitmanager.bgtheories:
             self.fitmanager.setbackground(bgtheory)
         else:
-            functionsfile = qt.QFileDialog.getOpenFileName(
+            functionsfile, _ = qt.QFileDialog.getOpenFileName(
                 self,
                 "Select python module with your function(s)",
                 "",
                 "Python Files (*.py);;All Files (*)",
-functionsfile, _ = qt.QFileDialog.getOpenFileName(
-self,
-"Select python module with your function(s)",
-"",
-"Python Files (*.py);;All Files (*)",
             )
 
-            if functionsfile:
+            if functionsfile != "":
                 try:
                     self.fitmanager.loadbgtheories(functionsfile)
                 except ImportError:
@@ -640,19 +635,14 @@ self,
             self.fitmanager.settheory(theoryname)
         else:
             # open a load file dialog
-            functionsfile = qt.QFileDialog.getOpenFileName(
+            functionsfile, _ = qt.QFileDialog.getOpenFileName(
                 self,
                 "Select python module with your function(s)",
                 "",
                 "Python Files (*.py);;All Files (*)",
-functionsfile, _ = qt.QFileDialog.getOpenFileName(
-self,
-"Select python module with your function(s)",
-"",
-"Python Files (*.py);;All Files (*)",
             )
 
-            if functionsfile:
+            if functionsfile != "":
                 try:
                     self.fitmanager.loadtheories(functionsfile)
                 except ImportError:
